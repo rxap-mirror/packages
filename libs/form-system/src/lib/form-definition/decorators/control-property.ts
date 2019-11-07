c15a417238a3891c095e7d68c8a7d1a736102207
Merzough Badry Münker <badry.muenker@digitaix.com>
2019-11-07 13:19:11 +0100

    add prohibited property key check

--- libs/form-system/src/lib/form-definition/decorators/control-property.ts
+++ libs/form-system/src/lib/form-definition/decorators/control-property.ts
@@ -6,12 +6,21 @@ import {
 import { FormControlMetaData } from './control';
 import { FormDefinitionMetaDataKeys } from './meta-data-keys';
 
+export const PROHIBITED_PROPERTY_KEYS = [
+  'formId',
+  'controlId'
+];
+
 export function RxapControlProperty<PropertyValue>(propertyKey: string, propertyValue: PropertyValue) {
   return function(target: any, pk: string) {
     const control = getMetadata<FormControlMetaData<any>>(FormDefinitionMetaDataKeys.CONTROL, target, pk);
 
     if (!control) {
       throw new Error(`The RxapFormControl decorator must be called before the RxapControlValidator decorator for propertyKey '${propertyKey}'`);
+    }
+
+    if (PROHIBITED_PROPERTY_KEYS.includes(propertyKey)) {
+      throw new Error(`Control property '${propertyKey}' is not allowed`);
     }
 
     control.properties[ propertyKey ] = propertyValue;
@@ -27,6 +36,10 @@ export function RxapControlProperties(properties: KeyValue) {
 
     if (!control) {
       throw new Error(`The RxapFormControl decorator must be called before the RxapControlValidator decorator for propertyKey '${propertyKey}'`);
+    }
+
+    if (Object.keys(properties).some(pk => PROHIBITED_PROPERTY_KEYS.includes(pk))) {
+      throw new Error(`Control properties '${Object.keys(properties).filter(pk => PROHIBITED_PROPERTY_KEYS.includes(pk)).join(', ')}' are not allowed`);
     }
 
     Object.assign(control.properties, properties);
