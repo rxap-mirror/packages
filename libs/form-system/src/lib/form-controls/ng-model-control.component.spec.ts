7fc232943d6304ec9f76dc69fc1f4599ebf1d043
Merzough Badry Münker <badry.muenker@digitaix.com>
2019-11-25 11:02:11 +0100

    fix ng model control issue

---
+++ libs/form-system/src/lib/form-controls/ng-model-control.component.spec.ts
@@ -0,0 +1,73 @@
+import {
+  NgModelControlComponent,
+  NgModelControlSubscriptions
+} from './ng-model-control.component';
+import { BaseFormControl } from '../forms/form-controls/base.form-control';
+import createSpy = jasmine.createSpy;
+
+describe('Form System', () => {
+
+  describe('Form Controls', () => {
+
+    describe('NgModelControl', () => {
+
+      let component: NgModelControlComponent<any, any>;
+      let control: BaseFormControl<any>;
+
+      beforeEach(() => {
+
+        component = new NgModelControlComponent<any, BaseFormControl<any>>({ markForCheck: () => {} } as any);
+        control   = component.control = BaseFormControl.EMPTY();
+
+        control.init();
+        control.rxapOnInit();
+
+
+      });
+
+      it('[BUG] registerOnModelChange would not call passed functions if the model getter is not used', () => {
+
+        const valueChangeSpy = createSpy();
+
+        component.registerOnModelChange(valueChangeSpy);
+
+        expect(valueChangeSpy).not.toBeCalled();
+
+        const value = 'value';
+
+        control.setValue(value);
+
+        expect(valueChangeSpy).toBeCalled();
+
+      });
+
+      it('registerOnModelChange should call passed function if the control value changes', () => {
+
+        const valueChangeSpy = createSpy();
+
+        expect(component.control).toBeInstanceOf(BaseFormControl);
+
+        expect(component.subscriptions.has(NgModelControlSubscriptions.MODEL)).toBeFalsy();
+        expect(component.subscriptions.has(NgModelControlSubscriptions.ON_CHANGE)).toBeFalsy();
+
+        component.registerOnModelChange(valueChangeSpy);
+
+        expect(component.subscriptions.has(NgModelControlSubscriptions.MODEL)).toBeTruthy();
+        expect(component.subscriptions.has(NgModelControlSubscriptions.ON_CHANGE)).toBeTruthy();
+
+        const value = 'value';
+
+        control.setValue(value);
+
+        expect(valueChangeSpy).toBeCalled();
+        expect(valueChangeSpy).toBeCalledWith(value);
+
+
+      });
+
+
+    });
+
+  });
+
+});
--- libs/form-system/src/lib/form-controls/ng-model-control.component.ts
+++ libs/form-system/src/lib/form-controls/ng-model-control.component.ts
@@ -2,8 +2,8 @@ import { BaseControlComponent } from './base-control.component';
 import { Injectable } from '@angular/core';
 import { BaseFormControl } from '../forms/form-controls/base.form-control';
 import { tap } from 'rxjs/operators';
-import { isDeepEqual } from '@rxap/utilities';
 import { Subject } from 'rxjs';
+import { isNotDeepEqual } from '../../../../utilities/src';
 
 export enum NgModelControlSubscriptions {
   MODEL     = 'rxap_ng_model_control_subscription_model',
@@ -17,13 +17,6 @@ export class NgModelControlComponent<ControlValue, FormControl extends BaseFormC
   private _model: ControlValue | null = null;
 
   public get model(): ControlValue | null {
-    if (this.control && !this.subscriptions.has(NgModelControlSubscriptions.MODEL)) {
-      this.subscriptions.add(NgModelControlSubscriptions.MODEL, this.control.valueChange$.pipe(
-        isDeepEqual(this._model),
-        tap(value => this._model = value),
-        tap(value => this._onModelChange.next(value))
-      ).subscribe());
-    }
     if (this.control && !this._model) {
       // if model is not defined, but the control is defined
       // copy the control value to the model
@@ -34,18 +27,33 @@ export class NgModelControlComponent<ControlValue, FormControl extends BaseFormC
 
   public set model(value: ControlValue | null) {
     this._model = value;
-    this._onModelChange.next(value);
+    this.onModelChange$.next(value);
     if (this.control) {
       this.control.setValue(value);
     }
   }
 
-  private _onModelChange = new Subject<ControlValue | null>();
+  public readonly onModelChange$ = new Subject<ControlValue | null>();
 
   public registerOnModelChange(fn: (_: any) => any): void {
-    this.subscriptions.set(NgModelControlSubscriptions.ON_CHANGE, this._onModelChange.pipe(
+    this.initOnModelChange();
+    this.subscriptions.set(NgModelControlSubscriptions.ON_CHANGE, this.onModelChange$.pipe(
       tap(model => fn(model))
     ).subscribe());
+  }
+
+  private initOnModelChange() {
+    if (this.control) {
+      if (!this.subscriptions.has(NgModelControlSubscriptions.MODEL)) {
+        this.subscriptions.add(NgModelControlSubscriptions.MODEL, this.control.valueChange$.pipe(
+          isNotDeepEqual(this._model),
+          tap(value => this._model = value),
+          tap(value => this.onModelChange$.next(value))
+        ).subscribe());
+      }
+    } else {
+      throw new Error('registerOnModelChange should be called after the control is initialized');
+    }
   }
 
 }
