6624996676061be2235ef22efd0e9f7be09bf35e
Merzough Badry Münker <badry.muenker@digitaix.com>
2019-11-25 11:00:27 +0100

    add is not deep equal

--- libs/utilities/src/lib/rxjs/operators/is-deep-equal.ts
+++ libs/utilities/src/lib/rxjs/operators/is-deep-equal.ts
@@ -5,3 +5,7 @@ import { equals } from 'ramda';
 export function isDeepEqual<T>(compareTo: T): MonoTypeOperatorFunction<T> {
   return filter(value => equals(value, compareTo));
 }
+
+export function isNotDeepEqual<T>(compareTo: T): MonoTypeOperatorFunction<T> {
+  return filter(value => !equals(value, compareTo));
+}
