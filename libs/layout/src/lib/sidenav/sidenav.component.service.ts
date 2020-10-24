a826a9be7242c979ca8444f8525773db04f786a9
Merzough Badry Münker <badry.muenker@digitaix.com>
2020-10-24 13:57:26 +0200

    feat(layout): add collapse side navigation button

--- libs/layout/src/lib/sidenav/sidenav.component.service.ts
+++ libs/layout/src/lib/sidenav/sidenav.component.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class SidenavComponentService {
 
+
+  public readonly collapsed$ = new BehaviorSubject(false);
+
+  public toggleNavigationCollapse(): void {
+    this.collapsed$.next(!this.collapsed$.value);
+  }
 
 }
