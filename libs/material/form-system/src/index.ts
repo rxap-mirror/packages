1f81bd0e781999ea1778145b033f522e4607b701
Merzough Badry Münker <badry.muenker@digitaix.com>
2020-10-26 16:29:49 +0100

    feat: add the rxap-material/form-system package

--- libs/material/form-system/src/index.ts
+++ libs/material/form-system/src/index.ts
@@ -1,1 +1,4 @@
-export {};
+export * from './lib/control-error.directive';
+export * from './lib/control-errors.directive';
+export * from './lib/input-clear-button.directive';
+export * from './lib/material-form-system.module';
--- libs/material/form-system/src/lib/material-form-system.module.ts
+++ libs/material/form-system/src/lib/material-form-system.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { ControlErrorDirectiveModule } from './control-error.directive';
 import { ControlErrorsDirectiveModule } from './control-errors.directive';
+import { InputClearButtonDirectiveModule } from './input-clear-button.directive';
 
 @NgModule({
   exports: [
     ControlErrorDirectiveModule,
-    ControlErrorsDirectiveModule
+    ControlErrorsDirectiveModule,
+    InputClearButtonDirectiveModule
   ]
 })
 export class MaterialFormSystemModule {}
