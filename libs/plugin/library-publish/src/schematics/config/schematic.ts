a77d0e0ce966779b8950bf27ad913828656a8b4c
Merzough Badry Münker <badry.muenker@digitaix.com>
2020-10-26 16:08:46 +0100

    fix(config): only use production config if available

--- libs/plugin/library-publish/src/schematics/config/schematic.ts
+++ libs/plugin/library-publish/src/schematics/config/schematic.ts
@@ -25,12 +25,36 @@ export default function(options: ConfigSchema): Rule {
 
           targetOptions.buildTarget = options.buildTarget ?? `${options.project}:build:production`;
 
+          if (!options.buildTarget) {
+            if (project.targets.has('build')) {
+              const buildTarget = project.targets.get('build')!;
+              if (buildTarget.configurations?.hasOwnProperty('production')) {
+                targetOptions.buildTarget = `${options.project}:build:production`;
+              } else {
+                targetOptions.buildTarget = `${options.project}:build`;
+              }
+            } else {
+              throw new Error('The build target is not defined.');
+            }
+          } else {
+            targetOptions.buildTarget = options.buildTarget;
+          }
+
           if (options.preTarget) {
             targetOptions.preTarget = options.preTarget;
-          } else if (project.targets.has('pack')) {
-            targetOptions.preTarget = `${options.project}:pack`;
           } else {
-            targetOptions.preTarget = targetOptions.buildTarget;
+            if (project.targets.has('pack')) {
+              targetOptions.preTarget = `${options.project}:pack`;
+            } else if (project.targets.has('build')) {
+              const buildTarget = project.targets.get('build')!;
+              if (buildTarget.configurations?.hasOwnProperty('production')) {
+                targetOptions.preTarget = `${options.project}:build:production`;
+              } else {
+                targetOptions.preTarget = `${options.project}:build`;
+              }
+            } else {
+              targetOptions.preTarget = options.buildTarget;
+            }
           }
 
           project.targets.add({
