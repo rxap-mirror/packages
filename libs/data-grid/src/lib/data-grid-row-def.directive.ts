7a83e5022cc30feb880494aeffc953cbcbd02445
Merzough Badry Münker <badry.muenker@digitaix.com>
2020-10-26 15:13:40 +0100

    fix: add missing data grid theme

--- libs/data-grid/src/lib/data-grid-row-def.directive.ts
+++ libs/data-grid/src/lib/data-grid-row-def.directive.ts
@@ -1,11 +1,10 @@
 import {
   Directive,
   Input,
-  ContentChild,
+  ContentChild
 } from '@angular/core';
 import { DataGridCellDefDirective } from './data-grid-cell-def.directive';
 import { DataGridHeaderCellDefDirective } from './data-grid-header-cell-def.directive';
-import { coerceBooleanProperty } from '@angular/cdk/coercion';
 import { Required } from '@rxap/utilities';
 
 @Directive({
@@ -14,35 +13,8 @@ import { Required } from '@rxap/utilities';
 })
 export class DataGridRowDefDirective {
 
-  public get readonly(): boolean | '' {
-    return this._readonly;
-  }
-
   @Input()
-  public set readonly(value: boolean | '') {
-    this._readonly = coerceBooleanProperty(value);
-  }
-
-  @Input()
-  public set controlPath(value: string) {
-    this.controlPaths = [ value ];
-  }
-
-  @Input() public formId: string | null             = null;
-  @Input() public submitRemoteMethod: string | null = null;
-  @Input() public data: any | null                  = null;
-  private _readonly: boolean                        = false;
-
-  private _controlPaths?: string[];
-
-  public get controlPaths(): string[] {
-    return this._controlPaths || [ this.name ];
-  }
-
-  @Input()
-  public set controlPaths(value: string[]) {
-    this._controlPaths = value;
-  }
+  public data: any | null = null;
 
   @Input('rxapDataGridRowDef')
   @Required
