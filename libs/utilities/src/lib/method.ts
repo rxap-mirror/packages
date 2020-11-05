ee3f42ec5ec306d16b22d76a44c33c4c215b2200
Merzough Badry Münker <badry.muenker@digitaix.com>
2020-11-05 20:29:11 +0100

    feat: add ToMethod

--- libs/utilities/src/lib/method.ts
+++ libs/utilities/src/lib/method.ts
@@ -8,3 +8,7 @@ export interface Method<ReturnType = any, Parameter = any> {
 
   call(parameters?: Parameter): Promise<ReturnType> | ReturnType;
 }
+
+export function ToMethod<ReturnType = any, Parameter = any>(call: ((parameters?: Parameter) => Promise<ReturnType> | ReturnType)): Method<ReturnType, Parameter> {
+  return { call };
+}
