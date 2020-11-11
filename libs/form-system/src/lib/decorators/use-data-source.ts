d86eaa75657d89b5bd9cf6afd4789b26c82b3973
Merzough Badry Münker <badry.muenker@digitaix.com>
2020-11-11 14:12:09 +0100

    refactor: add type

--- libs/form-system/src/lib/decorators/use-data-source.ts
+++ libs/form-system/src/lib/decorators/use-data-source.ts
@@ -15,9 +15,9 @@ export interface UseDataSourceValue<Source = any, Target = any> {
   settings?: UseDataSourceSettings<Source, Target>,
 }
 
-export function UseDataSource(dataSource: Constructor<IdOrInstanceOrToken<BaseDataSource>>, name: string, settings?: UseDataSourceSettings) {
+export function UseDataSource<Data>(dataSource: Constructor<IdOrInstanceOrToken<BaseDataSource<Data>>>, name: string, settings?: UseDataSourceSettings) {
   return function(target: any, propertyKey: string) {
-    const value: UseDataSourceValue = { dataSource, settings };
+    const value: UseDataSourceValue<Data> = { dataSource, settings };
     setMetadataMapMap(propertyKey, name, value, FormSystemMetadataKeys.DATA_SOURCES, target);
   };
 }
