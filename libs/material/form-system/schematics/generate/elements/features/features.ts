c7c6934b53e31eda323388c954ebb13b7abdc128
Merzough Badry Münker <badry.muenker@digitaix.com>
2020-11-13 16:36:42 +0100

    feat: add FormFieldDefaultOptionsElement

--- libs/material/form-system/schematics/generate/elements/features/features.ts
+++ libs/material/form-system/schematics/generate/elements/features/features.ts
@@ -3,9 +3,11 @@ import { ParsedElement } from '@rxap/xml-parser';
 import { FooterControlsElement } from './footer-controls.element';
 import { WindowFooterControlsElement } from './window-footer-controls.element';
 import { FormFeatureElement } from './form-feature.element';
+import { FormFieldDefaultOptionsElement } from './form-field-default-options.element';
 
 export const Features: Array<Constructor<ParsedElement>> = [
   FooterControlsElement,
   WindowFooterControlsElement,
-  FormFeatureElement
+  FormFeatureElement,
+  FormFieldDefaultOptionsElement
 ];
