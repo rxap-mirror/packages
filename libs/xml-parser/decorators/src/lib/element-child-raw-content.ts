c2bd02236b3d5c91b05677b6ad2206e4c11ce10a
Merzough Badry Münker <badry.muenker@digitaix.com>
2020-11-16 16:22:03 +0100

    feat: add ElementChildRawContent

---
+++ libs/xml-parser/decorators/src/lib/element-child-raw-content.ts
@@ -0,0 +1,94 @@
+import {
+  XmlElementMetadata,
+  AddParserToMetadata
+} from './utilities';
+import {
+  deepMerge,
+  getMetadata,
+  dasherize
+} from '@rxap/utilities';
+import { Mixin } from '@rxap/mixin';
+import {
+  TagElementParserMixin,
+  TagElementOptions,
+  IsTagElementOptions
+} from './mixins/tag-element.parser.mixin';
+import {
+  TextContentElementParserMixin,
+  TextContentElementOptions
+} from './mixins/text-content-element.parser';
+import { ElementParser } from './element.parser';
+import {
+  ParsedElement,
+  XmlParserService,
+  RequiredProperty,
+  RxapElement
+} from '@rxap/xml-parser';
+
+export interface ElementChildRawContentOptions<Value>
+  extends TextContentElementOptions<Value>,
+          TagElementOptions {
+}
+
+export function AssertElementChildRawContentOptions(options: any): asserts options is ElementChildRawContentOptions<any> {
+  if (!IsTagElementOptions(options)) {
+    throw new Error('The object is not a ElementChildRawContentOptions. The required property "tag" is missing!');
+  }
+}
+
+export interface ElementChildRawContentParser<T extends ParsedElement>
+  extends TextContentElementParserMixin<string>,
+          TagElementParserMixin {}
+
+@Mixin(TextContentElementParserMixin, TagElementParserMixin)
+export class ElementChildRawContentParser<T extends ParsedElement>
+  implements ElementParser<T> {
+
+  constructor(
+    public readonly propertyKey: string,
+    public readonly options: ElementChildRawContentOptions<string>
+  ) {
+    this.parse = this.parse.bind(this);
+    Reflect.set(this.parse, 'propertyKey', propertyKey);
+  }
+
+  public parse(
+    xmlParser: XmlParserService,
+    element: RxapElement,
+    parsedElement: T
+  ): T {
+
+    if (element.hasChild(this.tag)) {
+      const rawValue = element.getChildRawContent(this.tag, this.defaultValue);
+      if (rawValue !== undefined) {
+        // @ts-ignore
+        parsedElement[ this.propertyKey ] = this.parseValue(rawValue);
+      }
+    } else if (this.required) {
+      throw new Error(`Element <${element.name}> child <${this.tag}> raw content is required!`);
+    }
+
+    return parsedElement;
+  }
+
+}
+
+export function ElementChildRawContent<Value>(optionsOrString?: Partial<ElementChildRawContentOptions<Value>> | string) {
+  return function(target: any, propertyKey: string) {
+    let options = optionsOrString === undefined ?
+                  { tag: dasherize(propertyKey) } :
+                  typeof optionsOrString === 'string' ? { tag: optionsOrString } : optionsOrString;
+    options     = deepMerge(options, getMetadata(XmlElementMetadata.OPTIONS, target, propertyKey) || {});
+    if (!options.tag) {
+      options.tag = dasherize(propertyKey);
+    }
+
+    AssertElementChildRawContentOptions(options);
+
+    const parser = new ElementChildRawContentParser(propertyKey, options);
+    AddParserToMetadata(parser, target);
+    if (options.required) {
+      RequiredProperty()(target, propertyKey);
+    }
+  };
+}
