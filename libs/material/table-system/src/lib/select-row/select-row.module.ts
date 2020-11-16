31e182027e45402eb6993aeb0e24eb4e6e8f097d
Merzough Badry Münker <badry.muenker@digitaix.com>
2020-11-16 20:23:05 +0100

    fix(schematics): add support for row service

--- libs/material/table-system/src/lib/select-row/select-row.module.ts
+++ libs/material/table-system/src/lib/select-row/select-row.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { SelectRowService } from './select-row.service';
 import { CheckboxCellComponentModule } from './checkbox-cell/checkbox-cell.component.module';
 import { CheckboxHeaderCellComponentModule } from './checkbox-header-cell/checkbox-header-cell.component.module';
 import { SelectedRowsDirective } from './selected-rows.directive';
@@ -11,7 +10,6 @@ import { SelectedRowsDirective } from './selected-rows.directive';
     CheckboxHeaderCellComponentModule,
     SelectedRowsDirective
   ],
-  providers:    [ SelectRowService ],
   declarations: [ SelectedRowsDirective ]
 })
 export class SelectRowModule {}
