5602bf64ba4626b3d03b60cf02a27e6bf77ee294
Merzough Badry Münker <badry.muenker@digitaix.com>
2020-11-16 11:21:41 +0100

    feat(schematics): add WindowTitleElement

---
+++ libs/material/form-system/schematics/generate/elements/features/window-title.element.ts
@@ -0,0 +1,31 @@
+import {
+  ElementDef,
+  ElementExtends,
+  ElementTextContent,
+  ElementRequired
+} from '@rxap/xml-parser/decorators';
+import { FormFeatureElement } from './form-feature.element';
+import {
+  NodeFactory,
+  ToValueContext,
+  AddNgModuleImport
+} from '@rxap-schematics/utilities';
+import { SourceFile } from 'ts-morph';
+
+@ElementExtends(FormFeatureElement)
+@ElementDef('window-title')
+export class WindowTitleElement extends FormFeatureElement {
+
+  @ElementTextContent()
+  @ElementRequired()
+  public title!: string;
+
+  public template(): string {
+    return NodeFactory('ng-template', 'rxapWindowTitle')('\n' + this.title + '\n');
+  }
+
+  public handleComponentModule({ project, sourceFile, options }: ToValueContext & { sourceFile: SourceFile }) {
+    AddNgModuleImport(sourceFile, 'WindowTitleDirectiveModule', '@rxap/window-system');
+  }
+
+}
