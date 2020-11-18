dc2042f0323551c5ed2ea600ec26332696e56da4
Merzough Badry Münker <badry.muenker@digitaix.com>
2020-11-18 12:10:20 +0100

    feat: add support for template path file search

--- libs/schematics/utilities/src/lib/parse-template.ts
+++ libs/schematics/utilities/src/lib/parse-template.ts
@@ -4,13 +4,54 @@ import {
 } from '@rxap/xml-parser';
 import { Constructor } from '@rxap/utilities';
 import {
-  chain,
-  Rule,
-  Tree
+  Tree,
+  DirEntry
 } from '@angular-devkit/schematics';
+import { join } from 'path';
+
+export function FindTemplate(template: string, host: Tree, baseDirEntry: DirEntry = host.getDir('templates')): string | null {
+
+  if (!host.exists(join(baseDirEntry.path, template))) {
+    if (host.exists(join(baseDirEntry.path, 'features'))) {
+
+      for (const dirPath of host.getDir(join(baseDirEntry.path, 'features')).subdirs) {
+        const templateFilePath = FindTemplate(template, host, host.getDir(join(baseDirEntry.path, 'features', dirPath)));
+        if (templateFilePath) {
+          return templateFilePath;
+        }
+      }
+
+    }
+
+    return null;
+  }
+
+  return join(baseDirEntry.path, template);
+
+}
 
 export function ParseTemplate<T extends ParsedElement>(host: Tree, template: string, ...elements: Array<Constructor<ParsedElement>>): T {
-  const templateFile = host.read(template)?.toString('utf-8');
+
+  let templateFile: string;
+
+  if (template.match(/\.xml$/)) {
+
+    let templateFilePath: string | null = template;
+    if (!host.exists(template)) {
+      templateFilePath = FindTemplate(template, host);
+    }
+
+    if (!templateFilePath) {
+      throw new Error(`Could not find template file for '${template}'`);
+    } else {
+      console.log(`Find template file path '${templateFilePath}' for '${template}'`);
+    }
+
+    templateFile = host.read(templateFilePath)!.toString('utf-8');
+
+  } else {
+    templateFile = template;
+  }
 
   const parser = new XmlParserService();
 
