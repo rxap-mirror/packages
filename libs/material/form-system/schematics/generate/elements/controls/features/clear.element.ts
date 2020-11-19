93bd70c68e213e7b7f77cf8f42ff82fe8a586c47
Merzough Badry Münker <badry.muenker@digitaix.com>
2020-11-19 11:43:34 +0100

    fix(schematics): add StopPropagation support

--- libs/material/form-system/schematics/generate/elements/controls/features/clear.element.ts
+++ libs/material/form-system/schematics/generate/elements/controls/features/clear.element.ts
@@ -1,7 +1,8 @@
 import { ControlFeatureElement } from './control-feature.element';
 import {
   ElementDef,
-  ElementExtends
+  ElementExtends,
+  ElementAttribute
 } from '@rxap/xml-parser/decorators';
 import {
   NodeFactory,
@@ -14,14 +15,24 @@ import { SourceFile } from 'ts-morph';
 @ElementDef('clear')
 export class ClearElement extends ControlFeatureElement {
 
+  @ElementAttribute()
+  public stopPropagation?: boolean;
+
   public template(): string {
-    return NodeFactory('button', 'matSuffix', 'rxapInputClearButton', 'mat-icon-button')([
+    const attributes: Array<string | (() => string)> = [ 'matSuffix', 'rxapInputClearButton', 'mat-icon-button' ];
+    if (this.stopPropagation) {
+      attributes.push('rxapStopPropagation');
+    }
+    return NodeFactory('button', ...attributes)([
       NodeFactory('mat-icon')('clear')
     ]);
   }
 
   public handleComponentModule({ project, sourceFile, options }: ToValueContext & { sourceFile: SourceFile }) {
     super.handleComponentModule({ project, sourceFile, options });
+    if (this.stopPropagation) {
+      AddNgModuleImport(sourceFile, 'StopPropagationDirectiveModule', '@rxap/directives');
+    }
     AddNgModuleImport(sourceFile, 'InputClearButtonDirectiveModule', '@rxap-material/form-system');
     AddNgModuleImport(sourceFile, 'MatIconModule', '@angular/material/icon');
     AddNgModuleImport(sourceFile, 'MatButtonModule', '@angular/material/button');
