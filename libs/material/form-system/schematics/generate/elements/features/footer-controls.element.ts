a19d5eee4f7329d90b42b288e923c3591370bace
Merzough Badry Münker <badry.muenker@digitaix.com>
2020-11-25 18:07:48 +0100

    fix: add support for form title

--- libs/material/form-system/schematics/generate/elements/features/footer-controls.element.ts
+++ libs/material/form-system/schematics/generate/elements/features/footer-controls.element.ts
@@ -8,13 +8,23 @@ import {
 import {
   NodeFactory,
   ToValueContext,
-  AddNgModuleImport
+  AddNgModuleImport,
+  AddComponentProvider
 } from '@rxap-schematics/utilities';
-import { SourceFile } from 'ts-morph';
+import {
+  SourceFile,
+  Writers
+} from 'ts-morph';
+import { strings } from '@angular-devkit/core';
+import { FormElement } from '../form.element';
+
+const { dasherize, classify, camelize, capitalize } = strings;
 
 @ElementExtends(FormFeatureElement)
 @ElementDef('footer-controls')
 export class FooterControlsElement extends FormFeatureElement {
+
+  public __parent!: FormElement;
 
   @ElementChildTextContent()
   public navigateAfterSubmit?: string;
@@ -33,6 +43,41 @@ export class FooterControlsElement extends FormFeatureElement {
     return NodeFactory('ng-template', 'rxapFooter')([
       NodeFactory('rxap-form-controls', ...attributes)()
     ]);
+  }
+
+  public handleComponent({ project, sourceFile, options }: ToValueContext & { sourceFile: SourceFile }) {
+    super.handleComponent({ project, sourceFile, options });
+    if (this.__parent.title) {
+      if (!sourceFile.getFunction('WindowOptionsFactory')) {
+        sourceFile.addFunction({
+          name:       'WindowOptionsFactory',
+          isExported: true,
+          statements: [
+            w => {
+              w.write('return ');
+              Writers.object({
+                title: `$localize\`:@@form.${dasherize(this.__parent.name)}.window.title:${this.__parent.title}\``
+              })(w);
+              w.write(';');
+            }
+          ]
+        });
+      }
+      AddComponentProvider(
+        sourceFile,
+        {
+          provide:    'RXAP_WINDOW_SETTINGS',
+          useFactory: 'WindowOptionsFactory',
+          deps:       []
+        },
+        [
+          {
+            namedImports:    [ 'RXAP_WINDOW_SETTINGS' ],
+            moduleSpecifier: '@rxap/window-system'
+          }
+        ]
+      );
+    }
   }
 
   public handleComponentModule({ project, sourceFile, options }: ToValueContext & { sourceFile: SourceFile }) {
