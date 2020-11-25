e8f2203e33d479fb1b73c21a6b12d094a8d19eb5
Merzough Badry Münker <badry.muenker@digitaix.com>
2020-11-25 17:28:00 +0100

    fix: add support for static array

--- libs/data-source/pagination/directives/src/lib/to-pagination.pipe.ts
+++ libs/data-source/pagination/directives/src/lib/to-pagination.pipe.ts
@@ -3,19 +3,35 @@ import {
   PipeTransform,
   NgModule
 } from '@angular/core';
-import { BaseDataSource } from '@rxap/data-source';
+import {
+  BaseDataSource,
+  staticDataSource,
+  observableDataSource
+} from '@rxap/data-source';
 import {
   PaginationDataSource,
   PaginatorLike
 } from '@rxap/data-source/pagination';
+import {
+  isObservable,
+  Observable
+} from 'rxjs';
 
 @Pipe({
   name: 'toPagination'
 })
 export class ToPaginationPipe implements PipeTransform {
 
-  transform<Data>(value: BaseDataSource<Data[]>, paginator: PaginatorLike): PaginationDataSource<Data> {
-    return new PaginationDataSource<Data>(value, paginator);
+  transform<Data>(value: BaseDataSource<Data[]> | Data[] | Observable<Data[]>, paginator: PaginatorLike | null = null): PaginationDataSource<Data> {
+    let dataSource: BaseDataSource<Data[]>;
+    if (Array.isArray(value)) {
+      dataSource = staticDataSource(value, { id: 'auto-to-pagination' });
+    } else if (isObservable(value)) {
+      dataSource = observableDataSource(value, { id: 'auto-to-pagination' });
+    } else {
+      dataSource = value;
+    }
+    return new PaginationDataSource<Data>(dataSource, paginator);
   }
 
 }
@@ -24,4 +40,4 @@ export class ToPaginationPipe implements PipeTransform {
   declarations: [ ToPaginationPipe ],
   exports:      [ ToPaginationPipe ]
 })
-export class ToHttpPaginationPipeModule {}
+export class ToPaginationPipeModule {}
