38a233a96382b3833d9b013ff0c2d6deb29a8367
Merzough Badry Münker <badry.muenker@digitaix.com>
2020-12-07 12:50:15 +0100

    feat(MethodElement): add support for custom methods

--- libs/schematics/utilities/src/lib/elements/methods/method.element.ts
+++ libs/schematics/utilities/src/lib/elements/methods/method.element.ts
@@ -7,6 +7,10 @@ import {
 } from '@rxap/xml-parser/decorators';
 import { SourceFile } from 'ts-morph';
 import { ToValueContext } from '../../to-value-context';
+import { join } from 'path';
+import { strings } from '@angular-devkit/core';
+
+const { dasherize, classify, camelize } = strings;
 
 @ElementDef('method')
 export class MethodElement implements ParsedElement<string> {
@@ -18,12 +22,42 @@ export class MethodElement implements ParsedElement<string> {
   @ElementAttribute()
   public from?: string;
 
-  public toValue({ sourceFile }: { sourceFile: SourceFile } & ToValueContext): string {
+  public toValue({ sourceFile, project }: { sourceFile: SourceFile } & ToValueContext): string {
     if (this.from) {
       sourceFile.addImportDeclaration({
         namedImports:    [ this.name ],
         moduleSpecifier: this.from
       });
+    } else {
+      const methodSourceFile = project.createSourceFile(join('/methods', `${dasherize(this.name)}.method.ts`));
+      methodSourceFile.addClass({
+        name:       `${classify(this.name)}Method`,
+        isExported: true,
+        decorators: [
+          {
+            name:      'Injectable',
+            arguments: []
+          }
+        ],
+        implements: [ 'Method' ],
+        methods:    [
+          {
+            name:       'call',
+            parameters: [ { name: 'parameters', type: 'any' } ],
+            returnType: 'any'
+          }
+        ]
+      });
+      methodSourceFile.addImportDeclarations([
+        {
+          namedImports:    [ 'Injectable' ],
+          moduleSpecifier: '@angular/core'
+        },
+        {
+          namedImports:    [ 'Method' ],
+          moduleSpecifier: '@rxap/utilities'
+        }
+      ]);
     }
 
     return this.name;
