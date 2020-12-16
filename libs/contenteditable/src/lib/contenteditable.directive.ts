d7ef6f20e35fab86f0cb6598eae8b2b561d1f5d7
Merzough Badry Münker <badry.muenker@digitaix.com>
2020-12-16 11:50:38 +0100

    fix: streamline content editable method support

--- libs/contenteditable/src/lib/contenteditable.directive.ts
+++ libs/contenteditable/src/lib/contenteditable.directive.ts
@@ -21,15 +21,23 @@ export class ContenteditableDirective {
   @HostBinding('attr.contenteditable')
   public contenteditable = true;
 
+  // allow empty string
   @Input('rxapContenteditable')
-  public method?: Method<any, string | null>;
+  public set method(value: Method<any, string | null> | '') {
+    if (value) {
+      this._method = value;
+    }
+  }
+
+  private _method?: Method<any, string | null>;
 
   @Output()
   public change = new EventEmitter<string | null>();
 
   @Input()
   public set disabled(value: boolean | '') {
-    this._disabled = coerceBoolean(value);
+    this._disabled       = coerceBoolean(value);
+    this.contenteditable = !this._disabled;
   }
 
   private _disabled = false;
@@ -44,7 +52,7 @@ export class ContenteditableDirective {
   public onInput($event: any) {
     const value = ($event.target as HTMLElement).textContent;
     if (!this._disabled) {
-      this.method?.call(value);
+      this._method?.call(value);
       this.change.emit(value);
     }
   }
