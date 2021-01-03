65038fc97ff735b2d1e3c6f0a157128501080d31
Merzough Badry Münker <badry.muenker@digitaix.com>
2021-01-04 00:55:22 +0100

    chore: add major-release guide and script

---
+++ tools/major-release.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+  echo "The new major release version is not defined"
+  exit 1
+else
+  if [ -z "$GL_TOKEN" ]
+  then
+    echo "The gitlab api key is not defined"
+    exit 1
+  else
+    lerna version $1 --create-release gitlab --force-publish
+    node tools/update-dependencies.js
+    git commit -am "chore: update project local peer dependencies"
+    lerna publish from-git
+  fi
+fi
+
+
