5a4e4e1601c3343d33f43d4eeb7e36280b702e68
Merzough Badry Münker <badry.muenker@digitaix.com>
2021-01-03 22:51:28 +0100

    chore: allow any scope for packages

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,21 +1,4 @@
-const Project = require('@lerna/project');
-
 module.exports = {
-  extends: [
-    '@commitlint/config-conventional'
-  ],
-  rules: {
-    'scope-enum': async () => {
-
-      const packages = await new Project(process.cwd()).getPackages();
-
-      return [
-        2,
-        'always',
-        packages.map(p => p.name.replace(/@rxap[-\/]/, '').replace(/\//g, '-'))
-      ];
-
-    }
-  },
+  extends: ['@commitlint/config-conventional'],
   ignores: [message => message.toLowerCase().startsWith('wip')]
 };
