0449b186c28cc84c83c64af58ebe5eea731ae0ef
Merzough Münker <mmuenker@digitaix.com>
2021-01-20 16:22:59 +0100

    fix: refactor change detection

--- libs/window-system/src/lib/default-window/default-window.component.ts
+++ libs/window-system/src/lib/default-window/default-window.component.ts
@@ -7,6 +7,6 @@ import {
   selector:        'rxap-default-window',
   templateUrl:     './default-window.component.html',
   styleUrls:       [ './default-window.component.scss' ],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.Default
 })
 export class DefaultWindowComponent {}
