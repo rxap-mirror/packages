9267ea12a38063ad069da091a293773e5f57829e
Merzough Münker <mmuenker@digitaix.com>
2021-01-29 09:33:07 +0100

    feat(window-service): add support for default window settings

--- libs/window-system/src/lib/tokens.ts
+++ libs/window-system/src/lib/tokens.ts
@@ -10,3 +10,4 @@ export const RXAP_WINDOW_CONTEXT           = new InjectionToken<WindowContext<an
 export const RXAP_WINDOW_CONTAINER_CONTEXT = new InjectionToken<WindowContainerContext<any>>('rxap/core/window/WINDOW_CONTAINER_CONTEXT');
 export const RXAP_WINDOW_REF               = new InjectionToken<WindowRef>('rxap/window-system/WINDOW_REF');
 export const RXAP_WINDOW_SETTINGS          = new InjectionToken<WindowSettings>('rxap/window-system/window-settings');
+export const RXAP_WINDOW_DEFAULT_SETTINGS  = new InjectionToken<WindowSettings>('rxap/window-system/default-window-settings');
