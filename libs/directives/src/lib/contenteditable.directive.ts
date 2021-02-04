376a8b6ed627a86ca6e75ebd95d9cbbeb589c41c
Merzough Münker <mmuenker@digitaix.com>
2021-02-04 14:27:22 +0100

    feat: add contenteditable directives

---
+++ libs/directives/src/lib/contenteditable.directive.ts
@@ -0,0 +1,62 @@
+import {
+  Directive,
+  Input,
+  HostListener,
+  NgModule,
+  HostBinding,
+  EventEmitter,
+  Output
+} from '@angular/core';
+import {
+  DebounceCall,
+  Method
+} from '@rxap/utilities';
+
+export interface ContenteditableEvent {
+  value: string;
+  parameters?: any;
+}
+
+@Directive({
+  selector: '[rxapContenteditable]'
+})
+export class ContenteditableDirective {
+
+  @HostBinding('attr.contenteditable')
+  public contenteditable = true;
+
+  @Input('rxapContenteditable')
+  public method?: Method<any, ContenteditableEvent>;
+
+  @Output('rxapContenteditable')
+  public change = new EventEmitter<ContenteditableEvent>();
+
+  @Input()
+  public parameters?: any;
+
+  @HostListener('click', [ '$event' ])
+  public onClick($event: Event) {
+    $event.stopPropagation();
+  }
+
+  @HostListener('input', [ '$event' ])
+  @DebounceCall(1000)
+  public async onInput($event: any) {
+    const value = ($event.target as HTMLElement).textContent;
+    if (value && value.length > 3) {
+      const event: ContenteditableEvent = {
+        value,
+        parameters: this.parameters
+      };
+      this.change.emit(event);
+      await this.method?.call(event);
+    }
+  }
+
+}
+
+@NgModule({
+  declarations: [ ContenteditableDirective ],
+  exports:      [ ContenteditableDirective ]
+})
+export class ContenteditableDirectiveModule {}
