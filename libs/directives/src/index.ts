4f4b05f115b0c0066eb35ce00d31cbb914a77841
Merzough Münker <mmuenker@digitaix.com>
2021-02-04 14:27:22 +0100

    feat: add contenteditable directives

--- libs/directives/src/index.ts
+++ libs/directives/src/index.ts
@@ -5,3 +5,4 @@ export * from './lib/stop-propagation.directive';
 export * from './lib/share-button.directive';
 export * from './lib/full-width.directive';
 export * from './lib/avatar-background-image.directive';
+export * from './lib/contenteditable.directive';
