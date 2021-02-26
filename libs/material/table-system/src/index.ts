6ee41debeaf9a1725df2018cf5b2368f4cceb916
Merzough Münker <mmuenker@digitaix.com>
2021-02-26 16:04:49 +0100

    fix: remove table create button

--- libs/material/table-system/src/index.ts
+++ libs/material/table-system/src/index.ts
@@ -38,9 +38,8 @@ export * from './lib/table-column-menu/table-column-option/table-column-option.c
 
 // region table create button
 
-export * from './lib/table-create-button/table-create-button.component';
 export * from './lib/table-create-button/table-create-button.directive';
-export * from './lib/table-create-button/table-create-button.component.module';
+export * from './lib/table-create-button/tokens';
 
 // endregion
 
