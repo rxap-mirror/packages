426f26e2e21b419595489577e759c39f37e5ff49
Merzough Münker <mmuenker@digitaix.com>
2021-03-15 11:50:51 +0100

    fix: add missing default values

--- libs/utilities/src/lib/method.ts
+++ libs/utilities/src/lib/method.ts
@@ -3,6 +3,9 @@ import { CounterSubject } from './counter.subject';
 
 export interface Method<ReturnType = any, Parameter = any> {
   executed$?: Subject<ReturnType>;
+  destroyed$?: Subject<void>;
+  initialised$?: Subject<void>;
+  interceptors?: Set<Subject<any>>;
   executionsInProgress$?: CounterSubject;
   metadata?: any;
 
@@ -13,5 +16,13 @@ export function ToMethod<ReturnType = any, Parameter = any>(call: ((
   parameters?: Parameter,
   ...args: any[]
 ) => Promise<ReturnType> | ReturnType)): Method<ReturnType, Parameter> {
-  return { call };
+  return {
+    call,
+    destroyed$: new Subject<void>(),
+    initialised$: new Subject<void>(),
+    interceptors: new Set<Subject<any>>(),
+    executed$: new Subject<ReturnType>(),
+    executionsInProgress$: new CounterSubject(),
+    metadata: {},
+  };
 }
