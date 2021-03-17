e154748584f58145bc1f798094688181d6f49a9d
Merzough Münker <mmuenker@digitaix.com>
2021-03-17 14:10:45 +0100

    fix: add missing feature import

--- libs/material/form-system/schematics/generate/elements/component/features/features.ts
+++ libs/material/form-system/schematics/generate/elements/component/features/features.ts
@@ -1,4 +1,7 @@
 import { Constructor } from '@rxap/utilities';
 import { ParsedElement } from '@rxap/xml-parser';
+import { TableComponentFeatureElement } from './table-component-feature.element';
 
-export const ComponentFeatures: Array<Constructor<ParsedElement>> = [];
+export const ComponentFeatures: Array<Constructor<ParsedElement>> = [
+  TableComponentFeatureElement
+];
