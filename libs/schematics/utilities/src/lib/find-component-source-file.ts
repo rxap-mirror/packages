0dd84cdb28c9c40d8b4c6dc780b02936da931ff9
Merzough Münker <mmuenker@digitaix.com>
2021-03-17 14:27:22 +0100

    fix: use coerce suffix function

--- libs/schematics/utilities/src/lib/find-component-source-file.ts
+++ libs/schematics/utilities/src/lib/find-component-source-file.ts
@@ -2,8 +2,11 @@ import {
   Project,
   SourceFile
 } from 'ts-morph';
-import { classify } from '@rxap/utilities';
+import {
+  classify,
+  CoerceSuffix
+} from '@rxap/utilities';
 
 export function FindComponentSourceFile(name: string, project: Project): SourceFile {
-  return project.getSourceFileOrThrow(sourceFile => !!sourceFile.getClass(classify(name) + 'Component'));
+  return project.getSourceFileOrThrow(sourceFile => !!sourceFile.getClass(CoerceSuffix(classify(name), 'Component')));
 }
