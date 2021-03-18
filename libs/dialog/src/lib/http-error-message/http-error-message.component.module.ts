adb622ff5206141ccd1e063c7c113021a4c9e6e4
Merzough Münker <mmuenker@digitaix.com>
2021-03-18 15:16:27 +0100

    feat: add http error message dialog

---
+++ libs/dialog/src/lib/http-error-message/http-error-message.component.module.ts
@@ -0,0 +1,45 @@
+import {
+  NgModule,
+  ModuleWithProviders
+} from '@angular/core';
+import { HttpErrorMessageComponent } from './http-error-message.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpErrorMessageInterceptor } from './http-error-message.interceptor';
+import { HttpErrorMessageService } from './http-error-message.service';
+import { DataGridReadonlyComponentModule } from '@rxap/data-grid';
+import {
+  JsonViewerModule,
+  CopyToClipboardModule
+} from '@rxap/components';
+
+@NgModule({
+  declarations:    [ HttpErrorMessageComponent ],
+  imports:         [
+    MatDialogModule,
+    MatButtonModule,
+    DataGridReadonlyComponentModule,
+    CommonModule,
+    JsonViewerModule,
+    CopyToClipboardModule
+  ],
+  entryComponents: [ HttpErrorMessageComponent ],
+  exports:         [ HttpErrorMessageComponent, MatDialogModule ]
+})
+export class HttpErrorMessageModule {
+  public static forRoot(): ModuleWithProviders<HttpErrorMessageModule> {
+    return {
+      ngModule:  HttpErrorMessageModule,
+      providers: [
+        HttpErrorMessageService,
+        {
+          provide:  HTTP_INTERCEPTORS,
+          useClass: HttpErrorMessageInterceptor,
+          multi:    true
+        }
+      ]
+    };
+  }
+}
