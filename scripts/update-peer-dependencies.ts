ace447f1c7f7faffbf9226fa8704a1916246d31b
Merzough Münker <mmuenker@digitaix.com>
2021-03-19 16:39:28 +0100

    chore: add peer dependencies updater

---
+++ scripts/update-peer-dependencies.ts
@@ -0,0 +1,201 @@
+import { createProjectGraph } from '@nrwl/workspace/src/core/project-graph';
+import { ProjectGraph } from '@nrwl/workspace/src/core/project-graph/project-graph-models';
+import { join } from 'path';
+import {
+  readFileSync,
+  existsSync,
+  writeFileSync
+} from 'fs';
+
+const projectGraph: ProjectGraph = createProjectGraph();
+
+const ExcludeNpmDependencies = ([ name, _ ]: [ string, any ]) => !name.match(/^npm:/);
+
+const blackListNpmPeerDependencies: RegExp[] = [
+  /npm:@angular-devkit\//,
+  /npm:ramda/,
+  /npm:uuid/,
+  /npm:jest-preset-angular/,
+  /npm:ts-morph/,
+  /npm:@storybook/,
+  /npm:across-tabs/,
+  /npm:openapi-types/,
+  /npm:ajv-oai/,
+  /npm:@apidevtools\/json-schema-ref-parser/,
+  /@rxap\/json-schema-to-typescript/,
+  /npm:xmldom/,
+  /npm:handlebars/,
+  /npm:axios/
+];
+
+function FlattenDependencies(knownDependencies: string[]): string[] {
+
+  const dependencies: string[] = knownDependencies.slice();
+
+  for (const dep of knownDependencies) {
+    dependencies.push(
+      ...FlattenDependencies(projectGraph.dependencies[ dep ].map(dependency => dependency.target))
+    );
+  }
+
+  return dependencies
+    .filter((n, i, s) => s.indexOf(n) === i);
+
+}
+
+function ParsePackageJson(filePath: string): Record<string, any> {
+  return JSON.parse(readFileSync(join(__dirname, '..', filePath)).toString('utf-8'));
+}
+
+function GetProjectPackageJson(projectName: string): Record<string, any> {
+  const packageJsonFile = join(projectGraph.nodes[ projectName ].data.root, 'package.json');
+
+  if (!existsSync(packageJsonFile)) {
+    throw new Error(`Could not find the package.json file for the project '${projectName}'`);
+  }
+
+  return ParsePackageJson(packageJsonFile);
+}
+
+function LoadRootPackageVersionMap() {
+  const map = new Map<string, string>();
+
+  const rootPackageJson = ParsePackageJson('package.json');
+
+  for (const [ name, version ] of Object.entries(rootPackageJson.dependencies)) {
+    map.set(name, (version as string).replace(/^[~^]/, ''));
+  }
+
+  for (const [ name, version ] of Object.entries(rootPackageJson.devDependencies)) {
+    map.set(name, (version as string).replace(/^[~^]/, ''));
+  }
+
+  return map;
+}
+
+const rootPackageVersionMap = LoadRootPackageVersionMap();
+
+const dependencyVersionMap = new Map<string, string>();
+
+function GetDependencyVersion(dependencyName: string): string {
+
+  if (!dependencyVersionMap.has(dependencyName)) {
+
+    const match = dependencyName.match(/npm:(.*)/);
+    if (match) {
+
+      const packageName = match[ 1 ];
+
+      if (!rootPackageVersionMap.has(packageName)) {
+        throw new Error(`Could not find root package version for '${packageName}'`);
+      }
+
+      const version = '^' + rootPackageVersionMap.get(packageName)!;
+      dependencyVersionMap.set(dependencyName, version);
+    } else {
+      const packageJson = GetProjectPackageJson(dependencyName);
+      const version     = '^' + packageJson.version;
+      dependencyVersionMap.set(dependencyName, version);
+    }
+
+  }
+
+  return dependencyVersionMap.get(dependencyName)!;
+
+}
+
+function GetPackageName(dependencyName: string): string {
+  const match = dependencyName.match(/npm:(.*)/);
+  if (match) {
+    return match[ 1 ];
+  }
+
+  const packageJson = GetProjectPackageJson(dependencyName);
+
+  return packageJson.name;
+}
+
+function WriteProjectPackageJson(projectName: string, packageJson: Record<string, any>): void {
+  const packageJsonFile = join(projectGraph.nodes[ projectName ].data.root, 'package.json');
+
+  if (!existsSync(packageJsonFile)) {
+    throw new Error(`Could not find the package.json file for the project '${projectName}'`);
+  }
+  writeFileSync(packageJsonFile, JSON.stringify(packageJson, undefined, 2));
+}
+
+function AddDefaultPackageJsonProperties(packageJson: Record<string, any>): void {
+
+  if (!packageJson.license) {
+    packageJson.license = 'MIT';
+  }
+
+  if (!packageJson.keywords) {
+    packageJson.keywords = packageJson.keywords ?? [];
+    if (packageJson.name.match(/@rxap/)) {
+      packageJson.keywords.push('rxap');
+    }
+    if (Object.keys(packageJson.peerDependencies).some(dep => dep.match(/@angular/))) {
+      packageJson.keywords.push('angular');
+    }
+    if (packageJson.name.match(/@rxap\/schematics/)) {
+      packageJson.keywords.push('schematics');
+    }
+    if (packageJson.name.match(/@rxap\/material/)) {
+      packageJson.keywords.push('material');
+    }
+    if (packageJson.name.match(/@rxap\/plugin/)) {
+      packageJson.keywords.push('plugin');
+      packageJson.keywords.push('nrwl');
+    }
+    if (packageJson.name.match(/@rxap\/aws/)) {
+      packageJson.keywords.push('aws');
+    }
+    if (packageJson.name.match(/@rxap\/gcp/)) {
+      packageJson.keywords.push('gcp');
+    }
+    if (packageJson.name.match(/@rxap\/amplify/)) {
+      packageJson.keywords.push('amplify');
+    }
+    if (packageJson.name.match(/@rxap\/firebase/)) {
+      packageJson.keywords.push('firebase');
+    }
+  }
+
+  packageJson.repository = 'git@gitlab.com:rxap/packages.git';
+  packageJson.homepage   = 'https://gitlab.com/rxap/packages';
+  packageJson.author     = 'Merzough Münker';
+  packageJson.private    = false;
+  packageJson.bugs       = {
+    url:   'https://gitlab.com/rxap/packages/-/issues',
+    email: 'incoming+rxap-packages-14898188-issue-@incoming.gitlab.com'
+  };
+
+}
+
+for (const [ name, dependencies ] of
+  Object.entries(projectGraph.dependencies).filter(ExcludeNpmDependencies).map(([ _, dList ]) => [ _, dList.map(d => d.target) ] as [ string, string[] ])) {
+  const flattenDependencies = FlattenDependencies(dependencies).sort((a, b) => a.localeCompare(b));
+
+  const peerDependencies          = flattenDependencies.filter(dependency => !dependency.match(/^npm:/) ||
+                                                                             !blackListNpmPeerDependencies.some(regex => dependency.match(regex)));
+  const blackListPeerDependencies = flattenDependencies.filter(dependency => blackListNpmPeerDependencies.some(regex => dependency.match(regex)));
+
+  if (projectGraph.nodes[ name ].data.root.match(/apps\//)) {
+    continue;
+  }
+
+  const packageJson = GetProjectPackageJson(name);
+
+  packageJson.peerDependencies = peerDependencies.map(peerDependency => ({ [ GetPackageName(peerDependency) ]: GetDependencyVersion(peerDependency) }))
+                                                 .reduce((map, item) => ({ ...map, ...item }), {});
+
+  console.log(`Update project ${name}:`);
+  console.log('peerDependencies:', packageJson.peerDependencies);
+  console.log('blackList', blackListPeerDependencies);
+
+  AddDefaultPackageJsonProperties(packageJson);
+
+  WriteProjectPackageJson(name, packageJson);
+
+}
