c23273e8ada0a24ed415437a9a763e17027a6cd6
Merzough Münker <mmuenker@digitaix.com>
2021-03-24 12:34:30 +0100

    feat: add expandable row generator

---
+++ libs/schematics/table/schematics/generate/elements/columns/component-column.element.ts
@@ -0,0 +1,80 @@
+import {
+  ElementAttribute,
+  ElementDef,
+  ElementExtends
+} from '@rxap/xml-parser/decorators';
+import {
+  chain,
+  externalSchematic,
+  Rule,
+  noop
+} from '@angular-devkit/schematics';
+import {
+  AddNgModuleImport,
+  ToValueContext
+} from '@rxap/schematics-utilities';
+import { strings } from '@angular-devkit/core';
+import { join } from 'path';
+import { SourceFile } from 'ts-morph';
+import { ColumnElement } from './column.element';
+import { GenerateSchema } from '../../schema';
+
+const { dasherize, classify, camelize, capitalize } = strings;
+
+@ElementExtends(ColumnElement)
+@ElementDef('component-column')
+export class ComponentColumnElement extends ColumnElement {
+
+  @ElementAttribute()
+  public withElement: boolean = false;
+
+  public handleComponentModule({ sourceFile, project, options }: ToValueContext & { sourceFile: SourceFile }) {
+    super.handleComponentModule({ sourceFile, project, options });
+    AddNgModuleImport(
+      sourceFile,
+      `${classify(this.name)}CellComponentModule`,
+      `./${dasherize(this.name)}-cell/${dasherize(this.name)}-cell.component.module`
+    );
+  }
+
+  public template(): string {
+    return `
+    <th mat-header-cell
+    *matHeaderCellDef
+    ${this.__parent.hasFeature('sort') ? 'mat-sort-header' : ''}
+    i18n="${this.i18nTitle}">
+    ${capitalize(this.name)}
+    </th>
+    <td mat-cell
+    [rxap-${dasherize(this.name)}-cell]="element${this.valueAccessor}"
+    ${this.withElement ? '[element]="element"' : ''}
+    *matCellDef="let element"></td>
+    `;
+  }
+
+  public toValue({ project, options }: ToValueContext<GenerateSchema>): Rule {
+    const rules: Rule[] = [ super.toValue({ project, options }) ];
+
+    rules.push(tree => {
+      const path = options.path?.replace(/^\//, '') ?? '';
+      if (!tree.exists(join(path, dasherize(this.name) + '-cell', dasherize(this.name) + '-cell.component.module.ts'))) {
+        return externalSchematic(
+          '@rxap/schematics',
+          'component-module',
+          {
+            name:     dasherize(this.name) + '-cell',
+            project:  options.project,
+            path:     options.path?.replace(/^\//, ''),
+            prefix:   'rxap',
+            selector: `td[rxap-${dasherize(this.name)}-cell]`
+          }
+        );
+      } else {
+        return noop();
+      }
+    });
+
+    return chain(rules);
+  }
+
+}
