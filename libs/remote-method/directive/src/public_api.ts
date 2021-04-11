bce442f8a165798a99bf456249a22b91d8a31b5a
Merzough Münker <mmuenker@digitaix.com>
2021-04-11 12:14:39 +0200

    fix(directive): add missing export

--- libs/remote-method/directive/src/public_api.ts
+++ libs/remote-method/directive/src/public_api.ts
@@ -2,3 +2,4 @@ export * from './lib/remote-method.directive';
 export * from './lib/remote-method-template-collection.directive';
 export * from './lib/remote-method-template.directive';
 export * from './lib/error';
+export * from './lib/tokens';
