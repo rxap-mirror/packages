377974598dc28e3a921237f8f3d8e98898bc1743
Merzough Münker <mmuenker@digitaix.com>
2021-04-21 13:37:31 +0200

    fix(single-sign-on): add missing module export

--- libs/oauth/single-sign-on/src/lib/o-auth-single-sign-on.module.ts
+++ libs/oauth/single-sign-on/src/lib/o-auth-single-sign-on.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { ContinueComponentModule } from './continue/continue.component.module';
 import { LoadingComponentModule } from './loading/loading.component.module';
+import { ContainerComponentModule } from './container/container.component.module';
 
 @NgModule({
   exports: [
     ContinueComponentModule,
-    LoadingComponentModule
+    LoadingComponentModule,
+    ContainerComponentModule
   ]
 })
 export class OAuthSingleSignOnModule {}
