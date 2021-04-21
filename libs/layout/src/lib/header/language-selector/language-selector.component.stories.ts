7c57f39cc7ff2adc1f0c81c213d7ebb7ce179603
Merzough Münker <mmuenker@digitaix.com>
2021-04-21 17:48:53 +0200

    feat: add language selector

---
+++ libs/layout/src/lib/header/language-selector/language-selector.component.stories.ts
@@ -0,0 +1,22 @@
+import {
+  moduleMetadata,
+  addDecorator
+} from '@storybook/angular';
+import { LanguageSelectorComponentModule } from './language-selector.component.module';
+import { LanguageSelectorComponent } from './language-selector.component';
+
+addDecorator(moduleMetadata({
+  imports: [
+    LanguageSelectorComponentModule
+  ]
+}));
+
+export default {
+  title:     'LanguageSelectorComponent',
+  component: LanguageSelectorComponent
+};
+
+export const basic = () => ({
+  component: LanguageSelectorComponent,
+  props:     {}
+});
