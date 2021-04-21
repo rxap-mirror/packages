6a93c7ae969c86cd94f0fc1d6b679fc7919f579e
Merzough Münker <mmuenker@digitaix.com>
2021-04-21 15:55:56 +0200

    feat: add UpdateEnvironment function

--- libs/environment/src/index.ts
+++ libs/environment/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/environment/environment.component.module';
 export * from './lib/environment/environment.component';
 export * from './lib/environment/tokens';
 export * from './lib/environment';
+export * from './lib/update-environment';
