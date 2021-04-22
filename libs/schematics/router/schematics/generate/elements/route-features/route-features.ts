25a75b18011b3cb6314111f66c7bb8fbbebd4302
Merzough Münker <mmuenker@digitaix.com>
2021-04-22 17:28:58 +0200

    feat: add sso and empty route support

--- libs/schematics/router/schematics/generate/elements/route-features/route-features.ts
+++ libs/schematics/router/schematics/generate/elements/route-features/route-features.ts
@@ -4,10 +4,14 @@ import { AuthGuardElement } from './auth-guard.element';
 import { ComponentGenerateSchematicElement } from './component-generate-schematic.element';
 import { FormComponentGenerateSchematicElement } from './form-component-generate-schematic.element';
 import { TableGenerateSchematicElement } from './table-generate-schematic.element';
+import { EmptyRouterOutletElement } from './empty-router-outlet.element';
+import { SsoGuardElement } from './sso-guard.element';
 
 export const RouteFeatures: Array<Constructor<ParsedElement>> = [
   AuthGuardElement,
   ComponentGenerateSchematicElement,
   FormComponentGenerateSchematicElement,
-  TableGenerateSchematicElement
+  TableGenerateSchematicElement,
+  EmptyRouterOutletElement,
+  SsoGuardElement,
 ];
