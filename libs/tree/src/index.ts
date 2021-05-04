90d0f365c4a49811c266fef84cb862b2f832ab76
Merzough Münker <mmuenker@digitaix.com>
2021-05-04 15:36:11 +0200

    fix: add missing export

--- libs/tree/src/index.ts
+++ libs/tree/src/index.ts
@@ -1,1 +1,5 @@
 export * from './lib/tree.data-source';
+export * from './lib/tree-content.directive';
+export * from './lib/tree.component.module';
+export * from './lib/tree.component';
+export * from './lib/tokens';
