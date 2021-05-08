48e93ee668540ed0fd38db21c2b1498fb5586f70
Merzough Münker <mmuenker@digitaix.com>
2021-05-08 16:59:10 +0200

    feat(fix-missing-imports): add base path support

--- libs/schematics/ts-morph/src/lib/fix-missing-imports.ts
+++ libs/schematics/ts-morph/src/lib/fix-missing-imports.ts
@@ -1,6 +1,7 @@
 import {
   Rule,
-  Tree
+  Tree,
+  DirEntry
 } from '@angular-devkit/schematics';
 import {
   Project,
@@ -8,7 +9,12 @@ import {
   QuoteKind
 } from 'ts-morph';
 
-export function FixMissingImports(): Rule {
+/**
+ *
+ * @param basePath (optional) if definition all files from the base path are included and not only the changed or created files
+ * @constructor
+ */
+export function FixMissingImports(basePath?: string): Rule {
   return (tree: Tree) => {
     const project = new Project({
       useInMemoryFileSystem: true,
@@ -20,16 +26,31 @@ export function FixMissingImports(): Rule {
     });
     console.debug('fix missing imports for ts files');
 
-    for (const action of tree.actions.slice()) {
-      switch (action.kind) {
+    function AddFiles(dir: DirEntry) {
+      for (const file of dir.subfiles) {
+        if (file.match(/\.ts$/) && !file.match(/\.spec\.ts$/)) {
+          project.createSourceFile(file, tree.get(file)!.content.toString('utf-8'));
+        }
+      }
+      for (const subDir of dir.subdirs) {
+        AddFiles(tree.getDir(subDir));
+      }
+    }
 
-        case 'c':
-        case 'o':
-          if (action.path.match(/\.ts$/)) {
-            project.createSourceFile(action.path, action.content.toString('utf-8'), { overwrite: true });
-          }
-          break;
+    if (basePath) {
+      AddFiles(tree.getDir(basePath));
+    } else {
+      for (const action of tree.actions.slice()) {
+        switch (action.kind) {
 
+          case 'c':
+          case 'o':
+            if (action.path.match(/\.ts$/)) {
+              project.createSourceFile(action.path, action.content.toString('utf-8'), { overwrite: true });
+            }
+            break;
+
+        }
       }
     }
 
