655fa82be8be27ff1395272d2bd24544b071fac5
Merzough Münker <mmuenker@digitaix.com>
2021-05-18 11:25:51 +0200

    feat: add the RXAP_WINDOW_DATA InjectionToken

--- libs/window-system/src/lib/tokens.ts
+++ libs/window-system/src/lib/tokens.ts
@@ -7,6 +7,7 @@ import type { WindowRef } from './window-ref';
 import { WindowSettings } from './window-config';
 
 export const RXAP_WINDOW_CONTEXT           = new InjectionToken<WindowContext<any>>('rxap/core/window/WINDOW_CONTEXT');
+export const RXAP_WINDOW_DATA              = new InjectionToken<any>('rxap/core/window/WINDOW_DATA');
 export const RXAP_WINDOW_CONTAINER_CONTEXT = new InjectionToken<WindowContainerContext<any>>('rxap/core/window/WINDOW_CONTAINER_CONTEXT');
 export const RXAP_WINDOW_REF               = new InjectionToken<WindowRef>('rxap/window-system/WINDOW_REF');
 export const RXAP_WINDOW_SETTINGS          = new InjectionToken<WindowSettings>('rxap/window-system/window-settings');
