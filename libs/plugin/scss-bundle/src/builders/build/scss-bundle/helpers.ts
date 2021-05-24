be94e48f9b8a854f72e98715499b49acebd2a3ce
Merzough Münker <mmuenker@digitaix.com>
2021-05-24 20:52:29 +0200

    feat: add support for @use imports

---
+++ libs/plugin/scss-bundle/src/builders/build/scss-bundle/helpers.ts
@@ -0,0 +1,10 @@
+export function matchAll(text: string, regex: RegExp): RegExpExecArray[] {
+  const matches: RegExpExecArray[] = [];
+
+  let match: RegExpExecArray | null;
+  while ((match = regex.exec(text))) {
+    matches.push(match);
+  }
+
+  return matches;
+}
