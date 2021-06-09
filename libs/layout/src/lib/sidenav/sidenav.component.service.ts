cc11b3a17c795d0eb383cbe3e674470f0c4fc6a4
Merzough Münker <mmuenker@digitaix.com>
2021-06-09 12:35:47 +0200

    feat: set default sidenav collapsed state from config

--- libs/layout/src/lib/sidenav/sidenav.component.service.ts
+++ libs/layout/src/lib/sidenav/sidenav.component.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { ConfigService } from '@rxap/config';
 
 @Injectable({ providedIn: 'root' })
 export class SidenavComponentService {
 
 
   public readonly collapsed$ = new BehaviorSubject(false);
+
+  constructor(private readonly config: ConfigService<any>) {
+    this.collapsed$.next(this.config.get('navigation.collapsed', false));
+  }
 
   public toggleNavigationCollapse(): void {
     this.collapsed$.next(!this.collapsed$.value);
