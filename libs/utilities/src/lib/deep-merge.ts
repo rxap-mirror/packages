0911548dd1e6ae7cea1e2ac472626798c3fe1630
Merzough Münker <mmuenker@digitaix.com>
2021-06-10 09:50:40 +0200

    fix: remove ramda dependency

--- libs/utilities/src/lib/deep-merge.ts
+++ libs/utilities/src/lib/deep-merge.ts
@@ -1,8 +1,59 @@
-import {
-  isObject,
-  RecursivePartial
-} from './helpers';
-import { mergeDeepRight } from 'ramda';
+import { isObject, RecursivePartial } from './helpers';
+
+function _has(prop: string, obj: any) {
+  return Object.prototype.hasOwnProperty.call(obj, prop);
+}
+
+function mergeWithKey(
+  fn: (K: string, lk: string, rk: string) => any,
+  l: string[],
+  r: string[]
+) {
+  const result: Record<any, any> = {};
+  let k;
+
+  for (k in l) {
+    if (_has(k, l)) {
+      result[k] = _has(k, r) ? fn(k, l[k], r[k]) : l[k];
+    }
+  }
+
+  for (k in r) {
+    if (_has(k, r) && !_has(k, result)) {
+      result[k] = r[k];
+    }
+  }
+
+  return result;
+}
+
+function mergeDeepWithKey(
+  fn: (K: string, lk: string, rk: string) => any,
+  lObj: any,
+  rObj: any
+): any {
+  return mergeWithKey(
+    function (k, lVal, rVal) {
+      if (isObject(lVal) && isObject(rVal)) {
+        return mergeDeepWithKey(fn, lVal, rVal);
+      } else {
+        return fn(k, lVal, rVal);
+      }
+    },
+    lObj,
+    rObj
+  );
+}
+
+function mergeDeepRight(lObj: any, rObj: any) {
+  return mergeDeepWithKey(
+    function (k, lVal, rVal) {
+      return rVal;
+    },
+    lObj,
+    rObj
+  );
+}
 
 export function deepMerge<T>(a: T, b: Partial<T> | RecursivePartial<T> | T): T {
   if (!isObject(a) || !isObject(b)) {
@@ -13,8 +64,8 @@ export function deepMerge<T>(a: T, b: Partial<T> | RecursivePartial<T> | T): T {
     if (Array.isArray(a as any) && Array.isArray(b as any)) {
       const clone: any[] = (a as any).slice();
       for (let i = 0; i < (b as any).length; i++) {
-        if ((b as any)[ i ] !== undefined) {
-          clone[ i ] = deepMerge(clone[ i ], (b as any)[ i ]);
+        if ((b as any)[i] !== undefined) {
+          clone[i] = deepMerge(clone[i], (b as any)[i]);
         }
       }
       return clone as any;
@@ -23,5 +74,4 @@ export function deepMerge<T>(a: T, b: Partial<T> | RecursivePartial<T> | T): T {
   }
 
   return mergeDeepRight(a, b) as any;
-
 }
