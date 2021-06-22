e50df0af622032c6cc0cdc136a49a0a0b39d2826
Merzough Münker <mmuenker@digitaix.com>
2021-06-22 09:52:12 +0200

    fix: add support for root allOf, anyOf

--- libs/json-schema-to-typescript/src/lib/typescript-interface-generator.ts
+++ libs/json-schema-to-typescript/src/lib/typescript-interface-generator.ts
@@ -9,18 +9,15 @@ import {
   WriterFunction,
   TypeElementMemberedNodeStructure,
   QuoteKind,
-  TypeAliasDeclarationStructure
+  TypeAliasDeclarationStructure,
 } from 'ts-morph';
 import {
   bundle,
   Options,
-  JSONSchema
+  JSONSchema,
 } from '@apidevtools/json-schema-ref-parser';
 import { joinPath } from './join';
-import {
-  classify,
-  dasherize
-} from './strings';
+import { classify, dasherize } from './strings';
 import { getFromObject } from './get-from-object';
 
 export interface TypescriptInterfaceGeneratorOptions extends Options {
@@ -30,7 +27,6 @@ export interface TypescriptInterfaceGeneratorOptions extends Options {
 }
 
 export class TypescriptInterfaceGenerator {
-
   private bundledSchema: JSONSchema | null = null;
 
   private readonly project: Project;
@@ -38,13 +34,19 @@ export class TypescriptInterfaceGenerator {
   constructor(
     private readonly schema: JSONSchema,
     private readonly options: TypescriptInterfaceGeneratorOptions = {},
-    project: Project | null = null,
+    project: Project | null = null
   ) {
-    this.project = project ?? new Project({ manipulationSettings: { indentationText: IndentationText.TwoSpaces, quoteKind: QuoteKind.Single } });
+    this.project =
+      project ??
+      new Project({
+        manipulationSettings: {
+          indentationText: IndentationText.TwoSpaces,
+          quoteKind: QuoteKind.Single,
+        },
+      });
   }
 
   public async build(name: string): Promise<SourceFile> {
-
     await this.bundleSchema();
 
     if (!this.bundledSchema) {
@@ -52,11 +54,9 @@ export class TypescriptInterfaceGenerator {
     }
 
     return this.addType(this.bundledSchema, name);
-
   }
 
   private addType(schema: JSONSchema, name: string): SourceFile {
-
     let resolvedSchema: JSONSchema;
 
     if (schema.$ref) {
@@ -77,8 +77,10 @@ export class TypescriptInterfaceGenerator {
   }
 
   private addTypeAlias(schema: JSONSchema, name: string): SourceFile {
-
-    const filePath = joinPath(this.options.basePath, `${this.getFileName(name)}.ts`);
+    const filePath = joinPath(
+      this.options.basePath,
+      `${this.getFileName(name)}.ts`
+    );
 
     let sourceFile = this.project.getSourceFile(filePath);
 
@@ -94,21 +96,23 @@ export class TypescriptInterfaceGenerator {
       throw new Error('Could not create a write function for the type!');
     }
 
-    const typeAliasDeclarationStructure: OptionalKind<TypeAliasDeclarationStructure> = {
-      name: this.buildName(name),
-      type,
-      isExported: true,
-    }
+    const typeAliasDeclarationStructure: OptionalKind<TypeAliasDeclarationStructure> =
+      {
+        name: this.buildName(name),
+        type,
+        isExported: true,
+      };
 
     sourceFile.addTypeAlias(typeAliasDeclarationStructure);
 
     return sourceFile;
-
   }
 
   private addInterface(schema: JSONSchema, name: string): SourceFile {
-
-    const filePath = joinPath(this.options.basePath, `${this.getFileName(name)}.ts`);
+    const filePath = joinPath(
+      this.options.basePath,
+      `${this.getFileName(name)}.ts`
+    );
 
     let sourceFile = this.project.getSourceFile(filePath);
 
@@ -129,23 +133,38 @@ export class TypescriptInterfaceGenerator {
       throw new Error('The provided schema has not a properties declaration!');
     }
 
-    for (const [key, property] of Object.entries(schema.properties as Record<string, JSONSchema>)) {
-
-      interfaceStructure.properties?.push(this.buildPropertySignatureStructure(sourceFile, key, property, this.isRequired(schema, key)));
-
+    for (const [key, property] of Object.entries(
+      schema.properties as Record<string, JSONSchema>
+    )) {
+      interfaceStructure.properties?.push(
+        this.buildPropertySignatureStructure(
+          sourceFile,
+          key,
+          property,
+          this.isRequired(schema, key)
+        )
+      );
     }
 
     sourceFile.addInterface(interfaceStructure);
 
     return sourceFile;
-
   }
 
   private isRequired(schema: JSONSchema, key: string): boolean {
-    return !!schema.required && Array.isArray(schema.required) && schema.required.includes(key);
+    return (
+      !!schema.required &&
+      Array.isArray(schema.required) &&
+      schema.required.includes(key)
+    );
   }
 
-  private buildPropertySignatureStructure(currentFile: SourceFile, key: string, property: JSONSchema, required: boolean): OptionalKind<PropertySignatureStructure> {
+  private buildPropertySignatureStructure(
+    currentFile: SourceFile,
+    key: string,
+    property: JSONSchema,
+    required: boolean
+  ): OptionalKind<PropertySignatureStructure> {
     const propertyStructure: OptionalKind<PropertySignatureStructure> = {
       name: this.coercePropertyKey(key),
       type: this.propertyTypeWriteFunction(currentFile, property),
@@ -160,13 +179,18 @@ export class TypescriptInterfaceGenerator {
   }
 
   private coercePropertyKey(key: string): string {
-    if (key.match(/(^[0-9]+|-|#|\.|@|\/|:|\*)/) && !key.match(/\[\w+:\s?\w+\]/)) {
+    if (
+      key.match(/(^[0-9]+|-|#|\.|@|\/|:|\*)/) &&
+      !key.match(/\[\w+:\s?\w+\]/)
+    ) {
       return `'${key}'`;
     }
     return key;
   }
 
-  private unionType(array: Array<string | WriterFunction>): WriterFunction | string {
+  private unionType(
+    array: Array<string | WriterFunction>
+  ): WriterFunction | string {
     if (array.length < 2) {
       return array[0];
     }
@@ -177,7 +201,9 @@ export class TypescriptInterfaceGenerator {
     return Writers.unionType(first, second, ...array);
   }
 
-  private intersectionType(array: Array<string | WriterFunction>): WriterFunction | string {
+  private intersectionType(
+    array: Array<string | WriterFunction>
+  ): WriterFunction | string {
     if (array.length < 2) {
       return array[0];
     }
@@ -188,8 +214,11 @@ export class TypescriptInterfaceGenerator {
     return Writers.intersectionType(first, second, ...array);
   }
 
-  private recordType(propertyType: string | WriterFunction, keyType: 'string' | 'number' | 'boolean' = 'string'): WriterFunction {
-    return writer => {
+  private recordType(
+    propertyType: string | WriterFunction,
+    keyType: 'string' | 'number' | 'boolean' = 'string'
+  ): WriterFunction {
+    return (writer) => {
       writer.write('Record<');
       writer.write(keyType);
       writer.write(',');
@@ -199,16 +228,21 @@ export class TypescriptInterfaceGenerator {
         propertyType(writer);
       }
       writer.write('>');
-    }
+    };
   }
 
-  private propertyTypeWriteFunction(currentFile: SourceFile, schema: JSONSchema): WriterFunction | string {
-
+  private propertyTypeWriteFunction(
+    currentFile: SourceFile,
+    schema: JSONSchema
+  ): WriterFunction | string {
     switch (schema.type) {
       case 'string':
-
         if (schema.enum) {
-          return this.unionType((schema.enum as string[]).map(item => writer => writer.quote(item)));
+          return this.unionType(
+            (schema.enum as string[]).map(
+              (item) => (writer) => writer.quote(item)
+            )
+          );
         }
 
         return 'string';
@@ -226,40 +260,86 @@ export class TypescriptInterfaceGenerator {
       case 'any':
         return 'any';
 
+      case 'array':
+        if (schema.items) {
+          const items = schema.items;
+
+          if (!Array.isArray(items) && items !== true) {
+            return (writer) => {
+              writer.write('Array<');
+              const type = this.propertyTypeWriteFunction(currentFile, items);
+
+              if (typeof type === 'string') {
+                writer.write(type);
+              } else if (type === undefined) {
+              } else {
+                type(writer);
+              }
+              writer.write('>');
+            };
+          }
+        }
+
+        return 'Array<any>';
+
       case 'object':
-
-        if (schema.properties || schema.additionalProperties || schema.patternProperties) {
-
+      case undefined:
+        if (
+          schema.properties ||
+          schema.additionalProperties ||
+          schema.patternProperties
+        ) {
           const objectTypeStructure: TypeElementMemberedNodeStructure = {
-            properties: []
+            properties: [],
           };
 
           if (schema.properties) {
-            for (const [ key, property ] of Object.entries(schema.properties as Record<string, JSONSchema>)) {
-              objectTypeStructure.properties?.push(this.buildPropertySignatureStructure(currentFile, key, property, this.isRequired(schema, key)));
+            for (const [key, property] of Object.entries(
+              schema.properties as Record<string, JSONSchema>
+            )) {
+              objectTypeStructure.properties?.push(
+                this.buildPropertySignatureStructure(
+                  currentFile,
+                  key,
+                  property,
+                  this.isRequired(schema, key)
+                )
+              );
             }
           }
 
-          if (schema.patternProperties && Object.keys(schema.patternProperties).length) {
+          if (
+            schema.patternProperties &&
+            Object.keys(schema.patternProperties).length
+          ) {
             const typeList: Array<string | WriterFunction> = [];
-            for (const [ key, property ] of Object.entries(schema.patternProperties as Record<string, JSONSchema>)) {
-              typeList.push(this.propertyTypeWriteFunction(currentFile, property));
+            for (const [key, property] of Object.entries(
+              schema.patternProperties as Record<string, JSONSchema>
+            )) {
+              typeList.push(
+                this.propertyTypeWriteFunction(currentFile, property)
+              );
             }
             if (schema.properties && Object.keys(schema.properties).length) {
               return Writers.intersectionType(
                 Writers.objectType(objectTypeStructure),
                 this.recordType(typeList.shift()!),
-                ...typeList.map(type => this.recordType(type))
+                ...typeList.map((type) => this.recordType(type))
               );
             } else {
-              return this.intersectionType(typeList.map(type => this.recordType(type)));
+              return this.intersectionType(
+                typeList.map((type) => this.recordType(type))
+              );
             }
           }
 
           if (schema.additionalProperties) {
             let type: string | WriterFunction = 'any';
             if (schema.additionalProperties !== true) {
-              type = this.propertyTypeWriteFunction(currentFile, schema.additionalProperties);
+              type = this.propertyTypeWriteFunction(
+                currentFile,
+                schema.additionalProperties
+              );
             }
             if (schema.properties && Object.keys(schema.properties).length) {
               return Writers.intersectionType(
@@ -272,75 +352,38 @@ export class TypescriptInterfaceGenerator {
           }
 
           return Writers.objectType(objectTypeStructure);
-
-        } else {
-          return 'any';
-        }
-
-      case 'array':
-
-        if (schema.items) {
-
-          const items = schema.items;
-
-          if (!Array.isArray(items) && items !== true) {
-
-            return writer => {
-
-              writer.write('Array<');
-              const type = this.propertyTypeWriteFunction(currentFile, items);
-
-              if (typeof type === 'string') {
-                writer.write(type);
-              } else if (type === undefined) {
-
-              } else {
-                type(writer);
-              }
-              writer.write('>');
-
-            }
-
-          }
-
-        }
-
-        return 'Array<any>';
-
-      case undefined:
-
-        if (schema.$ref) {
+        } else if (schema.$ref) {
           const name = this.resolveRefName(schema.$ref);
 
           this.addType(this.resolveRef(schema.$ref), name);
 
-          if (this.options.addImports && currentFile.getBaseNameWithoutExtension() !== this.getFileName(name)) {
-
+          if (
+            this.options.addImports &&
+            currentFile.getBaseNameWithoutExtension() !== this.getFileName(name)
+          ) {
             currentFile.addImportDeclaration({
-              isTypeOnly:      true,
-              namedImports:    [ { name: this.buildName(name) } ],
+              isTypeOnly: true,
+              namedImports: [{ name: this.buildName(name) }],
               moduleSpecifier: `./${this.getFileName(name)}`,
             });
-
           }
 
           return this.buildName(name);
-
         } else if (schema.additionalProperties) {
           const additionalProperties = schema.additionalProperties;
-          return writer => {
+          return (writer) => {
             writer.write('Record');
             writer.write('<');
             writer.write('string');
             writer.write(',');
 
             if (typeof additionalProperties === 'boolean') {
-
               writer.write('any');
-
             } else {
-
-              const type = this.propertyTypeWriteFunction(currentFile, additionalProperties);
+              const type = this.propertyTypeWriteFunction(
+                currentFile,
+                additionalProperties
+              );
 
               if (type) {
                 if (typeof type === 'string') {
@@ -351,85 +394,105 @@ export class TypescriptInterfaceGenerator {
               } else {
                 writer.write('any');
               }
-
             }
 
             writer.write('>');
-
-          }
+          };
         } else if (schema.oneOf) {
-
           const typeList: Array<WriterFunction | string> = [];
 
           for (const oneOf of schema.oneOf) {
             if (typeof oneOf !== 'boolean') {
-              typeList.push(this.propertyTypeWriteFunction(currentFile, oneOf))
+              typeList.push(this.propertyTypeWriteFunction(currentFile, oneOf));
             }
           }
 
           return this.unionType(typeList);
         } else if (schema.anyOf) {
-
           const typeList: Array<WriterFunction | string> = [];
 
           for (const oneOf of schema.anyOf) {
             if (typeof oneOf !== 'boolean') {
-              typeList.push(this.propertyTypeWriteFunction(currentFile, oneOf))
+              typeList.push(this.propertyTypeWriteFunction(currentFile, oneOf));
             }
           }
 
           return this.unionType(typeList);
         } else if (schema.allOf) {
-
           const typeList: Array<WriterFunction | string> = [];
 
           for (const oneOf of schema.allOf) {
             if (typeof oneOf !== 'boolean') {
-              typeList.push(this.propertyTypeWriteFunction(currentFile, oneOf))
+              typeList.push(this.propertyTypeWriteFunction(currentFile, oneOf));
             }
           }
 
           return this.intersectionType(typeList);
         } else if (schema.const) {
-          return writer => writer.quote(schema.const);
+          return (writer) => writer.quote(schema.const);
         } else if (schema.properties) {
           const objectTypeStructure: TypeElementMemberedNodeStructure = {
-            properties: []
+            properties: [],
           };
 
-          for (const [ key, property ] of Object.entries(schema.properties as Record<string, JSONSchema>)) {
-            objectTypeStructure.properties?.push(this.buildPropertySignatureStructure(currentFile, key, property, this.isRequired(schema, key)));
+          for (const [key, property] of Object.entries(
+            schema.properties as Record<string, JSONSchema>
+          )) {
+            objectTypeStructure.properties?.push(
+              this.buildPropertySignatureStructure(
+                currentFile,
+                key,
+                property,
+                this.isRequired(schema, key)
+              )
+            );
           }
 
           return Writers.objectType(objectTypeStructure);
         }
 
-        console.warn(`The property type is undefined and a ref is not defined! found: ${Object.keys(schema)}`);
+        console.warn(
+          `The property type is undefined and a ref is not defined! found: ${Object.keys(
+            schema
+          )}`
+        );
 
         // TODO : add support for allOf and anyOf
 
-        return writer => writer.write('any');
+        return (writer) => writer.write('any');
 
       default:
-
         if (Array.isArray(schema.type)) {
-
-          const primitiveTypeList = schema.type.filter(type => ['string', 'integer', 'number', 'boolean', 'null', 'any'].includes(type));
-          const complexTypeList = schema.type.filter(type => !['string', 'integer', 'number', 'boolean', 'null', 'any'].includes(type));
+          const primitiveTypeList = schema.type.filter((type) =>
+            ['string', 'integer', 'number', 'boolean', 'null', 'any'].includes(
+              type
+            )
+          );
+          const complexTypeList = schema.type.filter(
+            (type) =>
+              ![
+                'string',
+                'integer',
+                'number',
+                'boolean',
+                'null',
+                'any',
+              ].includes(type)
+          );
 
           const typeList: Array<WriterFunction | string> = primitiveTypeList;
 
           for (const type of complexTypeList) {
-            typeList.push(this.propertyTypeWriteFunction(currentFile, { ...schema, type }));
+            typeList.push(
+              this.propertyTypeWriteFunction(currentFile, { ...schema, type })
+            );
           }
 
           return this.unionType(typeList);
         }
 
         throw new Error(`The property type '${schema.type}' is not supported!`);
-
     }
-
   }
 
   private getFileName(name: string): string {
@@ -462,7 +525,10 @@ export class TypescriptInterfaceGenerator {
     // remove the prefix '#'
     path.shift();
 
-    const schema: JSONSchema | undefined = getFromObject(this.bundledSchema, path.join('.'));
+    const schema: JSONSchema | undefined = getFromObject(
+      this.bundledSchema,
+      path.join('.')
+    );
 
     if (!schema) {
       throw new Error(`Could not resolve $ref '${ref}'.`);
@@ -476,5 +542,4 @@ export class TypescriptInterfaceGenerator {
       this.bundledSchema = await bundle(this.schema, this.options);
     }
   }
-
 }
