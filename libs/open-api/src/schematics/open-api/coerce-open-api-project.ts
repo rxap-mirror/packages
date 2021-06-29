1f3614727affb24729d17b5d3db6f556cf2c2cd7
Merzough Münker <mmuenker@digitaix.com>
2021-06-29 15:54:13 +0200

    refactor: split open-api schematic
    Signed-off-by: Merzough Münker <mmuenker@digitaix.com>

---
+++ libs/open-api/src/schematics/open-api/coerce-open-api-project.ts
@@ -0,0 +1,48 @@
+import {
+  Rule,
+  Tree,
+  chain,
+  externalSchematic,
+  noop
+} from '@angular-devkit/schematics';
+import { GetAngularJson } from '@rxap/schematics-utilities';
+
+export function CoerceOpenApiProject(project: string): Rule {
+  return (host: Tree) => {
+    const angularJson = GetAngularJson(host) as any;
+
+    if (!angularJson.projects.hasOwnProperty(project)) {
+      const defaultProjectPrefix =
+              angularJson.projects[ angularJson.defaultProject ].prefix;
+      return chain([
+        externalSchematic('@nrwl/angular', 'library', {
+          name:       project,
+          importPath: `@${defaultProjectPrefix}/${project}`
+        }),
+        (tree) => {
+          const baseTsconfig = JSON.parse(
+            tree.read('/tsconfig.base.json')?.toString() ?? '{}'
+          );
+
+          const paths = baseTsconfig?.compilerOptions?.paths ?? {};
+
+          if (Object.keys(paths).length) {
+            for (const key of Object.keys(paths)) {
+              if (key.match(/\/open-api$/)) {
+                delete paths[ key ];
+                paths[ key + '/*' ] = [ 'libs/open-api/src/lib/*' ];
+              }
+            }
+
+            tree.overwrite(
+              '/tsconfig.base.json',
+              JSON.stringify(baseTsconfig, undefined, 2)
+            );
+          }
+        }
+      ]);
+    }
+
+    return noop();
+  };
+}
