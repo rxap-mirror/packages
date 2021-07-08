dca873d475efc965641cb86b59ec89982cd13ab0
Merzough Münker <mmuenker@digitaix.com>
2021-07-08 12:46:09 +0200

    feat: add ng-add logic to update project files
    Signed-off-by: Merzough Münker <mmuenker@digitaix.com>

--- libs/environment/src/schematics/ng-add/index.ts
+++ libs/environment/src/schematics/ng-add/index.ts
@@ -1,6 +1,93 @@
-import { Rule } from '@angular-devkit/schematics';
-import { InstallPeerDependencies } from '@rxap/schematics-utilities';
+import {
+  Rule,
+  chain,
+  Tree,
+  SchematicsException
+} from '@angular-devkit/schematics';
+import {
+  InstallPeerDependencies,
+  GetProjectSourceRoot
+} from '@rxap/schematics-utilities';
+import { NgAddSchema } from './schema';
+import {
+  Project,
+  QuoteKind,
+  IndentationText
+} from 'ts-morph';
+import {
+  AddDir,
+  ApplyTsMorphProject,
+  AddNgModuleProvider,
+  AddToArray
+} from '@rxap/schematics-ts-morph';
 
-export default function(): Rule {
-  return InstallPeerDependencies();
+export default function(options: NgAddSchema): Rule {
+  return (host: Tree) => {
+
+    const rules: Rule[] = [
+      InstallPeerDependencies()
+    ];
+
+    if (options.project) {
+      const projectSourceRoot = GetProjectSourceRoot(host, options.project);
+      const project           = new Project({
+        useInMemoryFileSystem: true,
+        manipulationSettings:  { quoteKind: QuoteKind.Single, indentationText: IndentationText.TwoSpaces }
+      });
+      AddDir(host.getDir(projectSourceRoot), project);
+
+      const appModelSourceFile = project.getSourceFile('/app/app.module.ts');
+
+      if (!appModelSourceFile) {
+        throw new SchematicsException(`Could not find the AppModule source file in '[projectSourceRoot]/app/app.module.ts'`);
+      }
+
+      AddNgModuleProvider(
+        appModelSourceFile,
+        {
+          provide:  'RXAP_ENVIRONMENT',
+          useValue: 'environment'
+        },
+        [
+          {
+            namedImports:    [ 'RXAP_ENVIRONMENT' ],
+            moduleSpecifier: '@rxap/environment'
+          },
+          {
+            namedImports:    [ 'environment' ],
+            moduleSpecifier: '../environments/environment'
+          }
+        ]
+      );
+
+      const mainSourceFile = project.getSourceFile('/main.ts');
+
+      if (!mainSourceFile) {
+        throw new SchematicsException(`Could not find the main.ts source file in '[projectSourceRoot]/main.ts'`);
+      }
+
+      AddToArray(
+        mainSourceFile,
+        'setup',
+        'UpdateEnvironment(environment)',
+        'Promise<any>[]'
+      );
+
+      mainSourceFile.addImportDeclarations([
+        {
+          namedImports:    [ 'UpdateEnvironment' ],
+          moduleSpecifier: '@rxap/environment'
+        },
+        {
+          namedImports:    [ 'environment' ],
+          moduleSpecifier: './environments/environment'
+        }
+      ]);
+
+      rules.push(ApplyTsMorphProject(project, projectSourceRoot));
+    }
+
+    return chain(rules);
+
+  };
 }
