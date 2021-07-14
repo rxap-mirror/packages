0b1ce250a66bdad14d24509e13942960f25b10da
Merzough Münker <mmuenker@digitaix.com>
2021-07-14 16:28:20 +0200

    fix(cleanup): set correct return type
    Signed-off-by: Merzough Münker <mmuenker@digitaix.com>

--- libs/utilities/src/lib/object/cleanup.ts
+++ libs/utilities/src/lib/object/cleanup.ts
@@ -1,29 +1,29 @@
-export function DeleteUndefinedProperties<T>(obj: T): Partial<T> {
-  const keys = Object.keys(obj);
+export function DeleteUndefinedProperties<T>(obj: T): Exclude<T, undefined> {
+  const keys          = Object.keys(obj);
   const cloneObj: any = {};
 
   for (const key of keys) {
-    if ((obj as any)[key] !== undefined) {
-      cloneObj[key] = (obj as any)[key];
+    if ((obj as any)[ key ] !== undefined) {
+      cloneObj[ key ] = (obj as any)[ key ];
     }
   }
 
   return cloneObj;
 }
 
-export function DeleteNullProperties<T>(obj: T): Partial<T> {
-  const keys = Object.keys(obj);
+export function DeleteNullProperties<T>(obj: T): Exclude<T, null> {
+  const keys          = Object.keys(obj);
   const cloneObj: any = {};
 
   for (const key of keys) {
-    if ((obj as any)[key] !== null) {
-      cloneObj[key] = (obj as any)[key];
+    if ((obj as any)[ key ] !== null) {
+      cloneObj[ key ] = (obj as any)[ key ];
     }
   }
 
   return cloneObj;
 }
 
-export function DeleteEmptyProperties<T>(obj: T): Partial<T> {
+export function DeleteEmptyProperties<T>(obj: T): Exclude<Exclude<T, null>, undefined> {
   return DeleteUndefinedProperties(DeleteNullProperties(obj));
 }
