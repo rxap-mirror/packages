630d2061cbb45f6c6557c028a371f6cc6c2581e0
Merzough Münker <mmuenker@digitaix.com>
2021-07-21 15:27:23 +0200

    feat: add full text search
    Signed-off-by: Merzough Münker <mmuenker@digitaix.com>

---
+++ libs/table-system/src/lib/full-text-search/table-filter-input.directive.ts
@@ -0,0 +1,45 @@
+import {
+  Directive,
+  Inject,
+  OnDestroy,
+  OnInit
+} from '@angular/core';
+import { NgModel } from '@angular/forms';
+import { RXAP_TABLE_FILTER } from '@rxap/material-table-system';
+import { Subscription } from 'rxjs';
+import {
+  debounceTime,
+  tap
+} from 'rxjs/operators';
+import { TableFullTextSearchService } from './table-full-text-search.service';
+
+@Directive({
+  selector: 'input[ngModel][rxapTableFilterInput]'
+})
+export class TableFilterInputDirective implements OnInit, OnDestroy {
+  private _subscription?: Subscription;
+
+  constructor(
+    @Inject(NgModel)
+    private readonly ngModel: NgModel,
+    @Inject(RXAP_TABLE_FILTER)
+    private readonly stringFilterService: TableFullTextSearchService
+  ) {}
+
+  public ngOnDestroy() {
+    this._subscription?.unsubscribe();
+  }
+
+  public ngOnInit() {
+    this._subscription = this.ngModel.valueChanges
+                             ?.pipe(
+                               debounceTime(128),
+                               tap((value) =>
+                                 this.stringFilterService.change.next(
+                                   value && value.length >= 3 ? value : null
+                                 )
+                               )
+                             )
+                             .subscribe();
+  }
+}
