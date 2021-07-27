6b0d500d67f2f1552f152dd3104b08901a7917a4
Merzough Münker <mmuenker@digitaix.com>
2021-07-27 10:45:24 +0200

    feat(schematics): add init schematic
    Signed-off-by: Merzough Münker <mmuenker@digitaix.com>

--- libs/firebase/src/schematics/ng-add/index.ts
+++ libs/firebase/src/schematics/ng-add/index.ts
@@ -1,6 +1,14 @@
-import { Rule } from '@angular-devkit/schematics';
+import {
+  Rule,
+  chain,
+  schematic
+} from '@angular-devkit/schematics';
 import { InstallPeerDependencies } from '@rxap/schematics-utilities';
+import { NgAddSchema } from './schema';
 
-export default function(): Rule {
-  return InstallPeerDependencies();
+export default function(options: NgAddSchema): Rule {
+  return chain([
+    InstallPeerDependencies(),
+    schematic('init', options)
+  ]);
 }
--- libs/firebase/src/schematics/ng-add/schema.d.ts
+++ libs/firebase/src/schematics/ng-add/schema.d.ts
@@ -1,2 +1,12 @@
 export interface NgAddSchema {
+  project?: string;
+  functions?: boolean;
+  analytics?: boolean;
+  performance?: boolean;
+  storage?: boolean;
+  appCheck?: boolean;
+  firestore?: boolean;
+  auth?: boolean;
+  useEmulator?: boolean;
+  hostingSite?: string;
 }
