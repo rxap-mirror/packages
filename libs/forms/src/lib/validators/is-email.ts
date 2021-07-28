54abed6af69235c17cc9f5b2b867cc11cdf3aad2
Merzough Münker <mmuenker@digitaix.com>
2021-07-28 13:25:20 +0200

    fix: use @rxap/validator package
    Signed-off-by: Merzough Münker <mmuenker@digitaix.com>

--- libs/forms/src/lib/validators/is-email.ts
+++ libs/forms/src/lib/validators/is-email.ts
@@ -2,17 +2,17 @@ import {
   AbstractControl,
   ValidationErrors
 } from '@angular/forms';
-import isEmailValidator from 'validator/es/lib/isEmail';
-import type ValidatorJS from 'validator';
+import {
+  isEmail,
+  IsEmailOptions
+} from '@rxap/validator';
 
-export type IsEmailOptions = ValidatorJS.IsEmailOptions
-
-export function IsEmail({ message, options }: { message?: string, options?: ValidatorJS.IsEmailOptions } = {}) {
+export function IsEmail({ message, options }: { message?: string, options?: IsEmailOptions } = {}) {
   return (control: AbstractControl): ValidationErrors | null => {
     if (control.value === null) {
       return null;
     }
-    if (!(typeof control.value === 'string' && isEmailValidator(control.value, options))) {
+    if (!(typeof control.value === 'string' && isEmail(control.value, options))) {
       return {
         isEmail: {
           expected: 'A email value',
