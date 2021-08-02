a731b2086865c0982cdf7360f2e706fbfe171de1
Merzough Münker <mmuenker@digitaix.com>
2021-08-02 10:32:35 +0200

    fix(cell-copy-to-clipboard): only show copy button if value is not empty
    Signed-off-by: Merzough Münker <mmuenker@digitaix.com>

--- libs/material/table-system/src/lib/cells/copy-to-clipboard-cell/copy-to-clipboard-cell.component.module.ts
+++ libs/material/table-system/src/lib/cells/copy-to-clipboard-cell/copy-to-clipboard-cell.component.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CopyToClipboardCellComponent } from './copy-to-clipboard-cell.component';
 import { CopyToClipboardModule } from '@rxap/components';
-
+import { CommonModule } from '@angular/common';
 
 
 @NgModule({
   declarations: [CopyToClipboardCellComponent],
   imports: [
     CopyToClipboardModule,
+    CommonModule
   ],
   exports: [CopyToClipboardCellComponent]
 })
