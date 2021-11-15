d4669520113fc4bd18c319f3ede22a56b8514282
Merzough Münker <mmuenker@digitaix.com>
2021-11-15 11:13:19 +0100

    chore: add @rxap/socket-io package
    Signed-off-by: Merzough Münker <mmuenker@digitaix.com>

---
+++ libs/socket-io/README.md
@@ -0,0 +1,7 @@
+# socket-io
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test socket-io` to execute the unit tests.
---
+++ libs/socket-io/src/lib/socket-io.module.ts
@@ -0,0 +1,33 @@
+import {
+  NgModule,
+  ModuleWithProviders,
+  InjectionToken
+} from '@angular/core';
+import { SocketIoConfig } from './socket-io.config';
+import { WrappedSocket } from './socket-io.service';
+
+/** Socket factory */
+export function SocketFactory(config: SocketIoConfig) {
+  return new WrappedSocket(config);
+}
+
+export const SOCKET_CONFIG_TOKEN = new InjectionToken<SocketIoConfig>(
+  'rxap-socket-io/config'
+);
+
+@NgModule({})
+export class SocketIoModule {
+  static forRoot(config: SocketIoConfig): ModuleWithProviders<SocketIoModule> {
+    return {
+      ngModule: SocketIoModule,
+      providers: [
+        { provide: SOCKET_CONFIG_TOKEN, useValue: config },
+        {
+          provide: WrappedSocket,
+          useFactory: SocketFactory,
+          deps: [SOCKET_CONFIG_TOKEN],
+        },
+      ],
+    };
+  }
+}
