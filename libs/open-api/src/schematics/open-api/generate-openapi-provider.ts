ef7a84b44bd796a2a3213fcf3b7f66d28ab01558
Merzough Münker <mmuenker@digitaix.com>
2021-11-25 16:33:29 +0100

    feat: generate openapi provider object
    Signed-off-by: Merzough Münker <mmuenker@digitaix.com>

---
+++ libs/open-api/src/schematics/open-api/generate-openapi-provider.ts
@@ -0,0 +1,51 @@
+import { Project } from 'ts-morph';
+import { join } from 'path';
+import { strings } from '@angular-devkit/core';
+import {
+  REMOTE_METHOD_BASE_PATH,
+  REMOTE_METHOD_FILE_SUFFIX
+} from './const';
+
+const { dasherize, classify } = strings;
+
+export function GenerateOpenapiProvider(
+  project: Project,
+  operatorIdList: string[],
+) {
+
+  const sourceFile = project.createSourceFile('open-api-providers.ts');
+
+  const providerList: string[] = [];
+
+  sourceFile.addImportDeclaration({
+    moduleSpecifier: '@angular/core',
+    namedImports: [ 'Provider' ]
+  });
+
+  for (const operationId of operatorIdList) {
+
+    const name     = [ operationId, REMOTE_METHOD_FILE_SUFFIX ].join('.');
+    const moduleSpecifier = './' + join(REMOTE_METHOD_BASE_PATH, dasherize(name));
+    const className = classify(name.replace(/\./g, '-'));
+
+    sourceFile.addImportDeclaration({
+      moduleSpecifier,
+      namedImports: [ className ]
+    });
+
+    providerList.push(className);
+
+  }
+
+  sourceFile.addVariableStatement({
+    isExported: true,
+    declarations: [
+      {
+        name: 'OPEN_API_PROVIDERS',
+        type: 'Provider[]',
+        initializer: `[${providerList.join(', ')}]`,
+      }
+    ]
+  });
+
+}
