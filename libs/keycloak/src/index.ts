0420162427b5ef764f123df5d2e305891da95e08
Merzough Münker <mmuenker@digitaix.com>
2022-02-17 13:37:19 +0100

    fix: add the keycloak package
    Signed-off-by: Merzough Münker <mmuenker@digitaix.com>

---
+++ libs/keycloak/src/index.ts
@@ -0,0 +1,9 @@
+export {
+  KeycloakEvent,
+  KeycloakEventType
+} from './lib/interfaces/keycloak-event';
+export { KeycloakOptions } from './lib/interfaces/keycloak-options';
+export { KeycloakAuthGuard } from './lib/services/keycloak-auth-guard';
+export { KeycloakService } from './lib/services/keycloak.service';
+export { KeycloakBearerInterceptor } from './lib/interceptors/keycloak-bearer.interceptor';
+export { KeycloakModule } from './lib/keycloak.module';
