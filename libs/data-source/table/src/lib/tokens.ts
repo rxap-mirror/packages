83f345fbda16b119ec71a80ba0316c3275b819fb
Merzough Münker <mmuenker@digitaix.com>
2022-04-26 09:53:41 +0200

    fix: replace RXAP_TABLE_REMOTE_METHOD with RXAP_TABLE_METHOD

--- libs/data-source/table/src/lib/tokens.ts
+++ libs/data-source/table/src/lib/tokens.ts
@@ -5,3 +5,6 @@ export const RXAP_TABLE_DATA_SOURCE_SORT       = new InjectionToken('rxap/data-s
 export const RXAP_TABLE_DATA_SOURCE_FILTER     = new InjectionToken('rxap/data-source/table/filter');
 export const RXAP_TABLE_DATA_SOURCE_PARAMETERS = new InjectionToken('rxap/data-source/table/parameters');
 export const RXAP_TABLE_DATA_SOURCE            = new InjectionToken('rxap/data-source/table/source');
+export const RXAP_TABLE_METHOD                   = new InjectionToken(
+  'rxap/data-source/table/table-method'
+);
