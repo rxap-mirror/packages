ae3ecdc8c166286703d671a9f861556a1bc5aecb
Merzough Münker <mmuenker@digitaix.com>
2022-05-10 09:24:52 +0200

    fix: generate operation string with quotes

--- libs/open-api/src/schematics/open-api/generate-remote-method.ts
+++ libs/open-api/src/schematics/open-api/generate-remote-method.ts
@@ -96,8 +96,8 @@ export async function GenerateRemoteMethod(
         name:      'RxapOpenApiRemoteMethod',
         arguments: (writer) => Writers.object({
           serverId: parameter.options.serverId ? w => w.quote(parameter.options.serverId!) : 'undefined',
-          operation: `JSON.parse(${JSON.stringify(GenerateParameterToOperationObjectWithMetadata(parameter))})`
-        })
+          operation: `JSON.parse('${JSON.stringify(GenerateParameterToOperationObjectWithMetadata(parameter))}') as any`
+        })(writer)
       }
     ],
     methods:    [
