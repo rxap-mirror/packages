83637213fa134046da9c6fc6dd6a4ee034a6c106
Merzough Münker <mmuenker@digitaix.com>
2022-10-17 11:53:10 +0200

    fix: change to async validation

--- libs/config/src/lib/config.service.ts
+++ libs/config/src/lib/config.service.ts
@@ -74,7 +74,7 @@ export class ConfigService<Config extends Record<string, any> = Record<string, a
 
     if (schema) {
       try {
-        schema.validate(config);
+        config = await schema.validateAsync(config);
       } catch (error: any) {
         const message = `Config from '${url}' is not valid: ${error.message}`;
         if (required) {
