4890bdb45af48d304612c401f996ee0a8b2ee597
Merzough Münker <mmuenker@digitaix.com>
2022-11-07 09:47:16 +0100

    feat: compare with function for select state

--- libs/material/table-system/src/lib/select-row/select-row.service.ts
+++ libs/material/table-system/src/lib/select-row/select-row.service.ts
@@ -1,9 +1,9 @@
 import { Injectable, Optional, Inject } from '@angular/core';
-import { SelectionModel } from '@angular/cdk/collections';
 import type { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { SelectRowOptions } from './select-row.options';
 import { RXAP_MATERIAL_TABLE_SYSTEM_SELECT_ROW_OPTIONS } from './tokens';
+import { SelectionModel } from './selection-model';
 
 @Injectable()
 export class SelectRowService<Data extends Record<string, any>> {
@@ -20,10 +20,12 @@ export class SelectRowService<Data extends Record<string, any>> {
     @Inject(RXAP_MATERIAL_TABLE_SYSTEM_SELECT_ROW_OPTIONS)
     options: SelectRowOptions<Data> | null = null
   ) {
-    this.selectionModel = new SelectionModel<Data>(options?.multiple);
-    if (options?.selected) {
-      this.selectionModel.select(...options?.selected);
-    }
+    this.selectionModel = new SelectionModel<Data>(
+      options?.multiple,
+      options?.selected,
+      options?.emitChanges,
+      options?.compareWith
+    );
     this.selectedRows$ = this.selectionModel.changed.pipe(
       map(() => this.selectionModel.selected)
     );
