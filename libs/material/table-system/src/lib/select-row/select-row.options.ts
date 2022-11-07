3487287abf48c3cabe461b44ffd0002bb4fa47dd
Merzough Münker <mmuenker@digitaix.com>
2022-11-07 09:52:48 +0100

    feat: compare with function for select state

--- libs/material/table-system/src/lib/select-row/select-row.options.ts
+++ libs/material/table-system/src/lib/select-row/select-row.options.ts
@@ -1,4 +1,6 @@
-export interface SelectRowOptions<RowData = any> {
+export interface SelectRowOptions<RowData = unknown> {
   multiple?: boolean;
   selected?: RowData[];
+  emitChanges?: boolean;
+  compareWith?: (o1: RowData, o2: RowData) => boolean
 }
