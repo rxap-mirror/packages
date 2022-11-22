9ec85ad72339f247457e50ec470f26656f7bc4c6
Merzough Münker <mmuenker@digitaix.com>
2022-11-22 12:10:16 +0100

    fix: use new set object value implementation

--- libs/utilities/src/lib/set-object-value.ts
+++ libs/utilities/src/lib/set-object-value.ts
@@ -1,19 +1,5 @@
+import { SetToObject } from './get-from-object';
+
 export function SetObjectValue(obj: any, path: string, value: any): void {
-  if (obj && typeof obj === 'object') {
-    const fragments: string[] = path.split('.').filter(Boolean);
-    if (fragments.length === 0) {
-      return;
-    }
-    const fragment: string = fragments.shift()!;
-    if (!obj.hasOwnProperty(fragment)) {
-      if (fragments.length === 0) {
-        obj[ fragment ] = value;
-        return;
-      } else {
-        obj[ fragment ] = {};
-      }
-    }
-    return SetObjectValue(obj[ fragment ], fragments.join('.'), value);
-  }
-  return;
+  SetToObject(obj, path, value);
 }
