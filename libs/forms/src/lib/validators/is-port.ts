24f1d0ed3668d9cd4d31ee2b965b0f8cd578930c
Merzough Münker <mmuenker@digitaix.com>
2023-04-11 10:06:24 +0200

    feat: add validators IsPort And IsUuid

---
+++ libs/forms/src/lib/validators/is-port.ts
@@ -0,0 +1,23 @@
+import {
+  AbstractControl,
+  ValidationErrors
+} from '@angular/forms';
+import { isPort } from '@rxap/validator';
+
+export function IsPort({ message }: { message?: string } = {}) {
+  return (control: AbstractControl): ValidationErrors | null => {
+    if (control.value === null) {
+      return null;
+    }
+    if (!(typeof control.value === 'string' && isPort(control.value))) {
+      return {
+        isPort: {
+          expected: `A valid port number between 1 and 65535`,
+          actual:   control.value,
+          message
+        }
+      };
+    }
+    return null;
+  };
+}
---
+++ libs/forms/src/lib/validators/is-uuid.ts
@@ -0,0 +1,23 @@
+import {
+  AbstractControl,
+  ValidationErrors
+} from '@angular/forms';
+import { isUUID } from '@rxap/validator';
+
+export function IsUUID({ message, version = 'all' }: { message?: string, version?: string } = {}) {
+  return (control: AbstractControl): ValidationErrors | null => {
+    if (control.value === null) {
+      return null;
+    }
+    if (!(typeof control.value === 'string' && isUUID(control.value, version))) {
+      return {
+        isUuid: {
+          expected: version === 'all' ? `A valid uuid` : `A valid uuid version ${version}`,
+          actual:   control.value,
+          message
+        }
+      };
+    }
+    return null;
+  };
+}
