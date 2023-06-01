c6075a05c05f0250cc9a49ef9b03701ab8a98ccc
Merzough Münker <mmuenker@digitaix.com>
2023-06-01 11:44:01 +0200

    fix: use correct object for url property

--- libs/config/src/lib/config.service.ts
+++ libs/config/src/lib/config.service.ts
@@ -56,7 +56,7 @@ export class ConfigService<Config extends Record<string, any> = Record<string, a
    */
   public static async Load(options?: ConfigLoadOptions): Promise<void> {
     let config = this.Defaults;
-    const urls = options?.url ? coerceArray(config.url) : ConfigService.Urls;
+    const urls = options?.url ? coerceArray(options.url) : ConfigService.Urls;
     for (const url of urls) {
       config = await this.loadConfig(url, true, options?.schema);
     }
