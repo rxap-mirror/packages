f865528ed54a54a77816538f85a91f887e7f04f3
Merzough Münker <mmuenker@digitaix.com>
2023-07-11 16:22:31 +0200

    fix: simplify types

--- packages/angular/material/table-system/src/lib/table-row-actions/types.ts
+++ packages/angular/material/table-system/src/lib/table-row-actions/types.ts
@@ -1,16 +1,18 @@
 import { Method } from '@rxap/pattern';
 
-export type RowActionCheckFunction<Data extends Record<string, any>> = (
+export type RowActionCheckFunction<Data> = (
   element: Data,
   index: number,
   array: Data[],
 ) => boolean;
-export type TableRowActionTypeSwitchMethod<Data extends Record<string, any>> = Method<any, {
+
+export type TableRowActionTypeSwitchMethod<Data> = Method<any, {
   element: Data;
   type: string
 }>;
-export type TableRowActionTypeMethod<Data extends Record<string, any>> = Method<any,
-  Data>;
-export type TableRowActionMethod<Data extends Record<string, any> = Record<string, any>> =
+
+export type TableRowActionTypeMethod<Data> = Method<any, Data>;
+
+export type TableRowActionMethod<Data = any> =
   | TableRowActionTypeSwitchMethod<Data>
   | TableRowActionTypeMethod<Data>;
