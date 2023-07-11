74e5d2ca4158ec9334ba8c9ed02a0f4bedf7fb5e
Merzough Münker <mmuenker@digitaix.com>
2023-07-11 16:50:57 +0200

    docs: add deprecated comment

--- packages/utilities/src/lib/set-object-value.ts
+++ packages/utilities/src/lib/set-object-value.ts
@@ -1,5 +1,8 @@
 import { SetToObject } from './get-from-object';
 
+/**
+ * @deprecated Use SetToObject instead
+ */
 export function SetObjectValue(obj: any, path: string, value: any): void {
   SetToObject(obj, path, value);
 }
