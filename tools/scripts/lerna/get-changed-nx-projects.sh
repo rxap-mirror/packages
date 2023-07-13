9ad96f2d4db9225ab199d03fc172efff52609d5d
Merzough Münker <mmuenker@digitaix.com>
2023-07-13 12:59:44 +0200

    build: fix publish build process

--- tools/scripts/lerna/get-changed-nx-projects.sh
+++ tools/scripts/lerna/get-changed-nx-projects.sh
@@ -1,27 +1,25 @@
-#!/bin/bash
+function getChangedNxProjects() {
 
-BASE_DIR=$(git rev-parse --show-toplevel)
+  # Get the list of changed projects using lerna changed command
+  changed_projects=$(yarn lerna changed --json | jq -r '.[] | .location')
 
-cd "$BASE_DIR" || exit 1
+  # Initialize an empty array to store the nx project names
+  declare -a nx_project_names
 
-# Get the list of changed projects using lerna changed command
-changed_projects=$(yarn lerna changed --json | jq -r '.[] | .location')
+  # Iterate over the changed projects
+  for project_location in $changed_projects
+  do
+    # Parse the project.json file to get the nx project name
+    nx_project_name=$(jq -r '.name' ${project_location}/project.json)
 
-# Initialize an empty array to store the nx project names
-declare -a nx_project_names
+    # Add the nx project name to the array
+    nx_project_names+=($nx_project_name)
+  done
 
-# Iterate over the changed projects
-for project_location in $changed_projects
-do
-  # Parse the project.json file to get the nx project name
-  nx_project_name=$(jq -r '.name' ${project_location}/project.json)
+  # Convert the array of nx project names into a comma separated string
+  nx_project_names_string=$(IFS=','; echo "${nx_project_names[*]}")
 
-  # Add the nx project name to the array
-  nx_project_names+=($nx_project_name)
-done
+  # Output the string of comma-separated project names
+  echo "$nx_project_names_string"
 
-# Convert the array of nx project names into a comma separated string
-nx_project_names_string=$(IFS=','; echo "${nx_project_names[*]}")
-
-# Output the string of comma-separated project names
-echo "$nx_project_names_string"
+}
--- tools/scripts/lerna/hooks/prepublishOnly.sh
+++ tools/scripts/lerna/hooks/prepublishOnly.sh
@@ -9,23 +9,6 @@ cd "$BASE_DIR" || exit 1
 
 rm -fr "${BASE_DIR}/dist/packages"
 
-changed_projects=$(bash tools/scripts/lerna/get-changed-nx-projects.sh)
-
-if [[ ! -f "${BASE_DIR}/dist/changed-projects.txt" ]]; then
-  echo "The list of changed projects has not been cached. Ensure the perversion hook has been run."
-  exit 1
-fi
-
-cached_changed_projects=$(cat "${BASE_DIR}/dist/changed-projects.txt")
-
-if [[ "$changed_projects" != "$cached_changed_projects" ]]; then
-  echo "The list of changed projects has changed since the perversion hook"
-  echo "preversion: ${cached_changed_projects}"
-  echo "prepublishOnly: ${changed_projects}"
-  exit 1
-fi
-
 yarn nx run-many \
-  --projects="${changed_projects}" \
   --target="build" \
   --configuration="production"
