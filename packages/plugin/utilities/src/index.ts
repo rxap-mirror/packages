d0e1577e962b3836c16ecae521d7aef0d01b62bc
Merzough Münker <mmuenker@digitaix.com>
2023-07-20 11:15:43 +0200

    feat: add project target utility functions

--- packages/plugin/utilities/src/index.ts
+++ packages/plugin/utilities/src/index.ts
@@ -5,3 +5,4 @@ export * from './lib/project-root-files';
 export * from './lib/get-target-configuration-name-list';
 export * from './lib/guess-output-path';
 export * from './lib/yarn-run';
+export * from './lib/project-target';
