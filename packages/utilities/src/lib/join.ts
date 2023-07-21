b6440d8a78f6e9e786f65f979c0b266b85f5c473
Merzough Münker <mmuenker@digitaix.com>
2023-07-21 14:25:55 +0200

    feat: add joinWith and joinWithDash utilities

--- packages/utilities/src/lib/join.ts
+++ packages/utilities/src/lib/join.ts
@@ -14,3 +14,24 @@ export function JoinPath(...fragments: Array<string | null | undefined>) {
  * @deprecated use JoinPath instead
  */
 export const joinPath = JoinPath;
+
+export interface JoinWithOptions {
+  strict?: boolean;
+  removeDuplicated?: boolean;
+}
+
+export function joinWith(items: string[], separator = '-', options: JoinWithOptions = {}) {
+  if (options.strict) {
+    if (items.some(item => item === null || item === undefined || item === '')) {
+      throw new Error('Invalid join items');
+    }
+  }
+  if (options.removeDuplicated) {
+    items = [ ...Array.from(new Set(items).values()) ];
+  }
+  return items.filter(item => item !== null && item !== undefined && item !== '').join(separator);
+}
+
+export function joinWithDash(items: string[], options: JoinWithOptions = {}) {
+  return joinWith(items, '-', options);
+}
