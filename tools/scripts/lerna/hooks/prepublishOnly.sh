7fe5039c395a0f98fa65faf0a5a0d6380abffce4
Merzough Münker <mmuenker@digitaix.com>
2023-08-01 16:46:23 +0200

    build: check before publish if some package.json files are incorrect

--- tools/scripts/lerna/hooks/prepublishOnly.sh
+++ tools/scripts/lerna/hooks/prepublishOnly.sh
@@ -12,3 +12,24 @@ rm -fr "${BASE_DIR}/dist/packages"
 yarn nx run-many \
   --target="build" \
   --configuration="production"
+
+# exit with error if some package.json files are missing the publishConfig
+
+# Finds all package.json files starting from the current directory and excluding those in node_modules directories.
+files=$(find dist/packages -name "node_modules" -prune -o -name "package.json" -print)
+
+hasError=false
+
+for file in $files
+do
+    # Checks if publishConfig or publishConfig.access is not properly set.
+    access=$(jq -r '.publishConfig.access // "invalid"' $file)
+    if [ "$access" != "public" ]; then
+        hasError=true
+        echo "publishConfig.access is not set or not equal to public in file: $file"
+    fi
+done
+
+if [ "$hasError" = true ] ; then
+    exit 1
+fi
