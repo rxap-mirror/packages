da7b709fa753428bd737df0785e6f2b75825df00
Merzough Münker <mmuenker@digitaix.com>
2023-08-01 14:56:58 +0200

    chore: use bootstrap utility class

--- demos/angular/src/main.ts
+++ demos/angular/src/main.ts
@@ -1,20 +1,35 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { ConfigService } from '@rxap/config';
+import { StandaloneApplication } from '@rxap/ngx-bootstrap';
 import { SentryInit } from '@rxap/ngx-sentry';
 import { OpenApiInit } from '@rxap/open-api';
 import { UnregisterServiceWorker } from '@rxap/service-worker';
+import {
+  LoggerModule,
+  NgxLoggerLevel,
+} from 'ngx-logger';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 import { environment } from './environment';
 
-UnregisterServiceWorker(environment);
-ConfigService.Config = {
+const staticConfig = {
   api: {
     'service-app-angular-table': {
       baseUrl: '/api/app/angular/table',
     },
   },
 };
-OpenApiInit();
-SentryInit(environment);
-bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
+
+const application = new StandaloneApplication(
+  environment,
+  AppComponent,
+  appConfig,
+  { static: staticConfig },
+);
+application.importProvidersFrom(LoggerModule.forRoot({
+  serverLoggingUrl: '/api/logs',
+  level: NgxLoggerLevel.DEBUG,
+  serverLogLevel: NgxLoggerLevel.ERROR,
+}));
+application.before(() => UnregisterServiceWorker(environment));
+application.before(() => OpenApiInit());
+application.before(() => SentryInit(environment));
+application.bootstrap().catch((err) => console.error(err));
