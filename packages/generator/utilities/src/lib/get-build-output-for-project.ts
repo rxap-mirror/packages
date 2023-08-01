ee66f84c0b37635b089f2f2f30ebeabb6439f146
Merzough Münker <mmuenker@digitaix.com>
2023-08-01 16:56:10 +0200

    fix: ensure the output path is a relative path

--- packages/generator/utilities/src/lib/get-build-output-for-project.ts
+++ packages/generator/utilities/src/lib/get-build-output-for-project.ts
@@ -1,6 +1,6 @@
 import { ProjectConfiguration } from '@nx/devkit';
 
-export function GetBuildOutputForProject(project: ProjectConfiguration, workspaceRoot: string) {
+export function GetBuildOutputForProject(project: ProjectConfiguration) {
   project.targets ??= {};
   if (!project.targets['build']) {
     throw new Error(`The project ${ project.name } has no build target. Can not determine the build output path.`);
@@ -10,8 +10,19 @@ export function GetBuildOutputForProject(project: ProjectConfiguration, workspac
     throw new Error(`The project ${ project.name } has no build outputs. Can not determine the build output path.`);
   }
   const [ output ] = outputs;
-  return output
-    .replace('{workspaceRoot}', workspaceRoot)
+  const cleanOutput = output
+    .replace('{workspaceRoot}', '')
     .replace('{projectRoot}', project.root)
-    .replace(/\{options\.(.+)}/, (_, option) => project.targets!['build'].options[option]);
+    .replace(/\{options\.(.+)}/, (_, option) => project.targets!['build'].options[option])
+    .replace(/^\//, '');
+
+  if (!cleanOutput) {
+    throw new Error(`The project ${ project.name } has an invalid build output path. Can not determine the build output path.`);
+  }
+
+  if (cleanOutput.startsWith('/')) {
+    throw new Error(`The project ${ project.name } has an absolute build output path. Can not determine the build output path.`);
+  }
+
+  return cleanOutput;
 }
