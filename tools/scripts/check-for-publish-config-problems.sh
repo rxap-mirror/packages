dc4f6e7256180b51f56a6ed46edde057ee0480e9
Merzough Münker <mmuenker@digitaix.com>
2023-08-01 17:24:40 +0200

    chore: only check packages projects

--- tools/scripts/check-for-publish-config-problems.sh
+++ tools/scripts/check-for-publish-config-problems.sh
@@ -5,7 +5,7 @@ BASE_DIR=$(git rev-parse --show-toplevel)
 cd "$BASE_DIR" || exit 1
 
 # Finds all package.json files starting from the current directory and excluding those in node_modules directories.
-files=$(find dist -name "node_modules" -prune -o -name "package.json" -print)
+files=$(find dist/packages -name "node_modules" -prune -o -name "package.json" -print)
 
 for file in $files
 do
