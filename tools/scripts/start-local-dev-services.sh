ab549a7db6a9fc15027b6847fa80ce8c2430c02e
Merzough Münker <mmuenker@digitaix.com>
2023-08-03 15:42:46 +0200

    chore: add local dev server utility scripts

--- tools/scripts/start-local-dev-services.sh
+++ tools/scripts/start-local-dev-services.sh
@@ -9,16 +9,33 @@ NC='\033[0m' # No Color
 
 cd "${GIT_ROOT}" || exit 1
 
-yarn nx g workspace-plugin:docker-compose
+yarn nx g @rxap/plugin-workspace:docker-compose --ignore-project service-configuration --ignore-project service-status
 
 # Get the current branch name or the commit hash if in detached HEAD state
 current_branch=$(git symbolic-ref --short -q HEAD || git rev-parse --short HEAD)
 
 channel=${current_branch//\//-}
 
-echo -e "${BLUE}test if $channel is a valid channel for registry ${REGISTRY:-dev-dockerhub.eurogard.cloud}${NC}"
+source .env
 
-docker pull ${REGISTRY:-dev-dockerhub.eurogard.cloud}/m2v-frontends/service/status:$channel && VALID=true || VALID=false
+CHANNEL=${CHANNEL:-$channel}
+
+echo "ROOT_DOMAIN=$ROOT_DOMAIN"
+echo "REMOTE_DOMAIN=$REMOTE_DOMAIN"
+echo "CHANNEL=$CHANNEL"
+echo "HOST_IP=$HOST_IP"
+echo "REGISTRY=$REGISTRY"
+echo "ROOT_DOMAIN_PORT=$ROOT_DOMAIN_PORT"
+
+read -p "Does everything look good (y/N)? "
+
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+  exit 1
+fi
+
+export CHANNEL
+
+docker compose -f docker-compose.yml -f docker-compose.services.yml -f docker-compose.frontends.yml pull && VALID=true || VALID=false
 
 if [[ $VALID == true ]]; then
   echo -e "${GREEN}channel $channel is valid${NC}"
@@ -31,30 +48,28 @@ else
     channel=development
   else
     echo "exiting"
-    exit 0
+    exit 1
   fi
 fi
 
-export CHANNEL=$channel
+export CHANNEL
 
-source .env
+docker compose -f docker-compose.yml -f docker-compose.services.yml -f docker-compose.frontends.yml pull && VALID=true || VALID=false
 
-echo "ROOT_DOMAIN=$ROOT_DOMAIN"
-echo "REMOTE_DOMAIN=$REMOTE_DOMAIN"
-echo "CHANNEL=$CHANNEL"
-echo "SERVICE_USER_SETTINGS_BASE_URL=$SERVICE_USER_SETTINGS_BASE_URL"
-echo "SERVICE_SERVER_BASE_URL=$SERVICE_SERVER_BASE_URL"
-echo "LEGACY_BASE_URL=$LEGACY_BASE_URL"
-echo "HOST_IP=$HOST_IP"
-echo "REGISTRY=$REGISTRY"
-echo "ROOT_DOMAIN_PORT=$ROOT_DOMAIN_PORT"
+if [[ $VALID == true ]]; then
+  echo -e "${GREEN}channel $channel is valid${NC}"
+else
+  echo -e "${RED}channel $channel is not valid${NC}"
 
-read -p "Does everything look good (y/N)? "
+  read -p "Do you want to try to build the images locally (y/N)? "
 
-if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-  exit 1
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    yarn nx run-many --target docker --configuration production --imageRegistry "$REGISTRY"
+  else
+    echo "exiting"
+    exit 1
+  fi
 fi
 
-docker compose stop traefik
-docker compose -f docker-compose.yml -f docker-compose.services.yml -f docker-compose.frontends.yml pull
+docker compose stop traefik || true
 docker compose -f docker-compose.yml -f docker-compose.services.yml -f docker-compose.frontends.yml up -d
