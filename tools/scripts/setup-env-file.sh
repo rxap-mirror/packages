2f910ef961a6fc9a705d45fa1fe048bca3edf89e
Merzough Münker <mmuenker@digitaix.com>
2023-08-03 12:50:04 +0200

    chore: add GL_TOKEN environment

--- tools/scripts/setup-env-file.sh
+++ tools/scripts/setup-env-file.sh
@@ -4,7 +4,10 @@ GIT_ROOT=$(git rev-parse --show-toplevel)
 
 lan_nip="127-0-0-1.nip.io"
 
+source "${GIT_ROOT}/.env"
+
 echo > "${GIT_ROOT}/.env"
+echo "GL_TOKEN=${GL_TOKEN}" >> "${GIT_ROOT}/.env"
 echo "GITLAB_HOST=https://gitlab.com" >> "${GIT_ROOT}/.env"
 echo "REMOTE_ALIAS=origin" >> "${GIT_ROOT}/.env"
 echo "ROOT_DOMAIN=${lan_nip}" >> "${GIT_ROOT}/.env"
