cb98a1aca8c2d439b58a99e32b9014919d0fcf3e
Merzough Münker <mmuenker@digitaix.com>
2023-08-14 11:43:33 +0200

    chore: add missing health controller

--- demos/service/app/angular/table/src/app/health/health.controller.ts
+++ demos/service/app/angular/table/src/app/health/health.controller.ts
@@ -1,12 +1,24 @@
 import {
   Controller,
+  Get,
   Inject,
 } from '@nestjs/common';
-import { HealthCheckService } from '@nestjs/terminus';
+import {
+  HealthCheck,
+  HealthCheckResult,
+  HealthCheckService,
+} from '@nestjs/terminus';
 
 @Controller('health')
 export class HealthController {
   constructor(
     @Inject(HealthCheckService) private readonly health: HealthCheckService,
   ) {}
+
+  @Get()
+  @HealthCheck()
+  public healthCheck(): Promise<HealthCheckResult> {
+    return this.health.check([]);
+  }
+
 }
