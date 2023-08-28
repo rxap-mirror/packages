f87881ac6f6265988101b74503f18796e425774c
Merzough Münker <mmuenker@digitaix.com>
2023-08-28 16:48:03 +0200

    chore: generate schematic factories

--- packages/plugin/nestjs/src/generators/init-application/generator.ts
+++ packages/plugin/nestjs/src/generators/init-application/generator.ts
@@ -1,4 +1,5 @@
 import {
+  generateFiles,
   getProjects,
   ProjectConfiguration,
   readNxJson,
@@ -12,14 +13,133 @@ import {
   SkipNonApplicationProject,
 } from '@rxap/generator-utilities';
 import {
+  CoerceAppGuardProvider,
+  CoerceImports,
+  CoerceNestAppController,
+  CoerceNestAppModule,
+  CoerceNestEnvironmentProvider,
+  CoerceNestLoggerProvider,
+  CoerceThrottlerModuleImport,
+  CoerceVariableDeclaration,
+} from '@rxap/ts-morph';
+import { TsMorphNestProjectTransform } from '@rxap/workspace-ts-morph';
+import {
+  AddPackageJsonDependency,
+  AddPackageJsonDevDependency,
   CoerceTarget,
   CoerceTargetDefaultsDependency,
+  Strategy,
 } from '@rxap/workspace-utilities';
-import { wrapAngularDevkitSchematic } from 'nx/src/adapter/ngcli-adapter';
 import { join } from 'path';
+import {
+  Project,
+  WriterFunction,
+  Writers,
+} from 'ts-morph';
 import { SkipNonNestProject } from '../../lib/skip-non-nest-project';
+import healthIndicatorInitGenerator from '../health-indicator-init/generator';
+import healthIndicatorGenerator from '../health-indicator/generator';
+import jwtGenerator from '../jwt/generator';
+import openApiGenerator from '../open-api/generator';
+import sentryGenerator from '../sentry/generator';
 import swaggerGenerator from '../swagger/generator';
+import validatorGenerator from '../validator/generator';
 import { InitApplicationGeneratorSchema } from './schema';
+
+function coerceEnvironmentFiles(tree: Tree, options: { project: string, sentry: boolean }) {
+
+  TsMorphNestProjectTransform(
+    tree,
+    {
+      project: options.project,
+    },
+    (project, [ sourceFile, prodSourceFile ]) => {
+
+      CoerceImports(sourceFile, {
+        moduleSpecifier: '@rxap/nest-utilities',
+        namedImports: [ 'Environment' ],
+      });
+      CoerceImports(prodSourceFile, {
+        moduleSpecifier: '@rxap/nest-utilities',
+        namedImports: [ 'Environment' ],
+      });
+
+      const baseEnvironment: Record<string, WriterFunction | string> = {
+        name: w => w.quote('development'),
+        production: 'false',
+      };
+
+      if (options.sentry) {
+        baseEnvironment['sentry'] = Writers.object({
+          enabled: 'false',
+          debug: 'false',
+        });
+      }
+
+      CoerceVariableDeclaration(sourceFile, 'environment', {
+        type: 'Environment',
+        initializer: Writers.object(baseEnvironment),
+      });
+
+      baseEnvironment['name'] = w => w.quote('production');
+      baseEnvironment['production'] = 'true';
+
+      if (options.sentry) {
+        baseEnvironment['sentry'] = Writers.object({
+          enabled: 'true',
+          debug: 'false',
+        });
+      }
+
+      CoerceVariableDeclaration(prodSourceFile, 'environment', {
+        type: 'Environment',
+        initializer: Writers.object(baseEnvironment),
+      });
+
+    },
+    [
+      '/environments/environment.ts?',
+      '/environments/environment.prod.ts?',
+    ],
+  );
+
+}
+
+function removeAppServiceFile(tree: Tree, projectSourceRoot: string) {
+
+  const appServiceFilePath = join(projectSourceRoot, 'app', 'app.service.ts');
+  if (tree.exists(appServiceFilePath)) {
+    if (tree.read(appServiceFilePath)?.toString('utf-8')?.includes('return { message: \'Hello API\' };')) {
+      console.warn('Remove the app service file');
+      tree.delete(appServiceFilePath);
+      if (tree.exists(join(projectSourceRoot, 'app', 'app.service.spec.ts'))) {
+        console.warn('Remove the app service spec file');
+        tree.delete(join(projectSourceRoot, 'app', 'app.service.spec.ts'));
+      } else {
+        console.warn('The app service spec file does not exists');
+      }
+    } else {
+      console.warn('The app service file does not contains the default method');
+    }
+  } else {
+    console.warn('The app service file does not exists');
+  }
+
+}
+
+function removeAppControllerSpecFile(tree: Tree, projectSourceRoot: string) {
+  const appControllerSpecFilePath = join(projectSourceRoot, 'app', 'app.controller.spec.ts');
+  if (tree.exists(appControllerSpecFilePath)) {
+    if (tree.read(appControllerSpecFilePath)?.toString('utf-8')?.includes('should return "Hello API"')) {
+      console.warn('Remove the app controller spec file');
+      tree.delete(appControllerSpecFilePath);
+    } else {
+      console.warn('The app controller spec file does not contains the default test');
+    }
+  } else {
+    console.warn('The app controller spec file does not exists');
+  }
+}
 
 function skipProject(
   tree: Tree,
@@ -76,10 +196,50 @@ function updateProjectTargets(project: ProjectConfiguration) {
     },
   });
 
+  CoerceTarget(project, 'build', {
+    options: {
+      generatePackageJson: true,
+      assets: [
+        join(project.sourceRoot!, 'Dockerfile'),
+        join(project.sourceRoot!, 'healthcheck.js'),
+      ],
+    },
+  }, Strategy.OVERWRITE);
+
+
 }
 
 function updateGitIgnore(tree: Tree, project: ProjectConfiguration) {
   CoerceIgnorePattern(tree, join(project.root, '.gitignore'), [ 'package.json' ]);
+}
+
+async function addDependencies(tree: Tree, options: InitApplicationGeneratorSchema) {
+
+  await AddPackageJsonDependency(tree, '@nestjs/throttler', 'latest', { soft: true });
+  await AddPackageJsonDependency(tree, 'cookie-parser', 'latest', { soft: true });
+  await AddPackageJsonDependency(tree, '@nestjs/config', 'latest', { soft: true });
+  await AddPackageJsonDependency(tree, 'joi', 'latest', { soft: true });
+  await AddPackageJsonDependency(tree, '@rxap/nest-utilities', 'latest', { soft: true });
+  await AddPackageJsonDependency(tree, '@rxap/nest-server', 'latest', { soft: true });
+  await AddPackageJsonDevDependency(tree, '@types/cookie-parser', 'latest', { soft: true });
+  await AddPackageJsonDevDependency(tree, '@types/csurf', 'latest', { soft: true });
+  await AddPackageJsonDevDependency(tree, '@rxap/plugin-application', 'latest', { soft: true });
+  await AddPackageJsonDevDependency(tree, '@rxap/plugin-docker', 'latest', { soft: true });
+
+  switch (options.platform) {
+
+    case 'express':
+      await AddPackageJsonDependency(tree, '@nestjs/platform-express', 'latest', { soft: true });
+      await AddPackageJsonDependency(tree, 'helmet', 'latest', { soft: true });
+      break;
+
+    case 'fastify':
+      await AddPackageJsonDependency(tree, '@nestjs/platform-fastify', 'latest', { soft: true });
+      await AddPackageJsonDependency(tree, '@fastify/helmet', 'latest', { soft: true });
+      break;
+
+  }
+
 }
 
 async function createOpenApiClientSdkLibrary(
@@ -148,6 +308,8 @@ export async function initApplicationGenerator(
 
   setGeneralTargetDefaults(tree);
 
+  await addDependencies(tree, options);
+
   const projects = getProjects(tree);
 
   for (const [ projectName, project ] of projects.entries()) {
@@ -158,6 +320,8 @@ export async function initApplicationGenerator(
 
     console.log(`init project: ${ projectName }`);
 
+    generateFiles(tree, join(__dirname, 'files'), project.sourceRoot, { tmpl: '' });
+
     updateProjectTargets(project);
     updateGitIgnore(tree, project);
     await createOpenApiClientSdkLibrary(tree, project, projects);
@@ -165,19 +329,77 @@ export async function initApplicationGenerator(
     // apply changes to the project configuration
     updateProjectConfiguration(tree, projectName, project);
 
-    if (options.swagger !== false) {
-      await swaggerGenerator(tree, { project: projectName });
+    coerceEnvironmentFiles(tree,
+      {
+        project: projectName,
+        sentry: options.sentry,
+      },
+    );
+    TsMorphNestProjectTransform(
+      tree,
+      { project: projectName },
+      (project: Project, [ moduleSourceFile, controllerSourceFile ]) => {
+        CoerceNestAppModule(moduleSourceFile);
+        CoerceNestAppController(controllerSourceFile);
+        CoerceThrottlerModuleImport(moduleSourceFile);
+        CoerceAppGuardProvider(moduleSourceFile);
+        CoerceNestEnvironmentProvider(moduleSourceFile);
+        CoerceNestLoggerProvider(moduleSourceFile);
+      },
+      [
+        '/app/app.module.ts',
+        '/app/app.controller.ts',
+      ],
+    );
+
+    const projectSourceRoot = project.sourceRoot;
+
+    if (!projectSourceRoot) {
+      throw new Error(`Can't find project source root for project ${ projectName }`);
     }
 
-    if (options.legacy) {
-      await wrapAngularDevkitSchematic(
-        '@rxap/schematic-nestjs',
-        'init',
-      )(tree, {
-        ...options,
-        swagger: false,
-        project: projectName,
-      });
+    removeAppServiceFile(tree, projectSourceRoot);
+    removeAppControllerSpecFile(tree, projectSourceRoot);
+
+    if (options.healthIndicator || options.healthIndicatorList?.length) {
+      if (options.healthIndicatorList?.length) {
+        for (const healthIndicator of options.healthIndicatorList) {
+          await healthIndicatorGenerator(tree,
+            {
+              name: healthIndicator,
+              project: projectName,
+            },
+          );
+        }
+      } else {
+        await healthIndicatorInitGenerator(tree, { project: projectName });
+      }
+    }
+
+    if (options.sentry) {
+      await sentryGenerator(tree,
+        {
+          project: projectName,
+          dsn: options.sentryDsn,
+          required: !!options.sentryDsn,
+        },
+      );
+    }
+
+    if (options.validator) {
+      await validatorGenerator(tree, { project: projectName });
+    }
+
+    if (options.openApi) {
+      await openApiGenerator(tree, { project: projectName });
+    }
+
+    if (options.jwt) {
+      await jwtGenerator(tree, { project: projectName });
+    }
+
+    if (options.swagger !== false) {
+      await swaggerGenerator(tree, { project: projectName });
     }
 
   }
