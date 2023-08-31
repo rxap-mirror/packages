b17cc380ecc8f405bf06550da209c1f449099ed7
Merzough Münker <mmuenker@digitaix.com>
2023-08-31 18:59:53 +0200

    fix: ensure the metadata object is set

--- packages/pattern/src/lib/method.ts
+++ packages/pattern/src/lib/method.ts
@@ -7,9 +7,9 @@ export interface Method<ReturnType = any, Parameter = any> extends MetaData {
 export function ToMethod<ReturnType = any, Parameter = any>(call: ((
   parameters?: Parameter,
   ...args: any[]
-) => Promise<ReturnType> | ReturnType)): Method<ReturnType, Parameter> {
+) => Promise<ReturnType> | ReturnType), metadata: any = { id: 'to-method' }): Method<ReturnType, Parameter> {
   return {
     call,
-    metadata: {},
+    metadata,
   };
 }
