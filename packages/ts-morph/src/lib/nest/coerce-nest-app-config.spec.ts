616a6f8a6510482fbfdd829b60cadd744766062f
Merzough Münker <mmuenker@digitaix.com>
2023-09-01 17:16:09 +0200

    test: support new overwrite option

--- packages/ts-morph/src/lib/nest/coerce-nest-app-config.spec.ts
+++ packages/ts-morph/src/lib/nest/coerce-nest-app-config.spec.ts
@@ -64,7 +64,7 @@ import { environment } from '../environments/environment';
 
 const validationSchema: SchemaMap = {};
 
-validationSchema['PORT'] = Joi.number();
+validationSchema['PORT'] = Joi.number().default(3309);
 validationSchema['GLOBAL_API_PREFIX'] = Joi.string().default('api/configuration');
 validationSchema['SENTRY_DSN'] = Joi.string();
 validationSchema['SENTRY_ENABLED'] = Joi.string().default(environment.sentry?.enabled ?? false);
@@ -77,7 +77,65 @@ validationSchema['STORAGE_DIR'] = Joi.string().default(join(__dirname, 'assets')
 
 export const VALIDATION_SCHEMA = Joi.object(validationSchema);`);
 
-    console.log('done');
+  });
+
+  it('should add replace env validator', () => {
+
+    const sourceCode = `import * as Joi from 'joi';
+import { SchemaMap } from 'joi';
+import { join } from 'path';
+import { environment } from '../environments/environment';
+
+const validationSchema: SchemaMap = {};
+
+validationSchema['PORT'] = Joi.number().default(3309);
+validationSchema['GLOBAL_API_PREFIX'] = Joi.string().default('api/configuration');
+validationSchema['SENTRY_DSN'] = Joi.string();
+validationSchema['SENTRY_ENABLED'] = Joi.string().default(environment.sentry?.enabled ?? false);
+validationSchema['SENTRY_ENVIRONMENT'] = Joi.string();
+validationSchema['SENTRY_RELEASE'] = Joi.string();
+validationSchema['SENTRY_SERVER_NAME'] = Joi.string().default(process.env.ROOT_DOMAIN ?? 'service-configuration');
+validationSchema['SENTRY_DEBUG'] = Joi.string().default(environment.sentry?.debug ?? false);
+validationSchema['DATA_DIR'] = Joi.string().default(environment.production ? '/app/assets' : join(__dirname, 'assets'));
+
+export const VALIDATION_SCHEMA = Joi.object(validationSchema);`;
+
+    const sourceFile = project.createSourceFile('app.config.ts', sourceCode);
+
+    CoerceNestAppConfig(sourceFile, {
+      itemList: [
+        {
+          name: 'PORT',
+          type: 'number',
+        },
+        {
+          name: 'STORAGE_DIR',
+          type: 'string',
+          defaultValue: 'join(__dirname, \'assets\')',
+        },
+      ],
+      overwrite: true,
+    });
+
+    expect(sourceFile.getFullText()).toEqual(`import * as Joi from 'joi';
+import { SchemaMap } from 'joi';
+import { join } from 'path';
+import { environment } from '../environments/environment';
+
+const validationSchema: SchemaMap = {};
+
+validationSchema['PORT'] = Joi.number();
+validationSchema['GLOBAL_API_PREFIX'] = Joi.string().default('api/configuration');
+validationSchema['SENTRY_DSN'] = Joi.string();
+validationSchema['SENTRY_ENABLED'] = Joi.string().default(environment.sentry?.enabled ?? false);
+validationSchema['SENTRY_ENVIRONMENT'] = Joi.string();
+validationSchema['SENTRY_RELEASE'] = Joi.string();
+validationSchema['SENTRY_SERVER_NAME'] = Joi.string().default(process.env.ROOT_DOMAIN ?? 'service-configuration');
+validationSchema['SENTRY_DEBUG'] = Joi.string().default(environment.sentry?.debug ?? false);
+validationSchema['DATA_DIR'] = Joi.string().default(environment.production ? '/app/assets' : join(__dirname, 'assets'));
+validationSchema['STORAGE_DIR'] = Joi.string().default(join(__dirname, 'assets'));
+
+export const VALIDATION_SCHEMA = Joi.object(validationSchema);`);
 
   });
 
