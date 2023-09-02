29ceeb0e99fe374d1a51a74ca1ed72d8487e999c
Merzough Münker <mmuenker@digitaix.com>
2023-09-02 14:36:27 +0200

    feat: support angular library entrypoints

--- packages/workspace/utilities/src/lib/build-angular-base-path.ts
+++ packages/workspace/utilities/src/lib/build-angular-base-path.ts
@@ -1,5 +1,6 @@
 import { join } from 'path';
 import {
+  GetProjectRoot,
   GetProjectSourceRoot,
   GetProjectType,
 } from './get-project';
@@ -10,18 +11,29 @@ export interface BuildAngularBasePathOptions {
   feature?: string | null;
   shared?: boolean;
   directory?: string | null;
+  entrypoint?: string | null;
 }
 
 export function BuildAngularBasePath<Tree extends TreeLike>(
   tree: Tree,
   options: Readonly<BuildAngularBasePathOptions>,
 ): string {
-  const { feature, shared } = options;
+  const { feature, shared, entrypoint } = options;
   let { project, directory } = options;
   directory ??= '';
   project = shared ? 'shared' : project;
-  const projectSourceRoot = GetProjectSourceRoot(tree, project);
   const type = GetProjectType(tree, project);
+  if (entrypoint) {
+    if (feature) {
+      throw new Error('The feature option is not supported with the entrypoint option');
+    }
+    if (type !== 'library') {
+      throw new Error('The entrypoint option is only supported for library project');
+    }
+    const projectRoot = GetProjectRoot(tree, project);
+    return join(projectRoot, entrypoint, 'src', 'lib', directory);
+  }
+  const projectSourceRoot = GetProjectSourceRoot(tree, project);
   if (feature) {
     return join(projectSourceRoot, type === 'library' ? 'lib' : '', 'feature', feature, directory);
   } else {
