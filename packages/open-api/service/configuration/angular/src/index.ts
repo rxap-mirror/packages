b0a8b490c9a3a62ee4b7893d4daa5cc1eca39f8a
Merzough Münker <mmuenker@digitaix.com>
2023-09-02 16:54:06 +0200

    build: add ng-package.json files and generate index

---
+++ packages/open-api/service/configuration/angular/src/index.ts
@@ -0,0 +1,23 @@
+// region remote-methods
+export * from './lib/remote-methods/app-controller-environment.remote-method';
+export * from './lib/remote-methods/app-controller-name.remote-method';
+export * from './lib/remote-methods/configuration-controller-get-latest.remote-method';
+export * from './lib/remote-methods/configuration-controller-get-version.remote-method';
+export * from './lib/remote-methods/health-controller-health-check.remote-method';
+// endregion
+
+// region directives
+export * from './lib/directives/app-controller-environment.directive';
+export * from './lib/directives/app-controller-name.directive';
+export * from './lib/directives/configuration-controller-get-latest.directive';
+export * from './lib/directives/configuration-controller-get-version.directive';
+export * from './lib/directives/health-controller-health-check.directive';
+// endregion
+
+// region data-sources
+export * from './lib/data-sources/app-controller-environment.data-source';
+export * from './lib/data-sources/app-controller-name.data-source';
+export * from './lib/data-sources/configuration-controller-get-latest.data-source';
+export * from './lib/data-sources/configuration-controller-get-version.data-source';
+export * from './lib/data-sources/health-controller-health-check.data-source';
+// endregion
---
+++ packages/open-api/service/configuration/nest/src/index.ts
@@ -0,0 +1,7 @@
+// region commands
+export * from './lib/commands/app-controller-environment.command';
+export * from './lib/commands/app-controller-name.command';
+export * from './lib/commands/configuration-controller-get-latest.command';
+export * from './lib/commands/configuration-controller-get-version.command';
+export * from './lib/commands/health-controller-health-check.command';
+// endregion
