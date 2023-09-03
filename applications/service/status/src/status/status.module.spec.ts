ab47a91c052b04c4805df3bd53dc1ead3d57ad34
Merzough Münker <mmuenker@digitaix.com>
2023-09-03 19:04:39 +0200

    build: use es module import style

--- applications/service/status/src/status/status.module.spec.ts
+++ applications/service/status/src/status/status.module.spec.ts
@@ -1,6 +1,6 @@
 import { INestApplication } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
-import * as request from 'supertest';
+import request from 'supertest';
 import { ServiceHealthIndicator } from './service.health-indicator';
 import { StatusModule } from './status.module';
 
