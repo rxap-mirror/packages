3ebbdd98ec87eee7e7fe6af68fd287ad083619cb
Merzough Münker <mmuenker@digitaix.com>
2023-09-03 20:36:21 +0200

    feat: support string interpolation

--- packages/plugin/library/src/executors/run-generator/executor.ts
+++ packages/plugin/library/src/executors/run-generator/executor.ts
@@ -1,4 +1,8 @@
 import { ExecutorContext } from '@nx/devkit';
+import {
+  GetProjectRoot,
+  GetProjectSourceRoot,
+} from '@rxap/plugin-utilities';
 import { IsRxapRepository } from '@rxap/workspace-utilities';
 import run from 'nx/src/executors/run-commands/run-commands.impl';
 import { RunGeneratorExecutorSchema } from './schema';
@@ -19,6 +23,23 @@ function buildParameters(options: Record<string, unknown> = {}): string {
   return params.join(' ');
 }
 
+function stringInterpolation(options: Record<string, any>, params: Record<string, string>) {
+  if (!options) {
+    return;
+  }
+  for (const [ key, value ] of Object.entries(options)) {
+    if (typeof value === 'string') {
+      options[key] = value.replace(/\{([^}]+)}/g, (match, property) => {
+        if (!params[property]) {
+          throw new Error(`Could not find property ${ property } in possible string interpolation parameters: ${ JSON.stringify(
+            params) }`);
+        }
+        return params[property];
+      });
+    }
+  }
+}
+
 export default async function runExecutor(options: RunGeneratorExecutorSchema, context: ExecutorContext) {
   console.log('Executor ran for RunGenerator', options);
 
@@ -35,6 +56,16 @@ export default async function runExecutor(options: RunGeneratorExecutorSchema, c
   if (!options.withoutProjectArgument) {
     options.options['project'] ??= context.projectName;
   }
+
+  stringInterpolation(options.options, {
+    workspaceRoot: context.root,
+    projectRoot: GetProjectRoot(context),
+    projectSourceRoot: GetProjectSourceRoot(context),
+    projectName: context.projectName,
+    configurationName: context.configurationName,
+    cwd: context.cwd,
+    targetName: context.targetName,
+  });
 
   command += ` ${ buildParameters(options.options) }`;
 
