6c8560a06bcdc80bd02d98b738bfa63965bd3ee2
Merzough Münker <mmuenker@digitaix.com>
2023-09-07 14:24:15 +0200

    fix: ensure the username is always fully visible

--- packages/angular/layout/src/lib/header/user-profile-icon/user-profile-icon.component.cy.ts
+++ packages/angular/layout/src/lib/header/user-profile-icon/user-profile-icon.component.cy.ts
@@ -1,8 +1,48 @@
 import { TestBed } from '@angular/core/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { faker } from '@faker-js/faker';
+import {
+  RxapAuthenticationService,
+  RxapUserProfileService,
+} from '@rxap/authentication';
+import { IconModule } from '@rxap/icon';
+import { BehaviorSubject } from 'rxjs';
 import { UserProfileIconComponent } from './user-profile-icon.component';
 
 describe(UserProfileIconComponent.name, () => {
+
+  const profile = {
+    username: faker.internet.email(),
+  };
+  const userProfileService = {
+    getProfile() {
+      return Promise.resolve(profile);
+    },
+  };
+  const authenticationService = {
+    isAuthenticated$: new BehaviorSubject(true),
+    signOut() {
+      return Promise.resolve();
+    },
+  };
+
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        IconModule,
+        NoopAnimationsModule,
+      ],
+      providers: [
+        {
+          provide: RxapUserProfileService,
+          useValue: userProfileService,
+        },
+        {
+          provide: RxapAuthenticationService,
+          useValue: authenticationService,
+        },
+      ],
+    });
     TestBed.overrideComponent(UserProfileIconComponent, {
       add: {
         imports: [],
@@ -14,4 +54,14 @@ describe(UserProfileIconComponent.name, () => {
   it('renders', () => {
     cy.mount(UserProfileIconComponent);
   });
+
+  it('long username', () => {
+    profile.username = faker.lorem.words(5).replace(/\s/g, '');
+    cy.mount(UserProfileIconComponent).then(response => {
+      response.fixture.whenStable().then(() => {
+        cy.get('button').click();
+      });
+    });
+  });
+
 });
