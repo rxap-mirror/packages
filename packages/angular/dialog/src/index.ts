00e93043f9fab815f10ffa1b596d52dbb9f85e01
Merzough Münker <mmuenker@digitaix.com>
2023-09-08 14:51:49 +0200

    fix: remove unused code

--- packages/angular/dialog/src/index.ts
+++ packages/angular/dialog/src/index.ts
@@ -1,21 +1,12 @@
-export * from './lib/error-dialog/error-dialog.module';
-export * from './lib/error-dialog/error-dialog.component';
-export * from './lib/error-dialog/error-dialog.error-handler';
-
-export * from './lib/confirm-dialog/confirm-dialog.component.module';
-export * from './lib/confirm-dialog/confirm-dialog.component';
-export * from './lib/confirm-dialog/confirm-dialog.service';
-
+// region message-dialog
 export * from './lib/message-dialog/message-dialog.component.module';
 export * from './lib/message-dialog/message-dialog.component';
 export * from './lib/message-dialog/message-dialog.service';
+export * from './lib/message-dialog/types';
+// endregion
 
-// region http error dialog
-
-export * from './lib/http-error-message/http-error-message.component';
-export * from './lib/http-error-message/http-error-message.component.module';
-export * from './lib/http-error-message/http-error-message.interceptor';
-export * from './lib/http-error-message/http-error-message.service';
-export * from './lib/http-error-message/tokens';
-
+// region confirm-dialog
+export * from './lib/confirm-dialog/confirm-dialog.component.module';
+export * from './lib/confirm-dialog/confirm-dialog.component';
+export * from './lib/confirm-dialog/confirm-dialog.service';
 // endregion
