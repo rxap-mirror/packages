25a32a7c20e7203412c0e5c720278884a5dd2a25
Merzough Münker <mmuenker@digitaix.com>
2023-09-08 08:43:32 +0200

    fix: disable schema validation by default

--- packages/angular/open-api/src/lib/tokens.ts
+++ packages/angular/open-api/src/lib/tokens.ts
@@ -5,5 +5,11 @@ export const RXAP_OPEN_API_STRICT_VALIDATOR = new InjectionToken('rxap/open-api/
 export const DEFAULT_OPEN_API_REMOTE_METHOD_META_DATA = new InjectionToken(
   'rxap/open-api/default-remote-method-meta-data');
 export const DEFAULT_OPEN_API_DATA_SOURCE_META_DATA = new InjectionToken('rxap/open-api/default-data-source-meta-data');
-export const DISABLE_SCHEMA_VALIDATION = new InjectionToken<boolean>('rxap/open-api/disable-schema-validation');
-export const DISABLE_VALIDATION = new InjectionToken<boolean>('rxap/open-api/disable-validation');
+export const DISABLE_SCHEMA_VALIDATION = new InjectionToken<boolean>('rxap/open-api/disable-schema-validation', {
+  providedIn: 'root',
+  factory: () => true,
+});
+export const DISABLE_VALIDATION = new InjectionToken<boolean>('rxap/open-api/disable-validation', {
+  providedIn: 'root',
+  factory: () => true,
+});
