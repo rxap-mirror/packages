f5ab21a16328f2c99d98d56fcfa005589d5edd30
Merzough Münker <mmuenker@digitaix.com>
2023-09-08 15:02:18 +0200

    fix: use new error providers and interceptors

--- demos/angular/src/app/app.config.ts
+++ demos/angular/src/app/app.config.ts
@@ -1,14 +1,18 @@
-import { HttpClientModule } from '@angular/common/http';
 import {
-  ApplicationConfig,
-  importProvidersFrom,
-} from '@angular/core';
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
+import { ApplicationConfig } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import {
   provideRouter,
   withEnabledBlockingInitialNavigation,
 } from '@angular/router';
 import { RXAP_ENVIRONMENT } from '@rxap/environment';
+import {
+  HttpErrorInterceptor,
+  ProvideErrorHandler,
+} from '@rxap/ngx-error';
 import { SERVICE_STATUS_CHECK_METHOD } from '@rxap/ngx-status-check';
 import { StatusControllerHealthCheckRemoteMethod } from 'open-api-service-status/remote-methods/status-controller-health-check.remote-method';
 import { environment } from '../environments/environment';
@@ -16,9 +20,10 @@ import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(HttpClientModule),
+    provideHttpClient(withInterceptors([ HttpErrorInterceptor ])),
     provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
     provideAnimations(),
+    ProvideErrorHandler(),
     {
       provide: RXAP_ENVIRONMENT,
       useValue: environment,
