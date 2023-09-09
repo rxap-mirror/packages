8f3dcacd2e6aa99ae9ce8eec61578b44e824635b
Merzough Münker <mmuenker@digitaix.com>
2023-09-09 18:30:55 +0200

    fix: return the new dark mode value

--- applications/service/user/src/settings/dark-mode.controller.ts
+++ applications/service/user/src/settings/dark-mode.controller.ts
@@ -6,7 +6,6 @@ import {
   Put,
 } from '@nestjs/common';
 import { UserSub } from '@rxap/nest-jwt';
-import { UserSettings } from './settings';
 import { SettingsService } from './settings.service';
 
 @Controller('settings/dark-mode')
@@ -21,7 +20,7 @@ export class DarkModeController {
   @Put('toggle')
   public async toggle(
     @UserSub() userId: string,
-  ): Promise<UserSettings> {
+  ): Promise<boolean> {
     this.logger.verbose(`toggle dark mode for user '${ userId }'`, 'DarkModeController');
     const settings = await this.userSettings.get(userId);
     if (settings.darkMode) {
@@ -34,23 +33,23 @@ export class DarkModeController {
   @Put('enable')
   public async enable(
     @UserSub() userId: string,
-  ): Promise<UserSettings> {
+  ): Promise<boolean> {
     const settings = await this.userSettings.get(userId);
     settings.darkMode = true;
     await this.userSettings.set(userId, settings);
     this.logger.verbose(`enable dark mode for user '${ userId }'`, 'DarkModeController');
-    return this.userSettings.get(userId);
+    return settings.darkMode;
   }
 
   @Put('disable')
   public async disable(
     @UserSub() userId: string,
-  ): Promise<UserSettings> {
+  ): Promise<boolean> {
     const settings = await this.userSettings.get(userId);
     settings.darkMode = false;
     await this.userSettings.set(userId, settings);
     this.logger.verbose(`disable dark mode for user '${ userId }'`, 'DarkModeController');
-    return this.userSettings.get(userId);
+    return settings.darkMode;
   }
 
   @Get()
