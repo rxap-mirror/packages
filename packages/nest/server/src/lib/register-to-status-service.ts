d61af7b94e0bc045bc4d85574c6aeb5909c73fe3
Merzough Münker <mmuenker@digitaix.com>
2023-09-09 13:28:56 +0200

    fix: retry service register if failed

--- packages/nest/server/src/lib/register-to-status-service.ts
+++ packages/nest/server/src/lib/register-to-status-service.ts
@@ -9,17 +9,22 @@ export async function RegisterToStatusService(
   config: ConfigService,
   logger: Logger,
 ) {
-  try {
-    const statusServiceBaseUrl = config.getOrThrow('STATUS_SERVICE_BASE_URL');
-    const requestUrl = `${ statusServiceBaseUrl }/register`;
-    const port = config.getOrThrow('PORT');
-    logger.debug(`Register service: ${ requestUrl } for port: ${ port }`, 'Ready');
-    await axios.post(requestUrl, {
-      name: this.environment.app,
-      port,
-    });
-    logger.log('Service registered', 'Ready');
-  } catch (e: any) {
-    logger.error(`Failed to register service: ${ e.message }`, undefined, 'Ready');
-  }
+  const statusServiceBaseUrl = config.getOrThrow('STATUS_SERVICE_BASE_URL');
+  const requestUrl = `${ statusServiceBaseUrl }/register`;
+  const port = config.getOrThrow('PORT');
+  logger.debug(`Register service: ${ requestUrl } for port: ${ port }`, 'Ready');
+  let ready = false;
+  let counter = 0;
+  do {
+    try {
+      await axios.post(requestUrl, {
+        name: this.environment.app,
+        port,
+      });
+      ready = true;
+      logger.log('Service registered', 'Ready');
+    } catch (e: any) {
+      logger.error(`Failed to register service (${ counter++ }): ${ e.message }`, undefined, 'Ready');
+    }
+  } while (!ready && await new Promise((resolve) => setTimeout(() => resolve(true), 15 * 1000)));
 }
