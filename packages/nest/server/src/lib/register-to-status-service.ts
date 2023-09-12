1fdae7f3af3d5defc4b810fa2411ed7c5c7e5c97
Merzough Münker <mmuenker@digitaix.com>
2023-09-12 11:20:23 +0200

    fix: use factory function

--- packages/nest/server/src/lib/register-to-status-service.ts
+++ packages/nest/server/src/lib/register-to-status-service.ts
@@ -1,30 +1,37 @@
 import { Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { Environment } from '@rxap/environment';
 import axios from 'axios';
-import { Server } from './server';
 
-export async function RegisterToStatusService(
-  this: Server<any, any, any>,
-  _: any,
-  config: ConfigService,
-  logger: Logger,
-) {
-  const statusServiceBaseUrl = config.getOrThrow('STATUS_SERVICE_BASE_URL');
-  const requestUrl = `${ statusServiceBaseUrl }/register`;
-  const port = config.getOrThrow('PORT');
-  logger.debug(`Register service: ${ requestUrl } for port: ${ port }`, 'Ready');
-  let ready = false;
-  let counter = 0;
-  do {
-    try {
-      await axios.post(requestUrl, {
-        name: this.environment.app,
-        port,
-      });
-      ready = true;
-      logger.log('Service registered', 'Ready');
-    } catch (e: any) {
-      logger.error(`Failed to register service (${ counter++ }): ${ e.message }`, undefined, 'Ready');
-    }
-  } while (!ready && await new Promise((resolve) => setTimeout(() => resolve(true), 15 * 1000)));
+export interface RegisterToStatusServiceOptions {
+  registerPath?: string;
+}
+
+export function RegisterToStatusService({ registerPath = '/register' }: RegisterToStatusServiceOptions = {}) {
+  return async (
+    _: any,
+    config: ConfigService,
+    logger: Logger,
+    __: any,
+    environment: Environment,
+  ) => {
+    const statusServiceBaseUrl = config.getOrThrow('STATUS_SERVICE_BASE_URL');
+    const requestUrl = `${ statusServiceBaseUrl }${ registerPath }`;
+    const port = config.getOrThrow('PORT');
+    logger.debug(`Register service: ${ requestUrl } for port: ${ port }`, 'Ready');
+    let ready = false;
+    let counter = 0;
+    do {
+      try {
+        await axios.post(requestUrl, {
+          name: environment.app,
+          port,
+        });
+        ready = true;
+        logger.log('Service registered', 'Ready');
+      } catch (e: any) {
+        logger.error(`Failed to register service (${ counter++ }): ${ e.message }`, undefined, 'Ready');
+      }
+    } while (!ready && await new Promise((resolve) => setTimeout(() => resolve(true), 15 * 1000)));
+  };
 }
