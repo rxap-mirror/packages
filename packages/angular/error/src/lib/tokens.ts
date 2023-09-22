284ccd658576cdb4d8dbf2f2a7c10717de5c1e16
Merzough Münker <mmuenker@digitaix.com>
2023-09-22 08:58:02 +0200

    fix: update token description

--- packages/angular/error/src/lib/tokens.ts
+++ packages/angular/error/src/lib/tokens.ts
@@ -6,12 +6,12 @@ import {
 import { IErrorDialogComponent } from './error-dialog/error-dialog.component';
 import { ErrorInterceptorOptions } from './error-interceptor-options';
 
-export const RXAP_ERROR_DIALOG_DATA = new InjectionToken<Signal<any[]>>('rxap/http-error-message/dialog-data');
+export const RXAP_ERROR_DIALOG_DATA = new InjectionToken<Signal<any[]>>('rxap/error/dialog-data');
 
-export const RXAP_ERROR_DIALOG_ERROR = new InjectionToken<any>('rxap/http-error-message/dialog-data');
+export const RXAP_ERROR_DIALOG_ERROR = new InjectionToken<any>('rxap/error/dialog-error');
 
 export const RXAP_ERROR_DIALOG_COMPONENT = new InjectionToken<ComponentType<IErrorDialogComponent>>(
-  'rxap/http-error-message/dialog-component');
+  'rxap/error/dialog-component');
 
 export const RXAP_ERROR_INTERCEPTOR_OPTIONS = new InjectionToken<ErrorInterceptorOptions>(
-  'rxap/http-error-message/interceptor-options');
+  'rxap/error/interceptor-options');
