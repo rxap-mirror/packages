e7de358bffdf241361a69ca3e4ff1122afc18131
Merzough Münker <mmuenker@digitaix.com>
2023-09-27 16:24:10 +0200

    fix: update utility scripts

---
+++ packages/plugin/application/src/generators/init/files/general/tools/scripts/dist-node-modules-linking.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+BASE_DIR=$(git rev-parse --show-toplevel)
+
+cd "$BASE_DIR" || exit 1
+
+# define directories
+dist_dir="dist"
+packages_dir="${dist_dir}/packages"
+node_modules_dir="${dist_dir}/node_modules/@rxap"
+
+# create node_modules directory if it doesn't exist
+mkdir -p "${node_modules_dir}"
+
+# loop over all package.json files
+find "${packages_dir}" -name "package.json" | while read package_json; do
+  # extract package directory
+  package_dir=$(dirname "${package_json}")
+
+  # extract package name, replace '/' with '-', remove the scope @rxap
+  package_name=$(jq -r .name "${package_json}" | tr '/' '-' | sed 's/@rxap-//')
+
+  rm -f "${node_modules_dir}/${package_name}" || true
+  # create symlink
+  echo "create symlink ${package_dir} -> ${node_modules_dir}/${package_name}"
+  ln -s "../../../${package_dir}" "${node_modules_dir}/${package_name}"
+done
--- packages/plugin/application/src/generators/init/files/general/tools/scripts/schematic.sh
+++ packages/plugin/application/src/generators/init/files/general/tools/scripts/schematic.sh
@@ -17,7 +17,7 @@ echo "externalSchematic: $externalSchematic"
 IFS=':'
 
 # Use the read command to split the value into two variables
-read -r package schematic <<< "$1"
+read -r package schematic <<<"$1"
 
 echo "package: $package"
 echo "schematic: $schematic"
@@ -28,64 +28,64 @@ shift
 echo "arguments: $@"
 
 # Search for package.json files in the current directory and its subdirectories
-find packages -name "package.json" -type f | while read -r file; do
-    # Check if the name property equals to $package
-    if [ "$(jq -r .name "$file")" = "$package" ]; then
-        # Get the directory of the matched package.json
-        dir=$(dirname "$file")
+find . \( -type d \( -name node_modules -o -name dist -o -name .angular \) \) -prune -o -name "package.json" -type f | while read -r file; do
+  # Check if the name property equals to $package
+  if [ "$(jq -r .name "$file")" = "$package" ]; then
+    # Get the directory of the matched package.json
+    dir=$(dirname "$file")
 
-        # Read the name property from the project.json file in the same directory
-        if [ -f "$dir/project.json" ]; then
-            name=$(jq -r .name "$dir/project.json")
+    # Read the name property from the project.json file in the same directory
+    if [ -f "$dir/project.json" ]; then
+      name=$(jq -r .name "$dir/project.json")
 
-            # Print out the name for verification
-            echo "Name from project.json: $name"
+      # Print out the name for verification
+      echo "Name from project.json: $name"
 
-            # If you want to use this variable outside the while loop you would need to
-            # write it to a file or pass it to a command as argument here,
-            # because each iteration in the loop runs in a subshell
-            echo "$name" > /tmp/name.txt
-        else
-            echo "No project.json file found in $dir"
-            exit 1
-        fi
+      # If you want to use this variable outside the while loop you would need to
+      # write it to a file or pass it to a command as argument here,
+      # because each iteration in the loop runs in a subshell
+      echo "$name" >/tmp/name.txt
+    else
+      echo "No project.json file found in $dir"
+      exit 1
     fi
+  fi
 done
 
 # test if /tmp/name.txt exists
 if [ ! -f /tmp/name.txt ]; then
-    echo "No package found with name $package"
-    exit 1
+  echo "No package found with name $package"
+  exit 1
 fi
 
 # Read the name from the file
 name=$(cat /tmp/name.txt)
 
 if [ -z "$name" ]; then
-    echo "No package found with name $package"
-    exit 1
+  echo "No package found with name $package"
+  exit 1
 fi
 
 echo "Build project $name"
 
 yarn nx run "$name:build"
 
-# bash tools/scripts/dist-node-modules-linking.sh
+bash tools/scripts/dist-node-modules-linking.sh
 
 # Search for package.json files in the current directory and its subdirectories
-find dist/packages -name "package.json" -type f | while read -r file; do
-    # Check if the name property equals to $package
-    if [ "$(jq -r .name "$file")" = "$package" ]; then
-        # Get the directory of the matched package.json
-        dir=$(dirname "$file")
+find dist -type d -name node_modules -prune -o -name "package.json" -type f | while read -r file; do
+  # Check if the name property equals to $package
+  if [ "$(jq -r .name "$file")" = "$package" ]; then
+    # Get the directory of the matched package.json
+    dir=$(dirname "$file")
 
-        echo "Output directory for the package: $dir"
+    echo "Output directory for the package: $dir"
 
-        # If you want to use this variable outside the while loop you would need to
-        # write it to a file or pass it to a command as argument here,
-        # because each iteration in the loop runs in a subshell
-        echo "$dir" > /tmp/dir.txt
-    fi
+    # If you want to use this variable outside the while loop you would need to
+    # write it to a file or pass it to a command as argument here,
+    # because each iteration in the loop runs in a subshell
+    echo "$dir" >/tmp/dir.txt
+  fi
 done
 
 echo "detect $package in $dir"
@@ -94,8 +94,8 @@ echo "detect $package in $dir"
 dir=$(cat /tmp/dir.txt)
 
 if [ -z "$dir" ]; then
-    echo "No output dir for package found with name $package"
-    exit 1
+  echo "No output dir for package found with name $package"
+  exit 1
 fi
 
 yarn nx g "./$dir:$schematic" "$@"
