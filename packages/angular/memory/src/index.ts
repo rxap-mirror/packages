3624989337f8a8a36afaa88562ad083350a61143
Merzough Münker <mmuenker@digitaix.com>
2023-09-29 19:08:30 +0200

    build: generate index

--- packages/angular/memory/src/index.ts
+++ packages/angular/memory/src/index.ts
@@ -1,1 +1,12 @@
-export {};
+// region decorators
+export * from './lib/decorators/local-storage';
+export * from './lib/decorators/session-storage';
+export * from './lib/decorators/web-storage';
+// endregion
+
+// region 
+export * from './lib/local-storage.service';
+export * from './lib/session-storage.service';
+export * from './lib/storage-utility';
+export * from './lib/storage.service';
+// endregion
