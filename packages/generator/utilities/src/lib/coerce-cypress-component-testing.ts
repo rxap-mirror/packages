fb42a6f8f47ac3e5851217cf20d5386e71d80255
Merzough Münker <mmuenker@digitaix.com>
2023-10-02 20:24:47 +0200

    fix: ensure the cypress build target project is an angular application project

--- packages/generator/utilities/src/lib/coerce-cypress-component-testing.ts
+++ packages/generator/utilities/src/lib/coerce-cypress-component-testing.ts
@@ -1,23 +1,66 @@
 import { cypressComponentConfiguration } from '@nx/angular/generators';
 import {
+  getProjects,
   ProjectConfiguration,
   readProjectConfiguration,
   Tree,
   updateProjectConfiguration,
 } from '@nx/devkit';
+import {
+  IsAngularProject,
+  IsApplicationProject,
+} from '@rxap/workspace-utilities';
 import { join } from 'path';
+
+function determineCypressBuildTargetProject(tree: Tree, project: ProjectConfiguration, projectName: string): string {
+
+  if (IsApplicationProject(project)) {
+    return projectName;
+  }
+
+  const projects = getProjects(tree);
+
+  const angularApplicationProjects = Array.from(projects.entries())
+    .filter(([ , project ]) => IsApplicationProject(project) && IsAngularProject(project));
+
+  const cypressProject = angularApplicationProjects.find(([ projectName ]) => projectName.endsWith('cypress'));
+
+  if (cypressProject) {
+    return cypressProject[0];
+  }
+
+  if (angularApplicationProjects.length) {
+    return angularApplicationProjects[0][0];
+  } else {
+    throw new Error('No angular application project found. Can not determine the cypress build target project.');
+  }
+
+}
 
 export async function CoerceCypressComponentTesting(tree: Tree, project: ProjectConfiguration, projectName: string) {
 
   project.targets ??= {};
 
   if (!project.targets['component-test']) {
+    const buildTargetProjectName = determineCypressBuildTargetProject(tree, project, projectName);
     await cypressComponentConfiguration(tree, {
       project: projectName,
       generateTests: true,
       skipFormat: false,
-      buildTarget: `${ projectName }:build:development`,
+      buildTarget: `${ buildTargetProjectName }:build:development`,
     });
+    if (buildTargetProjectName !== projectName) {
+      const buildTargetProject = readProjectConfiguration(tree, buildTargetProjectName);
+      buildTargetProject.implicitDependencies ??= [];
+      if (!buildTargetProject.implicitDependencies.includes(projectName)) {
+        buildTargetProject.implicitDependencies.push(projectName);
+        updateProjectConfiguration(
+          tree,
+          buildTargetProjectName,
+          buildTargetProject,
+        );
+      }
+    }
     const _project = readProjectConfiguration(tree, projectName);
     _project.targets ??= {};
     _project.targets['component-test'].configurations ??= {};
