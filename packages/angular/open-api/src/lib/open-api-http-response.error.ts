5f869eae5ebfed76d81ec49956b364d0cbc22b53
Merzough Münker <mmuenker@digitaix.com>
2023-10-02 17:04:06 +0200

    fix: ensure the OpenApiHttpResponseError is instance of HttpErrorResponse

--- packages/angular/open-api/src/lib/open-api-http-response.error.ts
+++ packages/angular/open-api/src/lib/open-api-http-response.error.ts
@@ -1,21 +1,23 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { OpenApiMetaData } from './open-api.meta-data';
 
-export class OpenApiHttpResponseError extends Error {
+export class OpenApiHttpResponseError extends HttpErrorResponse {
 
   constructor(
     public readonly httpErrorResponse: HttpErrorResponse,
     public readonly metadata: OpenApiMetaData,
   ) {
-    super(httpErrorResponse.message);
+    super({
+      error: httpErrorResponse.error,
+      headers: httpErrorResponse.headers,
+      status: httpErrorResponse.status,
+      statusText: httpErrorResponse.statusText,
+      url: httpErrorResponse.url ?? undefined,
+    });
     if ((Error as any)['captureStackTrace']) {
       (Error as any)['captureStackTrace'](this, OpenApiHttpResponseError);
     }
-    this.name = 'OpenApiHttpResponseError';
-  }
-
-  get status() {
-    return this.httpErrorResponse.status;
+    Reflect.set(this, 'name', 'OpenApiHttpResponseError');
   }
 
   get operationId() {
