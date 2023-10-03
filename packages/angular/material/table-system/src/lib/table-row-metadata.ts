e447c7a66a4a5c7302e88e4c91a6e60bbdc05ebd
Merzough Münker <mmuenker@digitaix.com>
2023-10-03 13:17:54 +0200

    fix: introduce the NormalizeTableRow function

--- packages/angular/material/table-system/src/lib/table-row-metadata.ts
+++ packages/angular/material/table-system/src/lib/table-row-metadata.ts
@@ -1,7 +1,47 @@
 import { ToggleSubject } from '@rxap/rxjs';
 
 export interface TableRowMetadata {
+  /**
+   * the unique id of the row. Use to track the row in the table for change detection and other purposes
+   */
+  __rowId: string;
+  /**
+   * metadata for the row. Added by the table system to facilitate some features
+   */
   __metadata__: {
     loading$: ToggleSubject;
   };
+}
+
+export type RawTableRow<T extends TableRowMetadata> = Omit<T, '__metadata__' | '__rowId'>
+  & Partial<Pick<TableRowMetadata, '__rowId' | '__metadata__'>>;
+
+export type RowIdMapperFunction<T extends TableRowMetadata> = (row: RawTableRow<T>) => string;
+
+/**
+ * Normalize the table row. Add the __rowId and __metadata__ properties to the row.
+ *
+ * CAUTION: This function mutate the row object
+ *
+ * @template T extends TableRowMetadata
+ * @param row
+ * @param rowId
+ * @returns T the normalized row
+ */
+export function NormalizeTableRow<T extends TableRowMetadata>(
+  row: RawTableRow<T>,
+  rowId: string,
+): T;
+export function NormalizeTableRow<T extends TableRowMetadata>(
+  row: RawTableRow<T>,
+  rowIdMapper: RowIdMapperFunction<T>,
+): T;
+export function NormalizeTableRow<T extends TableRowMetadata>(
+  row: RawTableRow<T>,
+  rowIdOrMapper: string | RowIdMapperFunction<T>,
+): T {
+  row.__rowId = typeof rowIdOrMapper === 'string' ? rowIdOrMapper : rowIdOrMapper(row);
+  row.__metadata__ ??= { loading$: new ToggleSubject() };
+  row.__metadata__.loading$ ??= new ToggleSubject();
+  return row as T;
 }
