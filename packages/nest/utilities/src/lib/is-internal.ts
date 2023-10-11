85feed1db665b604667c371cbd7f6d72ae71f8a3
Merzough Münker <mmuenker@digitaix.com>
2023-10-11 18:44:06 +0200

    fix: use symbol instead of a string

--- packages/nest/utilities/src/lib/is-internal.ts
+++ packages/nest/utilities/src/lib/is-internal.ts
@@ -1,4 +1,4 @@
 import { SetMetadata } from '@nestjs/common';
 
-export const IS_INTERNAL_KEY = 'isInternal';
+export const IS_INTERNAL_KEY = Symbol('isInternal');
 export const Internal = () => SetMetadata(IS_INTERNAL_KEY, true);
