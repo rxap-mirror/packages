400cdd86206fea5f79e0376179cd41c9d2a08d5c
Merzough Münker <mmuenker@digitaix.com>
2023-10-12 11:13:06 +0200

    chore: add package version check utilities

--- tools/scripts/enforce-latest-package-version.sh
+++ tools/scripts/enforce-latest-package-version.sh
@@ -4,27 +4,12 @@
 # the latest version is determined by the appointed dist tag
 # the digest tag is derived from the current branch name
 
-BLACK='\033[0;30m'
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-BLUE='\033[0;34m'
-MAGENTA='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHT_GRAY='\033[0;37m'
-GRAY='\033[0;90m'
-LIGHT_RED='\033[0;91m'
-LIGHT_GREEN='\033[0;92m'
-LIGHT_YELLOW='\033[0;93m'
-LIGHT_BLUE='\033[0;94m'
-LIGHT_MAGENTA='\033[0;95m'
-LIGHT_CYAN='\033[0;96m'
-WHITE='\033[0;97m'
-NC='\033[0m'  # No Color
-
 GIT_ROOT=$(git rev-parse --show-toplevel)
 
 cd "${GIT_ROOT}" || exit 1
+
+source "./tools/scripts/semver.sh"
+source "./tools/scripts/colors.sh"
 
 # Get the current branch name or the commit hash if in detached HEAD state
 current_branch=$(git symbolic-ref --short -q HEAD || git rev-parse --short HEAD)
@@ -48,17 +33,19 @@ FILE_PATH=$1
 # Extract package name and version from package.json
 PACKAGE_NAME=$(jq -r '.name' $FILE_PATH)
 CURRENT_VERSION=$(jq -r '.version' $FILE_PATH)
+IS_PRIVATE=$(jq -r '.private' $FILE_PATH)
+
+if [ "$IS_PRIVATE" == "true" ]; then
+    echo -e "${LIGHT_YELLOW}Your package is private. Skipping version check.${NC}"
+    exit 0
+fi
 
 if [ "$PACKAGE_NAME" == "null" ]; then
-    echo -e "${RED}package.json is not valid!${NC}"
-    echo "path: $FILE_PATH"
-    exit 1
+    exit 0
 fi
 
 if [ "$CURRENT_VERSION" == "null" ]; then
-    echo -e "${RED}package.json is not valid!${NC}"
-    echo "path: $FILE_PATH"
-    exit 1
+    exit 0
 fi
 
 echo "Checking for updates for $PACKAGE_NAME with dist tag $NPM_DIST_TAG..."
@@ -66,13 +53,18 @@ echo "Checking for updates for $PACKAGE_NAME with dist tag $NPM_DIST_TAG..."
 # Fetch the latest published version from npm registry
 LATEST_VERSION=$(curl -s https://registry.npmjs.org/$PACKAGE_NAME | jq -r --arg dist "$NPM_DIST_TAG"  '.["dist-tags"][$dist]')
 
-# Compare the current version with the latest published version
-if [[ $CURRENT_VERSION == $LATEST_VERSION ]]; then
+if semverEQ "$CURRENT_VERSION" "$LATEST_VERSION"; then
     echo -e "${GREEN}Your package is up to date. Current version: ${CURRENT_VERSION}${NC}"
-else
-    echo -e "${YELLOW}A newer version is available. Current version: $CURRENT_VERSION, Latest version: $LATEST_VERSION${NC}"
-
-    # Update the version in package.json
-    jq --arg LATEST_VERSION $LATEST_VERSION '.version = $LATEST_VERSION' $FILE_PATH > temp.json && mv temp.json $FILE_PATH
+    exit 0
 fi
 
+if semverGT "$CURRENT_VERSION" "$LATEST_VERSION"; then
+    echo -e "${RED}Your package version is higher than the latest published version. Current version: ${CURRENT_VERSION}, Latest version: ${LATEST_VERSION}${NC}"
+fi
+
+if semverLT "$CURRENT_VERSION" "$LATEST_VERSION"; then
+    echo -e "${YELLOW}A newer version is available. Current version: $CURRENT_VERSION, Latest version: $LATEST_VERSION${NC}"
+    # Update the version in package.json
+    jq --arg latest_version "$LATEST_VERSION" '.version = $latest_version' $FILE_PATH > temp.json && mv temp.json $FILE_PATH
+    exit 0
+fi
