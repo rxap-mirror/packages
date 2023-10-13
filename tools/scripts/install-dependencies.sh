f02830a18053659597e5a4ae21b62dfb74d3a045
Merzough Münker <mmuenker@digitaix.com>
2023-10-13 18:40:00 +0200

    ci: improve yarn install

--- tools/scripts/install-dependencies.sh
+++ tools/scripts/install-dependencies.sh
@@ -1,42 +1,26 @@
 #!/bin/sh
 
-# List of packages to be installed
-packages="git g++ make python3 curl"
-missing_packages=""
-
-# Iterate through the list of packages and check if they are installed
-for package in $packages; do
-  if ! apk info | grep -q "^$package$"; then
-    missing_packages="$missing_packages $package"
-  fi
-done
-
-# If there are any missing packages, install them
-if [ -n "$missing_packages" ]; then
-  echo "Installing missing packages: $missing_packages"
-  apk add --no-cache "$missing_packages"
-else
-  echo "All packages are already installed"
-fi
-
 corepack enable
 corepack prepare yarn@stable --activate
 
 case "$CI_COMMIT_REF_NAME" in
 renovate/*)
+  echo "RUN: yarn --refresh-lockfile"
   yarn --refresh-lockfile
   ;;
 *)
-  if [ -d node_modules ] && [ -d .yarn/cache ]; then
-    if [ "$CI_JOB_STAGE" = ".pre" ]; then
+  if [ "$CI_JOB_STAGE" = ".pre" ]; then
+    echo "RUN: yarn --immutable --check-cache"
+    yarn --immutable --check-cache
+  else
+    if [ -d node_modules ] && [ -d .yarn/cache ]; then
+      echo "RUN: yarn --immutable --immutable-cache"
       yarn --immutable --immutable-cache
     else
-      # without --immutable-cache, yarn will try to update the cache
+      echo "WARNING: No node_modules or .yarn/cache directory found."
+      echo "RUN: yarn"
       yarn --immutable
     fi
-  else
-    echo "WARNING: No node_modules or .yarn/cache directory found."
-    yarn
   fi
   ;;
 esac
