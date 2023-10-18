7ce0886e3ffadbb9506215866826763d21ac0519
Merzough Münker <mmuenker@digitaix.com>
2023-10-18 11:33:10 +0200

    fix: support modification of the config object

--- packages/nest/testing/src/lib/mock-config-service-factory.ts
+++ packages/nest/testing/src/lib/mock-config-service-factory.ts
@@ -1,11 +1,20 @@
-export function MockConfigServiceFactory(config: Record<string, unknown> = {}) {
+export interface MockConfigService {
+  get: <T = unknown>(key: string) => T;
+  getOrThrow: <T = unknown>(key: string) => T;
+  set: (key: string, value: any) => void;
+}
+
+export function MockConfigServiceFactory(config: Record<string, unknown> = {}): MockConfigService {
   return {
-    get: (key: string) => config[key],
-    getOrThrow: (key: string) => {
+    get: <T = unknown>(key: string): T => config[key] as T,
+    getOrThrow: <T = unknown>(key: string): T => {
       if (config[key] === undefined) {
         throw new Error(`Config key ${ key } is not defined`);
       }
-      return config[key];
+      return config[key] as T;
     },
+    set: (key: string, value: unknown) => {
+      config[key] = value;
+    }
   };
 }
