2ad13504bcaf0e021e36be5c73458e3a60aa8c48
Merzough Münker <mmuenker@digitaix.com>
2023-10-29 13:48:48 +0100

    feat: support viewChange in MethodDataSources

--- packages/angular/data-source/src/lib/method.data-source.ts
+++ packages/angular/data-source/src/lib/method.data-source.ts
@@ -6,8 +6,10 @@ import {
 import { Method } from '@rxap/pattern';
 import { ToggleSubject } from '@rxap/rxjs';
 import {
+  EMPTY,
   from,
   Observable,
+  OperatorFunction,
   ReplaySubject,
   Subject,
   TeardownLogic,
@@ -59,28 +61,41 @@ export class MethodDataSource<Data, Parameters = any>
     }
   }
 
-  protected override _connect(viewer: BaseDataSourceViewer & Parameters): [ Observable<Data>, TeardownLogic ] | Observable<Data> {
+  protected override _connect(viewer: (BaseDataSourceViewer & Parameters) | BaseDataSourceViewer<Parameters>): [ Observable<Data>, TeardownLogic ] | Observable<Data> {
     if (this.withoutParameters) {
       return super._connect(viewer);
     } else {
       return this._refresh.pipe(
         tap(() => this.hasError$.disable()),
         startWith(undefined),
-        tap(() => this.loading$.enable()),
-        switchMap(() => from(this.execute(viewer)).pipe(
-          tap({
-            next: () => {
-              this.loading$.disable();
-            },
-            error: (error: any) => {
-              this.loading$.disable();
-              this.hasError$.enable();
-              this.error$.next(error);
-            },
-          }),
-        )),
+        switchMap<void, Observable<Data>>(() => {
+          if (viewer.viewChange && viewer.viewChange !== EMPTY) {
+            return (
+              viewer.viewChange as Observable<Parameters>
+            ).pipe(
+              tap(() => this.loading$.enable()),
+              switchMap((parameters) => from(this.execute(parameters)).pipe(this.handelExecution())),
+            );
+          } else {
+            this.loading$.enable();
+            return from(this.execute(viewer as Parameters)).pipe(this.handelExecution());
+          }
+        }),
       );
     }
+  }
+
+  protected handelExecution(): OperatorFunction<Data, Data> {
+    return tap({
+      next: () => {
+        this.loading$.disable();
+      },
+      error: (error: any) => {
+        this.loading$.disable();
+        this.hasError$.enable();
+        this.error$.next(error);
+      },
+    });
   }
 
   protected async execute(parameters?: Parameters): Promise<Data> {
