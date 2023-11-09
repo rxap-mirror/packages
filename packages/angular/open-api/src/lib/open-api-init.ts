8fddd7ac97d6bb92aa7835a6933306d6de9eee24
Merzough Münker <mmuenker@digitaix.com>
2023-11-09 12:34:04 +0100

    fix: support custom origin

--- packages/angular/open-api/src/lib/open-api-init.ts
+++ packages/angular/open-api/src/lib/open-api-init.ts
@@ -1,10 +1,19 @@
 import { ConfigService } from '@rxap/config';
+import { JoinPath } from '@rxap/utilities';
 import { OpenApiConfigService } from './open-api-config.service';
 
 export interface OpenApiInitOptions {
+  /**
+   * true - load the open api config file from the default url
+   * default url: openapi.json
+   */
   load?: {
     openApiUrl?: string;
   } | boolean;
+  /**
+   * The location origin prefix for the open api base urls if the base url is not absolute. defaults to location.origin
+   */
+  origin?: string;
 }
 
 export function OpenApiInit(options: OpenApiInitOptions = {}) {
@@ -13,20 +22,23 @@ export function OpenApiInit(options: OpenApiInitOptions = {}) {
     {} as any,
     ConfigService.Config,
   );
+  options.origin ??= location.origin;
   if (api.baseUrl) {
     OpenApiConfigService.InsertServer({
-      url: api.baseUrl.match(/^https?:\/\//) ? api.baseUrl : location.origin + api.baseUrl,
+      url: api.baseUrl.match(/^https?:\/\//) ? api.baseUrl : JoinPath(options.origin, api.baseUrl),
     }, OpenApiConfigService.DefaultServerIndex);
   }
   for (const [ serverId, config ] of Object.entries(api)) {
     if (config && typeof config === 'object') {
       const baseUrl = config.baseUrl;
       if (baseUrl) {
-        const url = baseUrl.match(/^https?:\/\//) ? baseUrl : location.origin + baseUrl;
-        console.log(`Add server '${ serverId }' with url '${ url }'`);
-        OpenApiConfigService.InsertServer({
-          url,
-        }, OpenApiConfigService.DefaultServerIndex, serverId);
+        const url = baseUrl.match(/^https?:\/\//) ? baseUrl : JoinPath(options.origin, baseUrl);
+        console.debug(`Add server '${ serverId }' with url '${ url }'`);
+        OpenApiConfigService.InsertServer(
+          { url },
+          OpenApiConfigService.DefaultServerIndex,
+          serverId,
+        );
       }
     }
   }
