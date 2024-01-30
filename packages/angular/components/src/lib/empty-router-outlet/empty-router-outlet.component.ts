9912526f923b19bd3d7fcd8895b7ea5c4202a577
Merzough Münker <mmuenker@digitaix.com>
2024-01-30 13:52:57 +0100

    fix: ensure a unique component id is generated

--- packages/angular/components/src/lib/empty-router-outlet/empty-router-outlet.component.ts
+++ packages/angular/components/src/lib/empty-router-outlet/empty-router-outlet.component.ts
@@ -5,6 +5,8 @@ import {
 import { RouterOutlet } from '@angular/router';
 
 @Component({
+  // the selector is required to prevent a component ID collision with the component ɵEmptyOutletComponent from the @angular/router package
+  selector: 'rxap-empty-router-outlet',
   templateUrl: './empty-router-outlet.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
