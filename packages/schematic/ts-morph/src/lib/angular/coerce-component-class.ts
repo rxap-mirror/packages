51d41d7d6f509a05a831758f72e9434e3e5a5471
Merzough Münker <mmuenker@digitaix.com>
2024-02-09 13:04:19 +0100

    fix: use coerce component utility function

--- packages/schematic/ts-morph/src/lib/angular/coerce-component-class.ts
+++ packages/schematic/ts-morph/src/lib/angular/coerce-component-class.ts
@@ -1,20 +1,16 @@
-import { SchematicsException } from '@angular-devkit/schematics';
-import { classify } from '@rxap/schematics-utilities';
+import { CoerceComponent } from '@rxap/ts-morph';
 import {
   ArrayLiteralExpression,
   ClassDeclaration,
   ObjectLiteralExpression,
   Project,
   SourceFile,
-  Writers,
 } from 'ts-morph';
-import { CoerceClass } from '../coerce-class';
 import { CoerceSourceFile } from '../coerce-source-file';
 import {
   TsMorphAngularProjectTransformOptions,
   TsMorphAngularProjectTransformRule,
 } from '../ts-morph-transform';
-import { CoerceDecorator } from '../ts-morph/coerce-decorator';
 
 export interface CoerceComponentClassRuleOptions extends TsMorphAngularProjectTransformOptions {
   componentName: string;
@@ -24,7 +20,8 @@ export interface CoerceComponentClassRuleOptions extends TsMorphAngularProjectTr
     sourceFile: SourceFile,
     classDeclaration: ClassDeclaration,
     componentDecoratorObject: ObjectLiteralExpression,
-    providerArray: ArrayLiteralExpression,
+    providersArray: ArrayLiteralExpression,
+    importsArray: ArrayLiteralExpression,
   ) => void;
 }
 
@@ -38,30 +35,10 @@ export function CoerceComponentClassRule(options: Readonly<CoerceComponentClassR
   return TsMorphAngularProjectTransformRule(options, (project) => {
 
     const sourceFile = CoerceSourceFile(project, `/${ componentName }.component.ts`);
-    const classDeclaration = CoerceClass(
-      sourceFile,
-      classify(componentName) + 'Component', {
-        isExported: true,
-      },
-    );
-    const componentDecoratorDeclaration = CoerceDecorator(classDeclaration, 'Component', {
-      arguments: [
-        Writers.object({
-          selector: selector ?? componentName,
-          template: '',
-          styles: '[]',
-        }),
-      ],
-    });
-    const componentDecoratorObject = componentDecoratorDeclaration.getArguments()[0];
-    if (!componentDecoratorObject) {
-      throw new SchematicsException(`Could not find component decorator object for component '${ componentName }'`);
-    }
-    if (!(componentDecoratorObject instanceof ObjectLiteralExpression)) {
-      throw new SchematicsException(`Component decorator object for component '${ componentName }' is not an object literal expression`);
-    }
-    // const providerArrayDeclaration = CoercePropertyDeclaration(componentDecoratorObject, 'providers', {
-    throw new Error('Not implemented yet');
+
+    const { classDeclaration, componentDecoratorObject, providersArray, importsArray } = CoerceComponent(sourceFile, componentName, { selector, styleUrls: true, templateUrl: true });
+
+    tsMorphTransform?.(project, sourceFile, classDeclaration, componentDecoratorObject, providersArray, importsArray);
 
   });
 
