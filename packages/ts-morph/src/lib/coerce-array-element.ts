ee18576b1cfd2af4d67ae4b4022d794760115165
Merzough Münker <mmuenker@digitaix.com>
2024-02-20 12:41:40 +0100

    feat: add default find existing element function

--- packages/ts-morph/src/lib/coerce-array-element.ts
+++ packages/ts-morph/src/lib/coerce-array-element.ts
@@ -22,12 +22,24 @@ export function FindArrayElementByObjectProperty(property: string, value: string
   };
 }
 
+export function DefaultFindExistingElement(element: string) {
+  return (e: Expression) => e.getText().trim() === element;
+}
+
 export function CoerceArrayElement(
   array: ArrayLiteralExpression,
   element: string | WriterFunction,
-  findExisting: (e: Expression) => boolean,
+  findExisting?: (e: Expression) => boolean,
   insertAt?: (array: ArrayLiteralExpression) => number,
 ): Expression {
+
+  if (!findExisting) {
+    if (typeof element === 'string') {
+      findExisting = DefaultFindExistingElement(element);
+    } else {
+      throw new Error('The findExisting function is required or the element must be a string!');
+    }
+  }
 
   for (const e of array.getElements()) {
     if (findExisting(e)) {
