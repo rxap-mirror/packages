47fda87cb418a30e40060788b0b880603f530138
Merzough Münker <mmuenker@digitaix.com>
2024-02-28 14:14:40 +0100

    feat: support unshift instead of push

--- packages/utilities/src/lib/array/coerce-array-items.ts
+++ packages/utilities/src/lib/array/coerce-array-items.ts
@@ -1,7 +1,11 @@
-export function CoerceArrayItems<T = any>(array: T[], items: T[], compareTo = ((a: T, b: T) => a === b)) {
+export function CoerceArrayItems<T = any>(array: T[], items: T[], compareTo = ((a: T, b: T) => a === b), unshift = false) {
   for (const item of items) {
     if (!array.find((a) => compareTo(a, item))) {
-      array.push(item);
+      if (unshift) {
+        array.unshift(item);
+      } else {
+        array.push(item);
+      }
     }
   }
 }
