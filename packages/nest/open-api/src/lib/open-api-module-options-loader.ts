57dfadd106d036f6727be9bd92debbb5a144fde7
Merzough Münker <mmuenker@digitaix.com>
2024-03-04 13:36:15 +0100

    fix: log composed file path with scope

--- packages/nest/open-api/src/lib/open-api-module-options-loader.ts
+++ packages/nest/open-api/src/lib/open-api-module-options-loader.ts
@@ -8,6 +8,7 @@ import { ConfigService } from '@nestjs/config';
 import { RXAP_GLOBAL_STATE } from '@rxap/nest-utilities';
 import {
   ExistsFileWithScope,
+  InjectScopeInFilePath,
   ReadFileWithScope,
 } from '@rxap/node-utilities';
 import { OpenApiModuleOptions } from './open-api.module';
@@ -34,12 +35,12 @@ export class OpenApiModuleOptionsLoader implements ConfigurableModuleOptionsFact
       try {
         content = this.interpolateEnvVariables(content);
       } catch (e: any) {
-        throw new Error(`Could not interpolate env variables in open api server config file "${ openApiServerConfigFilePath }": ${ e.message }`);
+        throw new Error(`Could not interpolate env variables in open api server config file "${ this.fileNameWithScope(openApiServerConfigFilePath) }": ${ e.message }`);
       }
       try {
         config.serverConfig = JSON.parse(content);
       } catch (e: any) {
-        throw new Error(`Could not parse open api server config file "${ openApiServerConfigFilePath }": ${ e.message }`);
+        throw new Error(`Could not parse open api server config file "${ this.fileNameWithScope(openApiServerConfigFilePath) }": ${ e.message }`);
       }
     }
     return config;
@@ -51,6 +52,10 @@ export class OpenApiModuleOptionsLoader implements ConfigurableModuleOptionsFact
 
   private readFileWithScope(filePath: string): string {
     return ReadFileWithScope(filePath, RXAP_GLOBAL_STATE.environment?.name);
+  }
+
+  private fileNameWithScope(filePath: string): string {
+    return InjectScopeInFilePath(filePath, RXAP_GLOBAL_STATE.environment?.name);
   }
 
   private interpolateEnvVariables(value: string): string {
