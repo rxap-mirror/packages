9635eba77aeeb74d61a5ef9ea2fc525722dbbcb7
Merzough Münker <mmuenker@digitaix.com>
2024-03-07 16:20:25 +0100

    fix: ensure the cell component are valid

--- packages/schematic/angular/src/lib/minimum-table-component-options.ts
+++ packages/schematic/angular/src/lib/minimum-table-component-options.ts
@@ -581,7 +581,7 @@ export function cellComponentRule(normalizedOptions: NormalizedMinimumTableCompo
             shared,
             name: CoerceSuffix(dasherize(column.name), '-cell'),
             componentOptions: {
-              selector: `mat-cell[{{prefix}}-${ dasherize(column.name) }]`,
+              selector: `td[mat-cell][{{prefix}}-${ dasherize(column.name) }]`,
             },
             directory,
             overwrite: overwrite || column.modifiers.includes(TableColumnModifier.OVERWRITE),
