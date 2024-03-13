595572b45a9805ab2b99acc1198c35eb2f541164
Merzough Münker <mmuenker@digitaix.com>
2024-03-13 15:02:23 +0100

    refactor: extract method

--- packages/plugin/localazy/src/generators/gitlab-ci/generator.ts
+++ packages/plugin/localazy/src/generators/gitlab-ci/generator.ts
@@ -1,36 +1,6 @@
-import {
-  getProjects,
-  ProjectConfiguration,
-  Tree,
-} from '@nx/devkit';
-import { IsApplicationProject } from '@rxap/workspace-utilities';
-import { stringify } from 'yaml';
+import { Tree } from '@nx/devkit';
+import { generateGitlabCi } from './generate-gitlab-ci';
 import { GitlabCiGeneratorSchema } from './schema';
-
-const dotLocalazy: any = {
-  extends: '.run',
-  stage: 'localazy',
-  environment: {
-    action: 'prepare',
-  },
-  variables: {
-    TARGET: '${PROJECT_NAME}:localazy-upload'
-  }
-};
-
-function skipProject(tree: Tree, options: GitlabCiGeneratorSchema, project: ProjectConfiguration, projectName: string) {
-
-  if (!IsApplicationProject(project)) {
-    return true;
-  }
-
-  if (!project.targets?.['localazy-upload']) {
-    return true;
-  }
-
-  return false;
-
-}
 
 export async function gitlabCiGenerator(
   tree: Tree,
@@ -38,40 +8,7 @@ export async function gitlabCiGenerator(
 ) {
   console.log('localazy gitlab-ci generator:', options);
 
-  if (options.tags) {
-    dotLocalazy.tags = options.tags;
-  }
-
-  const dockerYaml = {
-    '.localazy': dotLocalazy,
-    'localazy-upload': {
-      extends: '.localazy',
-      environment: {
-        name: '$ENVIRONMENT_NAME/$PROJECT_NAME',
-      },
-      parallel: {
-        matrix: [] as Record<string, any>,
-      }
-    }
-  };
-
-  for (const [ projectName, project ] of getProjects(tree).entries()) {
-
-    if (skipProject(tree, options, project, projectName)) {
-      continue;
-    }
-
-    console.log(`add project: ${ projectName } to localazy gitlab-ci configuration`);
-
-    dockerYaml['localazy-upload'].parallel.matrix.push({
-      PROJECT_NAME: projectName,
-    });
-
-  }
-
-  const gitlabCiYaml = stringify(dockerYaml);
-
-  tree.write('.gitlab/ci/jobs/localazy.yaml', gitlabCiYaml);
+  generateGitlabCi(tree, options);
 
 }
 
