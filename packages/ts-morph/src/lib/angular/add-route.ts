948d8127d132622f442d69a593ef995b16febe40
Merzough Münker <mmuenker@digitaix.com>
2024-03-15 13:31:07 +0100

    feat: auto detect the inset at index

--- packages/ts-morph/src/lib/angular/add-route.ts
+++ packages/ts-morph/src/lib/angular/add-route.ts
@@ -13,7 +13,37 @@ import {
 } from './build-route-object';
 import { FindParentRoute } from './find-parent-route';
 
-export function AddRoute(sourceFile: SourceFile, route: AngularRoute, path?: string[], name = 'ROUTES', insertAt = 0) {
+export function DefaultInsertAtFactory(route: AngularRoute): (array: ArrayLiteralExpression) => number {
+  return (array: ArrayLiteralExpression) => {
+    const elementCount = array.getElements().length;
+    if (route.path === '') {
+      if (elementCount > 0) {
+        const lastElement = array.getElements()[elementCount - 1];
+        if (lastElement.isKind(SyntaxKind.ObjectLiteralExpression)) {
+          if (lastElement.asKindOrThrow(SyntaxKind.ObjectLiteralExpression).getProperty('path')?.getText().match(/\*\*/)) {
+            return elementCount - 2;
+          }
+        }
+      }
+      return array.getElements().length - 1;
+    }
+    let insertAt = 0;
+    if (elementCount > 0) {
+      // find the first object literal
+      // this ensures that use of variables for route imports like STATUS_CHECK_ROUTE is always the first element
+      for (const element of array.getElements()) {
+        if (!element.isKind(SyntaxKind.ObjectLiteralExpression)) {
+          insertAt++;
+        } else {
+          break;
+        }
+      }
+    }
+    return insertAt;
+  };
+}
+
+export function AddRoute(sourceFile: SourceFile, route: AngularRoute, path?: string[], name = 'ROUTES', insertAt: (array: ArrayLiteralExpression) => number = DefaultInsertAtFactory(route)) {
   const routes = sourceFile.getVariableDeclaration(name);
   if (routes) {
     let initializer: ArrayLiteralExpression | null = routes.getInitializerIfKindOrThrow(SyntaxKind.ArrayLiteralExpression);
@@ -22,7 +52,7 @@ export function AddRoute(sourceFile: SourceFile, route: AngularRoute, path?: str
     }
     if (initializer) {
       CoerceArrayElement(
-        initializer, BuildRouteObject(route), FindArrayElementByObjectProperty('path', route.path), () => insertAt);
+        initializer, BuildRouteObject(route), FindArrayElementByObjectProperty('path', route.path), insertAt);
     } else {
       console.warn('Initializer not found');
     }
