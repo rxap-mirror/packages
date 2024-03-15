4c10be5ce97e57c7f554870eb4f93a2555f621f3
Merzough Münker <mmuenker@digitaix.com>
2024-03-15 13:00:25 +0100

    feat: support complex load children and component

--- packages/ts-morph/src/lib/angular/build-route-object.ts
+++ packages/ts-morph/src/lib/angular/build-route-object.ts
@@ -5,8 +5,8 @@ import {
 
 export interface AngularRoute {
   path: string;
-  loadChildren?: string;
-  loadComponent?: string;
+  loadChildren?: string | { import: string, then: string };
+  loadComponent?: string | { import: string, then: string };
 }
 
 export function BuildRouteObject(route: AngularRoute) {
@@ -14,10 +14,18 @@ export function BuildRouteObject(route: AngularRoute) {
     path: w => w.quote(route.path),
   };
   if (route.loadChildren) {
-    obj['loadChildren'] = `() => import('${ route.loadChildren }')`;
+    if (typeof route.loadChildren === 'string') {
+      obj['loadChildren'] = `() => import('${ route.loadChildren }')`;
+    } else {
+      obj['loadChildren'] = `() => import('${ route.loadChildren.import }').then((m) => m.${ route.loadChildren.then })`;
+    }
   }
   if (route.loadComponent) {
-    obj['loadComponent'] = `() => import('${ route.loadComponent }')`;
+    if (typeof route.loadComponent === 'string') {
+      obj['loadComponent'] = `() => import('${ route.loadComponent }')`;
+    } else {
+      obj['loadComponent'] = `() => import('${ route.loadComponent.import }').then((m) => m.${ route.loadComponent.then })`;
+    }
   }
   return Writers.object(obj);
 }
