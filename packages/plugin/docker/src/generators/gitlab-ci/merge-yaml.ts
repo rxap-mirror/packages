a3ea7093d7368444ce31dde993b574d7d4c85153
Merzough Münker <mmuenker@digitaix.com>
2024-03-16 15:42:31 +0100

    fix: check if current file exists

--- packages/plugin/docker/src/generators/gitlab-ci/merge-yaml.ts
+++ packages/plugin/docker/src/generators/gitlab-ci/merge-yaml.ts
@@ -4,7 +4,7 @@ import { basename } from 'path';
 
 // TODO : move to utility package
 export function mergeYaml(tree: Tree, filePath: string, newContent: string) {
-  const currentContent = tree.read(filePath, 'utf-8') ?? '';
+  const currentContent = tree.exists(filePath) ? tree.read(filePath, 'utf-8') : undefined;
   const mergedContent = YamlMergeFunction(currentContent, newContent, basename(filePath), filePath);
   tree.write(filePath, mergedContent);
 }
