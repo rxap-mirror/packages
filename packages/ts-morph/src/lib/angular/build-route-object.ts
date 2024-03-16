ce4dec6008f77a8482a50d53c6b450522d64451c
Merzough Münker <mmuenker@digitaix.com>
2024-03-16 13:31:47 +0100

    feat: support route outlet and data

--- packages/ts-morph/src/lib/angular/build-route-object.ts
+++ packages/ts-morph/src/lib/angular/build-route-object.ts
@@ -8,6 +8,8 @@ export interface AngularRoute {
   component?: string;
   loadChildren?: string | { import: string, then: string };
   loadComponent?: string | { import: string, then: string };
+  outlet?: string | null;
+  data?: Record<string, any> | null;
 }
 
 export function BuildRouteObject(route: AngularRoute) {
@@ -29,6 +31,29 @@ export function BuildRouteObject(route: AngularRoute) {
     }
   } else if (route.component) {
     obj['component'] = route.component;
+  }
+  if (route.outlet) {
+    obj['outlet'] = w => w.quote(route.outlet!);
+  }
+  if (route.data) {
+    obj['data'] = Writers.object(Object.entries(route.data)
+      .reduce((acc, [ key, value ]) => {
+        if (['true', 'false'].includes(value) || typeof value === 'boolean') {
+          if (typeof value === 'string') {
+            acc[key] = w => w.quote(value);
+          } else {
+            acc[key] = value ? 'true' : 'false';
+          }
+        }
+        if (typeof value === 'number') {
+          acc[key] = value.toString();
+        }
+        if (!isNaN(Number(value))) {
+          acc[key] = value;
+        }
+        acc[key] = w => w.quote(value);
+        return acc;
+      }, {} as Record<string, string | WriterFunction>));
   }
   return Writers.object(obj);
 }
