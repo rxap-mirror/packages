d8477c4709c32222fe3807137346abff9e506d77
Merzough Münker <mmuenker@digitaix.com>
2024-03-27 12:16:51 +0100

    feat: support custom jwt header name

--- packages/nest/open-api/src/lib/default.upstream-interceptor.ts
+++ packages/nest/open-api/src/lib/default.upstream-interceptor.ts
@@ -4,15 +4,27 @@ import {
   Logger,
   Scope,
 } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { REQUEST } from '@nestjs/core';
 import { AxiosRequestConfig } from 'axios';
 import { Request } from 'express';
-import { OpenApiUpstreamInterceptor } from './open-api-operation/types';
 import { OpenApiServerId } from './open-api-operation/open-api-server-id';
+import { OpenApiUpstreamInterceptor } from './open-api-operation/types';
 
 @OpenApiServerId('__default__')
 @Injectable({ scope: Scope.REQUEST })
 export class DefaultUpstreamInterceptor implements OpenApiUpstreamInterceptor {
+
+  @Inject(ConfigService)
+  private readonly config!: ConfigService;
+
+  private get authHeaderName(): string {
+    return this.config.get<string>('JWT_AUTH_HEADER', 'Authorization');
+  }
+
+  private get upstreamHeaderName(): string {
+    return this.config.get<string>('UPSTREAM_JWT_AUTH_HEADER', this.authHeaderName);
+  }
 
   constructor(
     @Inject(REQUEST) private readonly request: Request,
@@ -23,13 +35,13 @@ export class DefaultUpstreamInterceptor implements OpenApiUpstreamInterceptor {
   intercept(config: AxiosRequestConfig): AxiosRequestConfig {
 
     config.headers ??= {};
-    const token = this.request.header('Authorization');
-    this.logger.debug('Inject the Authorization header', 'DefaultUpstreamInterceptor');
+    const token = this.request.header(this.authHeaderName);
+    this.logger.debug(`Inject the ${this.upstreamHeaderName} header`, 'DefaultUpstreamInterceptor');
     if (token) {
-      config.headers['Authorization'] = this.request.header('Authorization');
+      config.headers[this.upstreamHeaderName] = this.request.header(this.authHeaderName);
     } else {
       this.logger.warn(
-        `No Authorization header found in the request - [${ config.method?.toUpperCase() }] ${ config.url }`,
+        `No ${this.authHeaderName} header found in the request - [${ config.method?.toUpperCase() }] ${ config.url }`,
         'DefaultUpstreamInterceptor',
       );
     }
