4f0c1368e9ad866a540c617609badc67991602c9
Merzough Münker <mmuenker@digitaix.com>
2024-03-31 17:46:07 +0200

    refactor: move app service implementation into separate repository

--- applications/service/changelog/src/environments/environment.swagger.ts
+++
@@ -1,8 +0,0 @@
-import { Environment } from '@rxap/nest-utilities';
-
-export const environment: Environment = {
-  name: 'swagger',
-  app: 'service-changelog',
-  production: true,
-  swagger: true,
-};
--- applications/service/status/src/environments/environment.swagger.ts
+++
@@ -1,8 +0,0 @@
-import { Environment } from '@rxap/nest-utilities';
-
-export const environment: Environment = {
-  name: 'swagger',
-  app: 'service-status',
-  production: true,
-  swagger: true,
-};
