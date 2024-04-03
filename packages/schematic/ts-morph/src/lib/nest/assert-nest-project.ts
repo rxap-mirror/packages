146e987f550145373f340516947e316d4ae31818
Merzough Münker <mmuenker@digitaix.com>
2024-04-03 17:19:48 +0200

    fix: print the required commands to setup projects

--- packages/schematic/ts-morph/src/lib/nest/assert-nest-project.ts
+++ packages/schematic/ts-morph/src/lib/nest/assert-nest-project.ts
@@ -2,10 +2,12 @@ import {
   Rule,
   SchematicsException,
 } from '@angular-devkit/schematics';
+import 'colors';
 import {
+  buildNestProjectDirectoryPath,
   buildNestProjectName,
   HasNestServiceProject,
-} from './project-utilities';
+} from '@rxap/workspace-utilities';
 
 export interface AssertNestProjectOptions {
   project: string;
@@ -17,6 +19,8 @@ export function AssertNestProject(options: AssertNestProjectOptions): Rule {
   const { project, feature, shared } = options;
   return tree => {
     if (!HasNestServiceProject(tree, { project, feature, shared })) {
+      console.log('Use the command: ' + `nx g @nx/nest:application --projectNameAndRootFormat as-provided --name ${ buildNestProjectName({ project, feature, shared }) } --directory ${ buildNestProjectDirectoryPath({ project, feature, shared }) }`.blue + ' to create the required nest project');
+      console.log('Use the command: ' + `nx g @rxap/plugin-nestjs:init-application --project ${ buildNestProjectName({ project, feature, shared }) } --generateMain --overwrite`.blue + ' to initialize the nest project');
       throw new SchematicsException(`The project ${ buildNestProjectName({ project, feature, shared }) } does not exists!`);
     }
   };
