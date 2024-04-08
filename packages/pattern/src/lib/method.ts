0440ae3bf9942a65f9b644f0f5db478d85f86cc6
Merzough Münker <mmuenker@digitaix.com>
2024-04-08 10:23:17 +0200

    fix: add utility interfaces

--- packages/pattern/src/lib/method.ts
+++ packages/pattern/src/lib/method.ts
@@ -4,16 +4,32 @@ export interface Method<ReturnType = any, Parameter = any> extends MetaData {
   call(parameters?: Parameter, ...args: any[]): Promise<ReturnType> | ReturnType;
 }
 
+export interface AsyncMethod<ReturnType = any, Parameter = any> extends MetaData {
+  call(parameters?: Parameter, ...args: any[]): Promise<ReturnType>;
+}
+
+export interface SyncMethod<ReturnType = any, Parameter = any> extends MetaData {
+  call(parameters?: Parameter, ...args: any[]): ReturnType;
+}
+
 export interface MethodWithParameters<ReturnType = any, Parameter = any> extends Method<ReturnType, Parameter> {
   call(parameters: Parameter, ...args: any[]): Promise<ReturnType> | ReturnType;
 }
 
-export function ToMethod<ReturnType = any, Parameter = any>(call: ((
+export interface AsyncMethodWithParameters<ReturnType = any, Parameter = any> extends Method<ReturnType, Parameter> {
+  call(parameters: Parameter, ...args: any[]): Promise<ReturnType>;
+}
+
+export interface SyncMethodWithParameters<ReturnType = any, Parameter = any> extends Method<ReturnType, Parameter> {
+  call(parameters: Parameter, ...args: any[]): ReturnType;
+}
+
+export function ToMethod<ReturnType = any, Parameter = any, MethodType extends Method<ReturnType, Parameter> = Method<ReturnType, Parameter>>(call: ((
   parameters?: Parameter,
   ...args: any[]
-) => Promise<ReturnType> | ReturnType), metadata: any = { id: 'to-method' }): Method<ReturnType, Parameter> {
+) => Promise<ReturnType> | ReturnType), metadata: any = { id: 'to-method' }): MethodType {
   return {
     call,
     metadata,
-  };
+  } as MethodType;
 }
