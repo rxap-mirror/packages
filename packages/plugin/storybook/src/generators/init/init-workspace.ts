1e5ee101b592b706c1d0f104f33125ddf4339cdf
Merzough Münker <mmuenker@digitaix.com>
2024-04-08 16:38:11 +0200

    fix: add required compodoc package

--- packages/plugin/storybook/src/generators/init/init-workspace.ts
+++ packages/plugin/storybook/src/generators/init/init-workspace.ts
@@ -1,7 +1,10 @@
 import { Tree } from '@nx/devkit';
+import { AddPackageJsonDevDependency } from '@rxap/workspace-utilities';
 import { InitGeneratorSchema } from './schema';
 
 export async function initWorkspace(tree: Tree, options: InitGeneratorSchema) {
   console.log('init storybook workspace');
+
+  await AddPackageJsonDevDependency(tree, '@compodoc/compodoc', 'latest', { soft: true });
 
 }
