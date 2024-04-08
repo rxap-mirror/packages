f1247525b5f593be33fed1cb52295e80cdbb7c25
Merzough Münker <mmuenker@digitaix.com>
2024-04-08 17:24:06 +0200

    feat: init the shared library projects

--- packages/plugin/angular/src/generators/init/generator.ts
+++ packages/plugin/angular/src/generators/init/generator.ts
@@ -1,10 +1,38 @@
 import { Tree } from '@nx/devkit';
+import initLibraryGenerator from '../init-library/generator';
 import { InitGeneratorSchema } from './schema';
 
 export async function initGenerator(tree: Tree, options: InitGeneratorSchema) {
   console.log('angular init generator:', options);
 
+  await initLibraryGenerator(tree, {
+    projects: [
+      'components',
+      'forms',
+      'controls',
+      'tables'
+    ],
+    coerce: {
+      directory: 'angular',
+      addTailwind: true
+    },
+    indexExport: false,
+  });
 
+  await initLibraryGenerator(tree, {
+    projects: [
+      'methods',
+      'data-sources',
+      'pipes',
+      'directives',
+      'guards',
+      'services'
+    ],
+    coerce: {
+      directory: 'angular'
+    },
+    indexExport: false,
+  });
 
 }
 
