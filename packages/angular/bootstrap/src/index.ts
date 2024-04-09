e3ee3bcd371df56a41177e605e33953835d6b619
Merzough Münker <mmuenker@digitaix.com>
2024-04-09 21:01:08 +0200

    chore: expose all functions

--- packages/angular/bootstrap/src/index.ts
+++ packages/angular/bootstrap/src/index.ts
@@ -1,3 +1,6 @@
+// region 
 export * from './lib/application';
 export * from './lib/module-application';
+export * from './lib/setup-dynamic-mfe';
 export * from './lib/standalone-application';
+// endregion
