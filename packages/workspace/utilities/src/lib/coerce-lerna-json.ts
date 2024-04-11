252d59fc94f886a52559560943cab224ebd2365c
mmuenker <mmuenker@digitaix.com>
2024-04-11 10:48:39 +0000

    fix: use json update function

--- packages/workspace/utilities/src/lib/coerce-lerna-json.ts
+++ packages/workspace/utilities/src/lib/coerce-lerna-json.ts
@@ -1,31 +1,30 @@
 import { CoerceArrayItems } from '@rxap/utilities';
 import { join } from 'path';
-import { CoerceFile } from './coerce-file';
+import { UpdateJsonFile } from './json-file';
 import { TreeLike } from './tree';
 
 export function CoerceLernaJson(tree: TreeLike, baseDir = '') {
-  const lernaJson = JSON.parse(CoerceFile(tree, join(baseDir, 'lerna.json'), '{}', true));
-  lernaJson.$schema ??= 'https://json.schemastore.org/lerna';
-  lernaJson.useWorkspaces ??= true;
-  lernaJson.version ??= 'independent';
-  lernaJson.npmClient ??= 'yarn';
-  lernaJson.command ??= {};
-  lernaJson.command.version ??= {};
-  lernaJson.command.version.message ??= 'chore(release): version';
-  lernaJson.command.version.conventionalCommits ??= true;
-  lernaJson.command.version.allowBranch ??= [];
-  CoerceArrayItems(lernaJson.command.version.allowBranch, [
-    'development',
-    'latest',
-    'next',
-    '+(1|2|3|4|5|6|7|8|9|0).x.x',
-    '+(1|2|3|4|5|6|7|8|9|0).+(1|2|3|4|5|6|7|8|9|0).x',
-    '+(1|2|3|4|5|6|7|8|9|0).x.x-dev',
-    '+(1|2|3|4|5|6|7|8|9|0).+(1|2|3|4|5|6|7|8|9|0).x-dev',
-  ]);
-  lernaJson.command.version.ignoreChanges ??= [];
-  CoerceArrayItems(lernaJson.command.version.ignoreChanges, [
-    [
+  UpdateJsonFile(tree, (lerna: any) => {
+    lerna.$schema ??= 'https://json.schemastore.org/lerna';
+    lerna.useWorkspaces ??= true;
+    lerna.version ??= 'independent';
+    lerna.npmClient ??= 'yarn';
+    lerna.command ??= {};
+    lerna.command.version ??= {};
+    lerna.command.version.message ??= 'chore(release): version';
+    lerna.command.version.conventionalCommits ??= true;
+    lerna.command.version.allowBranch ??= [];
+    CoerceArrayItems(lerna.command.version.allowBranch, [
+      'development',
+      'latest',
+      'next',
+      '+(1|2|3|4|5|6|7|8|9|0).x.x',
+      '+(1|2|3|4|5|6|7|8|9|0).+(1|2|3|4|5|6|7|8|9|0).x',
+      '+(1|2|3|4|5|6|7|8|9|0).x.x-dev',
+      '+(1|2|3|4|5|6|7|8|9|0).+(1|2|3|4|5|6|7|8|9|0).x-dev',
+    ]);
+    lerna.command.version.ignoreChanges ??= [];
+    CoerceArrayItems(lerna.command.version.ignoreChanges, [
       '**/*.md',
       '**/*.spec.ts',
       '**/*.stories.ts',
@@ -35,6 +34,7 @@ export function CoerceLernaJson(tree: TreeLike, baseDir = '') {
       '**/tsconfig.json',
       '**/tsconfig.*.json',
       '**/*.yaml',
-    ],
-  ]);
+    ]);
+  }, join(baseDir, 'lerna.json'), { create: true });
+
 }
