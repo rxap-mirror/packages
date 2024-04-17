b2aa2bf4cabbaf021f5dcd6653e154606ae5178a
Merzough Münker <mmuenker@digitaix.com>
2024-04-17 14:31:40 +0200

    fix: include all projects with a docker target

--- packages/plugin/gitlab-ci/src/generators/docker/skip-project.ts
+++ packages/plugin/gitlab-ci/src/generators/docker/skip-project.ts
@@ -2,15 +2,10 @@ import {
   ProjectConfiguration,
   Tree,
 } from '@nx/devkit';
-import { IsApplicationProject } from '@rxap/workspace-utilities';
 import { DockerGeneratorSchema } from './schema';
 
 export function skipProject(
   tree: Tree, options: DockerGeneratorSchema, project: ProjectConfiguration, projectName: string) {
-
-  if (!IsApplicationProject(project)) {
-    return true;
-  }
 
   if (!project.targets?.['docker']) {
     console.warn(`The project '${ projectName }' has no docker target`);
