0e6e4cb4c39f8643fb3c76e39182c8285264af1e
Merzough Münker <mmuenker@digitaix.com>
2024-04-28 15:47:14 +0200

    feat: add AutoFormFieldHeightBottomContainerDirective

--- packages/angular/material/directives/form-field/src/index.ts
+++ packages/angular/material/directives/form-field/src/index.ts
@@ -1,3 +1,4 @@
-// region 
+// region
+export * from './lib/auto-form-field-height-bottom-container.directive';
 export * from './lib/form-field-no-padding.directive';
 // endregion
---
+++ packages/angular/material/directives/form-field/src/lib/auto-form-field-height-bottom-container.directive.ts
@@ -0,0 +1,36 @@
+import {
+  AfterViewInit,
+  Directive,
+  ElementRef,
+  inject,
+} from '@angular/core';
+import { ObserveElementHeight } from '@rxap/browser-utilities';
+import { log } from '@rxap/rxjs';
+import {
+  distinctUntilChanged,
+  tap,
+} from 'rxjs';
+
+@Directive({
+  selector: 'mat-form-field[rxapAutoHeightBottomContainer]',
+  standalone: true,
+})
+export class AutoFormFieldHeightBottomContainerDirective implements AfterViewInit {
+
+  public element = inject(ElementRef);
+
+  ngAfterViewInit() {
+    ObserveElementHeight(this.element.nativeElement, '.mat-mdc-form-field-error-wrapper').pipe(
+      distinctUntilChanged(),
+      log('height change'),
+      tap(height => {
+        if (height) {
+          this.element.nativeElement.querySelector('.mat-mdc-form-field-bottom-align').style.height = `${ height }px`;
+        } else {
+          this.element.nativeElement.querySelector('.mat-mdc-form-field-bottom-align').style.height = 'auto';
+        }
+      }),
+    ).subscribe();
+  }
+
+}
