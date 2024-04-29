677667112864e717c539f7736718b2f50d9e9b8c
Merzough Münker <mmuenker@digitaix.com>
2024-04-29 11:40:14 +0200

    fix: export all files

--- packages/angular/tree/src/index.ts
+++ packages/angular/tree/src/index.ts
@@ -1,5 +1,7 @@
-export * from './lib/tree.data-source';
-export * from './lib/tree-content.directive';
-
-export * from './lib/tree.component';
+// region 
+export * from './lib/search.form';
 export * from './lib/tokens';
+export * from './lib/tree-content.directive';
+export * from './lib/tree.component';
+export * from './lib/tree.data-source';
+// endregion
