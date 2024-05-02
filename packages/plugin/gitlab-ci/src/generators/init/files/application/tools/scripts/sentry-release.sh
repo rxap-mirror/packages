c18caa6384c50012709512d5a592e45d7a56338b
Merzough Münker <mmuenker@digitaix.com>
2024-05-02 10:06:30 +0200

    fix: improve script logging

--- packages/plugin/gitlab-ci/src/generators/init/files/application/tools/scripts/sentry-release.sh
+++ packages/plugin/gitlab-ci/src/generators/init/files/application/tools/scripts/sentry-release.sh
@@ -22,16 +22,33 @@ fi
 
 echo "ENVIRONMENT=$ENVIRONMENT"
 
+BLACK='\033[0;30m'
 RED='\033[0;31m'
 GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
 BLUE='\033[0;34m'
-NC='\033[0m' # No Color
+MAGENTA='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT_GRAY='\033[0;37m'
+GRAY='\033[0;90m'
+LIGHT_RED='\033[0;91m'
+LIGHT_GREEN='\033[0;92m'
+LIGHT_YELLOW='\033[0;93m'
+LIGHT_BLUE='\033[0;94m'
+LIGHT_MAGENTA='\033[0;95m'
+LIGHT_CYAN='\033[0;96m'
+WHITE='\033[0;97m'
+NC='\033[0m'  # No Color
 
 projects=($(jq -r '.projects | .[]' "${GIT_ROOT}/workspace.json"))
 
+echo -e "${CYAN}Projects: ${projects[@]}${NC}"
+
 for projectBasePath in "${projects[@]}"; do
+  echo -e "${BLUE}Project: ${projectBasePath}${NC}"
   if [[ "$(jq -r '.projectType' "${GIT_ROOT}/${projectBasePath}/project.json")" == "application" ]]; then
     tags=($(jq -r '.tags | .[]' "${GIT_ROOT}/${projectBasePath}/project.json"))
+    echo -e "${GRAY}Tags: ${tags[@]}${NC}"
     if [[ "${tags[@]}" =~ "sentry" ]]; then
       outputPath=$(jq -r '.targets.build.options.outputPath' "${GIT_ROOT}/${projectBasePath}/project.json")
       SENTRY_PROJECT=$(jq -r '.sentry.projectId' "${GIT_ROOT}/${projectBasePath}/project.json")
@@ -40,16 +57,25 @@ for projectBasePath in "${projects[@]}"; do
       fi
       echo -e "${BLUE}Create sentry release '${VERSION}' with project id '${SENTRY_PROJECT}' for project '$(jq -r '.name' "${GIT_ROOT}/${projectBasePath}/project.json")' with dist '${outputPath}'${NC}"
       if [[ -z "$CI" ]]; then
-        echo -e "${RED}Skip sentry cli command - not in a CI environment${NC}"
+        echo -e "${YELLOW}Skip sentry cli command - not in a CI environment${NC}"
       else
         export SENTRY_PROJECT
+        echo -e "${GRAY}SENTRY_PROJECT=${SENTRY_PROJECT}${NC}"
+        echo -e "${GRAY}sentry-cli releases new --finalize ${VERSION}${NC}"
         sentry-cli releases new --finalize "$VERSION"
+        echo -e "${GRAY}sentry-cli releases set-commits --auto ${VERSION}${NC}"
         sentry-cli releases set-commits --auto "$VERSION"
+        echo -e "${GRAY}sentry-cli releases files ${VERSION} upload-sourcemaps ${outputPath}${NC}"
         sentry-cli releases files "$VERSION" upload-sourcemaps "$outputPath"
+        echo -e "${GRAY}sentry-cli releases deploys ${VERSION} new -e ${ENVIRONMENT}${NC}"
         sentry-cli releases deploys "$VERSION" new -e "$ENVIRONMENT"
       fi
       unset SENTRY_PROJECT
+    else
+      echo -e "${YELLOW}Skip project - no sentry tag${NC}"
     fi
+  else
+    echo -e "${YELLOW}Skip project - not an application${NC}"
   fi
 done
 
