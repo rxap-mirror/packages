422e7646276b65b2dd70e2b1fd444ab8d743c072
Merzough Münker <mmuenker@digitaix.com>
2024-05-07 17:35:26 +0200

    feat: support service e2e targets

--- packages/plugin/gitlab-ci/src/generators/docker/generate-gitlab-ci.ts
+++ packages/plugin/gitlab-ci/src/generators/docker/generate-gitlab-ci.ts
@@ -1,6 +1,7 @@
 import { Tree } from '@nx/devkit';
 import { GetRootDockerOptions } from '@rxap/workspace-utilities';
 import { generateDockerGitlabCiFileContent } from './generate-docker-gitlab-ci-file-content';
+import { generateServiceE2eGitlabCiFileContent } from './generate-service-e2e-gitlab-ci-file-content';
 import { generateStartupGitlabCiFileContent } from './generate-startup-gitlab-ci-file-content';
 import { mergeYaml } from './merge-yaml';
 import { DockerGeneratorSchema } from './schema';
@@ -16,5 +17,9 @@ export function GenerateGitlabCi(tree: Tree, options: DockerGeneratorSchema) {
   const startupGitlabCiYaml = generateStartupGitlabCiFileContent(tree, options, rootDocker);
 
   mergeYaml(tree, '.gitlab/ci/jobs/startup.yaml', startupGitlabCiYaml);
+
+  const serviceE2eGitlabCiYaml = generateServiceE2eGitlabCiFileContent(tree, options, rootDocker);
+
+  mergeYaml(tree, '.gitlab/ci/jobs/service-e2e.yaml', serviceE2eGitlabCiYaml);
 
 }
---
+++ packages/plugin/gitlab-ci/src/generators/docker/generate-service-e2e-gitlab-ci-file-content.ts
@@ -0,0 +1,147 @@
+import {
+  getProjects,
+  Tree,
+} from '@nx/devkit';
+import {
+  GetTargetOptions,
+  HasProject,
+  IsServiceProject,
+  RootDockerOptions,
+} from '@rxap/workspace-utilities';
+import { stringify } from 'yaml';
+import { DockerGeneratorSchema } from './schema';
+import { skipProject } from './skip-project';
+
+const DOT_SERVICE_E2E = {
+  extends: '.run',
+  stage: 'startup',
+  needs: [
+    {
+      job: 'docker',
+      artifacts: false,
+    },
+  ],
+  services: [
+    {
+      name: '${SERVICE_REGISTRY_IMAGE}${IMAGE_SUFFIX}:${CI_PIPELINE_ID}',
+      alias: 'service',
+    },
+  ],
+  environment: {
+    action: 'prepare',
+    name: '$ENVIRONMENT_NAME',
+  },
+  variables: {
+    HOST: 'service',
+    PORT: '3000',
+    ROOT_DOMAIN: '$ROOT_DOMAIN',
+  } as Record<string, string>,
+  artifacts: {
+    name: "${CI_COMMIT_REF_SLUG}_run_${TARGET}",
+    paths: [ "coverage", "junit" ],
+    expire_in: '1 week',
+    reports: { junit: 'junit/**/junit.xml' },
+  },
+  rules: [
+    {
+      if: '$DISABLE_SERVICE_E2E_TESTS',
+      when: 'never',
+    },
+    {
+      if: '$CI_COMMIT_TAG =~ /^v\\d+\\.\\d+\\.\\d+/',
+      when: 'on_success',
+    },
+    {
+      if: '$CI_COMMIT_BRANCH == $CI_DEFAULT_BRANCH',
+      when: 'on_success',
+    },
+    {
+      if: '$CI_MERGE_REQUEST_SOURCE_BRANCH_NAME =~ /^renovate\\// || $CI_COMMIT_BRANCH =~ /^renovate\\//',
+      when: 'on_success',
+    },
+    {
+      if: '$CI_MERGE_REQUEST_SOURCE_BRANCH_NAME =~ /^snyk-/ || $CI_COMMIT_BRANCH =~ /^snyk-/',
+      when: 'on_success',
+    },
+    {
+      when: 'manual',
+      allow_failure: true,
+    },
+  ],
+  tags: [],
+};
+const SERVICE_E2E = {
+  extends: '.service-e2e',
+  variables: {} as Record<string, string>,
+  parallel: {
+    matrix: [],
+  },
+};
+
+export function generateServiceE2eGitlabCiFileContent(
+  tree: Tree,
+  options: DockerGeneratorSchema,
+  rootDocker: RootDockerOptions,
+) {
+
+  const dotServiceE2e = structuredClone(DOT_SERVICE_E2E);
+
+  if (options.tags?.length) {
+    dotServiceE2e.tags = options.tags;
+  }
+
+  const startupYaml = {
+    '.service-e2e': dotServiceE2e,
+    startup: structuredClone(SERVICE_E2E),
+  };
+
+  if (options.gitlab !== false) {
+    startupYaml['.service-e2e'].variables.SERVICE_REGISTRY_IMAGE = '${CI_REGISTRY_IMAGE}';
+  }
+
+  if (options.gcp) {
+    startupYaml['.service-e2e'].variables.SERVICE_REGISTRY_IMAGE = '${GCP_REGISTRY}/${GCP_PROJECT}/${IMAGE_NAME}';
+  }
+
+  if (rootDocker.imageName) {
+    startupYaml.startup.variables.IMAGE_NAME = rootDocker.imageName;
+  }
+
+  for (const [ projectName, project ] of
+    Array.from(getProjects(tree).entries()).sort(([ a ], [ b ]) => a.localeCompare(b))) {
+
+    if (skipProject(tree, options, project, projectName)) {
+      continue;
+    }
+
+    if (!IsServiceProject(project)) {
+      continue;
+    }
+
+    const e2eProjectName = `${projectName}-e2e`;
+    if (!HasProject(tree, e2eProjectName)) {
+      continue;
+    }
+
+    const dockerTargetOptions = GetTargetOptions(project.targets['docker'], 'production');
+
+    const imageSuffix = dockerTargetOptions.imageSuffix as string | undefined;
+
+    const matrix: Record<string, string> = {};
+
+    startupYaml.startup.parallel.matrix.push(matrix);
+
+    if (dockerTargetOptions.imageName && dockerTargetOptions.imageName !== rootDocker.imageName) {
+      matrix.IMAGE_NAME = dockerTargetOptions.imageName as string;
+    }
+
+    matrix.TARGET = `${e2eProjectName}:e2e:ci`;
+
+    if (imageSuffix) {
+      matrix.IMAGE_SUFFIX = imageSuffix;
+    }
+
+  }
+
+  return stringify(startupYaml);
+}
