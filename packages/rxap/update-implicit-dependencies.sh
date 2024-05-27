5bfffee063bbd6ab058aed990dab423f8c8b837e
Merzough Münker <mmuenker@digitaix.com>
2024-05-27 22:22:58 +0200

    chore: sort project list

--- packages/rxap/update-implicit-dependencies.sh
+++ packages/rxap/update-implicit-dependencies.sh
@@ -13,7 +13,10 @@ echo "Remove the items 'workspace' and 'workspace-tools' and 'angular' from the 
 projects=$(echo $projects | jq 'map(select(. != "workspace"))')
 projects=$(echo $projects | jq 'map(select(. != "workspace-tools"))')
 projects=$(echo $projects | jq 'map(select(. != "angular"))')
+projects=$(echo $projects | jq 'map(select(. != "rxap"))')
 
+# sort the projects array by name
+projects=$(echo $projects | jq 'sort_by(.)')
 # set the property implicitDependencies of the package.json to the list of workspace packages
 echo "Set the property implicitDependencies of the package.json to the list of workspace packages:"
 project_json_file="packages/rxap/project.json"
