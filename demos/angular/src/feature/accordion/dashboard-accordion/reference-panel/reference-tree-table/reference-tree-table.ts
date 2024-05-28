71de0dc2b7f1cb70e3ca0ca7ac28207d89abf048
Merzough Münker <mmuenker@digitaix.com>
2024-05-28 18:31:21 +0200

    refactor: cleanup demo schematics

--- demos/angular/src/feature/accordion/dashboard-accordion/reference-panel/reference-tree-table/reference-tree-table.ts
+++
@@ -1,4 +0,0 @@
-import { TableRowMetadata } from '@rxap/material-table-system';
-import { DashboardAccordionReferenceControllerGetRootResponse } from 'open-api-service-app-angular-accordion/responses/dashboard-accordion-reference-controller-get-root.response';
-
-export type IReferenceTreeTable = TableRowMetadata & DashboardAccordionReferenceControllerGetRootResponse[number];
--- demos/angular/src/feature/accordion/routes.ts
+++ demos/angular/src/feature/accordion/routes.ts
@@ -27,10 +27,6 @@ const ROUTES: Route[] = [
         loadComponent: () => import('./complex-accordion/complex-accordion.component'),
       },
       {
-        path: 'dashboard/:uuid',
-        loadComponent: () => import('./dashboard-accordion/dashboard-accordion.component'),
-      },
-      {
         path: 'complex',
         redirectTo: 'complex/' + faker.string.uuid(),
       },
