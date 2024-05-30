3308e6e454bfd406f36de2cc8fb0d983d78ffc97
Merzough Münker <mmuenker@digitaix.com>
2024-05-30 22:19:14 +0200

    chore: ensure the rxap package package groups are updated

--- tools/scripts/lerna/update-rxap-package-group.sh
+++ tools/scripts/lerna/update-rxap-package-group.sh
@@ -2,5 +2,4 @@
 
 yarn nx run rxap:update-package-group --skip-nx-cache
 
-git add "packages/rxap/package.json"
-git commit -m "fix: update package groups"
+git commit -am "fix: update package groups"
