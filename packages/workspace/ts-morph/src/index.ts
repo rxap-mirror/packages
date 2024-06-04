c354ecff141870c76302fd4dc490d1364ca32a4e
Merzough Münker <mmuenker@digitaix.com>
2024-06-04 13:55:12 +0200

    feat: add OverwriteOptions type

--- packages/workspace/ts-morph/src/index.ts
+++ packages/workspace/ts-morph/src/index.ts
@@ -1,5 +1,6 @@
 // region 
 export * from './lib/add-dir';
 export * from './lib/apply-ts-morph-project';
+export * from './lib/overwrite-options';
 export * from './lib/ts-morph-transform';
 // endregion
