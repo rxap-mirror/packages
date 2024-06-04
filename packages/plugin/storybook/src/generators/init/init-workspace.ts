b71ceeeb0dbaf84fe4ad4798682a7eadab6d2b43
Merzough Münker <mmuenker@digitaix.com>
2024-06-04 13:19:55 +0200

    fix: add missing required package

--- packages/plugin/storybook/src/generators/init/init-workspace.ts
+++ packages/plugin/storybook/src/generators/init/init-workspace.ts
@@ -9,5 +9,6 @@ export async function initWorkspace(tree: Tree, options: InitGeneratorSchema) {
   await AddPackageJsonDevDependency(tree, '@storybook/addon-themes', 'latest', { soft: true });
   await AddPackageJsonDevDependency(tree, '@storybook/addon-interactions', 'latest', { soft: true });
   await AddPackageJsonDevDependency(tree, '@storybook/addon-themes', 'latest', { soft: true });
+  await AddPackageJsonDevDependency(tree, '@storybook/icons', 'latest', { soft: true });
 
 }
