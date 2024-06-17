2a08aa23b8a7fdb4fc6fe7d72a5fd51b1d98672a
Merzough Münker <mmuenker@digitaix.com>
2024-06-17 10:28:22 +0200

    fix: delete target

--- packages/plugin/library/src/generators/init/update-project-targets.ts
+++ packages/plugin/library/src/generators/init/update-project-targets.ts
@@ -1,6 +1,7 @@
 import { ProjectConfiguration } from '@nx/devkit';
 import {
   CoerceTarget,
+  DeleteTarget,
   IsGeneratorProject,
   IsPluginProject,
   IsSchematicProject,
@@ -11,6 +12,8 @@ export function updateProjectTargets(project: ProjectConfiguration, options: Ini
 
   if (options.targets?.indexExport !== false && !IsPluginProject(project) && !IsGeneratorProject(project) && !IsSchematicProject(project)) {
     CoerceTarget(project, 'index-export', {});
+  } else if (options.targets?.indexExport === false) {
+    DeleteTarget(project, 'index-export');
   } else {
     console.log('skip index-export target for plugin, generator or schematic project'.yellow);
   }
