15399eed87c4444c85af31ac1b61cb286d6dc4b8
Merzough Münker <mmuenker@digitaix.com>
2024-06-17 21:11:03 +0200

    fix: add the rxap package

--- packages/plugin/workspace/src/generators/init/add-package-dependencies.ts
+++ packages/plugin/workspace/src/generators/init/add-package-dependencies.ts
@@ -2,6 +2,7 @@ import { Tree } from '@nx/devkit';
 import { AddPackageJsonDevDependency } from '@rxap/workspace-utilities';
 
 export async function addPackageDependencies(tree: Tree) {
+  await AddPackageJsonDevDependency(tree, 'rxap', 'latest', { soft: true });
   await AddPackageJsonDevDependency(tree, 'husky', 'latest', { soft: true });
   await AddPackageJsonDevDependency(tree, 'nx-cloud', 'latest', { soft: true });
   await AddPackageJsonDevDependency(tree, '@commitlint/cli', 'latest', { soft: true });
