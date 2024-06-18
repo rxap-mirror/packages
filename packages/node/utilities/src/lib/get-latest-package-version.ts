d499a8a0e7e6f7ee0b54e9f14bf77509ff7ae910
Merzough Münker <mmuenker@digitaix.com>
2024-06-18 10:32:02 +0200

    feat: use the rxap package to resolve package versions

--- packages/node/utilities/src/lib/get-latest-package-version.ts
+++ packages/node/utilities/src/lib/get-latest-package-version.ts
@@ -26,9 +26,38 @@ function updateLastPackageVersionCache(packageName: string, version: string) {
   writeFileSync(CACHE_FILE, JSON.stringify(LATEST_PACKAGE_VERSIONS, null, 2));
 }
 
+function hasRxapPackage() {
+  try {
+    const packageJsonFilePath = require.resolve('rxap');
+    return true;
+  } catch (e: any) {
+    console.log('No rxap package found: ' + e.message);
+  }
+  return false;
+}
+
+function getRxapPackageJson() {
+  try {
+    const packageJsonFilePath = require.resolve('rxap/package.json');
+    return require(packageJsonFilePath);
+  } catch (e: any) {
+    throw new Error('No rxap package found: ' + e.message);
+  }
+}
+
 export async function GetLatestPackageVersion(packageName: string, skipCache?: boolean): Promise<string | null> {
   if (!skipCache && LATEST_PACKAGE_VERSIONS[packageName]) {
     return LATEST_PACKAGE_VERSIONS[packageName];
+  }
+
+  if (hasRxapPackage()) {
+    const rxapPackageJson = getRxapPackageJson();
+    const packageGroupList: Array<{ package: string, version: string }> = rxapPackageJson['nx-migrations']?.packageGroup ?? [];
+    const packageGroup = packageGroupList.find(group => group.package === packageName);
+    if (packageGroup) {
+      updateLastPackageVersionCache(packageName, packageGroup.version);
+      return packageGroup.version;
+    }
   }
 
   const info = await GetPackageInfo(packageName, skipCache);
