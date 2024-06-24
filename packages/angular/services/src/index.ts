5af4a685824a3717ed36f521cea433224a0bf893
Merzough Münker <mmuenker@digitaix.com>
2024-06-24 13:52:24 +0200

    refactor: remove service

--- packages/angular/services/src/index.ts
+++ packages/angular/services/src/index.ts
@@ -1,10 +1,9 @@
-// region 
+// region
 export * from './lib/avatar-image.service';
 export * from './lib/footer.service';
 export * from './lib/header.service';
 export * from './lib/image-loader.service';
 export * from './lib/loading-indicator.service';
 export * from './lib/reset.service';
-export * from './lib/version.service';
 export * from './lib/window-container-sidenav.service';
 // endregion
