89696a27838c5668273fb6686c2d9aeb8f3fe34a
Merzough Münker <mmuenker@digitaix.com>
2024-06-24 14:13:14 +0200

    chore: update start script

--- tools/scripts/start-local-dev-services.sh
+++ tools/scripts/start-local-dev-services.sh
@@ -2,14 +2,51 @@
 
 GIT_ROOT=$(git rev-parse --show-toplevel)
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-BLUE='\033[0;34m'
-NC='\033[0m' # No Color
+source "${GIT_ROOT}/tools/scripts/colors.sh"
+
+# Initialize default values for flags
+local_mode="false"
+skip_generate="false"
+always_yes="false"
+configuration="development"
+without_services="false"
+without_frontends="false"
+
+# Loop through arguments and process them
+while [ "$1" != "" ]; do
+  case $1 in
+  --local) local_mode="true" ;;
+  --skip-generate) skip_generate="true" ;;
+  --yes) always_yes="true" ;;
+  --production) configuration="production" ;;
+  --without-services) without_services="true" ;;
+  --without-frontends) without_frontends="true" ;;
+  *)
+    echo "Usage: $0 --local"
+    exit 1
+    ;;
+  esac
+  shift
+done
+
+if [ "$local_mode" == "true" ]; then
+  echo "Local mode activated."
+  # Place your code for local mode here
+else
+  echo "Normal mode."
+  # Place your code for normal mode here
+fi
 
 cd "${GIT_ROOT}" || exit 1
 
-yarn nx run workspace:docker-compose
+if [ ! -f .env ]; then
+  echo "No .env file found. Generating one..."
+  yarn init:env
+fi
+
+if [[ $skip_generate != "true" ]]; then
+  yarn nx run workspace:docker-compose
+fi
 
 # Get the current branch name or the commit hash if in detached HEAD state
 current_branch=$(git symbolic-ref --short -q HEAD || git rev-parse --short HEAD)
@@ -19,6 +56,7 @@ channel=${current_branch//\//-}
 source .env
 
 CHANNEL=${CHANNEL:-$channel}
+BUILD_LOCAL=${BUILD_LOCAL:-$local_mode}
 
 echo "ROOT_DOMAIN=$ROOT_DOMAIN"
 echo "REMOTE_DOMAIN=$REMOTE_DOMAIN"
@@ -27,16 +65,31 @@ echo "HOST_IP=$HOST_IP"
 echo "REGISTRY=$REGISTRY"
 echo "ROOT_DOMAIN_PORT=$ROOT_DOMAIN_PORT"
 echo "SKIP_PULL=$SKIP_PULL"
+echo "BUILD_LOCAL=$BUILD_LOCAL"
 
-read -p "Does everything look good (y/N)? "
+if [[ $always_yes != "true" ]]; then
+  read -rp "Does everything look good (y/N)? "
 
-if [[ ! $REPLY =~ ^[Yy]$ ]]; then
-  exit 1
+  if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    exit 1
+  fi
 fi
+
 
 export CHANNEL
 
-LOCAL_DOCKER_COMPOSE_FILES="-f docker-compose.services.yml -f docker-compose.frontends.yml"
+# region check if authenticated with docker
+
+/bin/bash "${GIT_ROOT}/tools/scripts/check-docker-auth.sh" "${REGISTRY:-registry.gitlab.com}" || docker login "${REGISTRY:-registry.gitlab.com}"# endregion
+
+LOCAL_DOCKER_COMPOSE_FILES=""
+if [[ $without_services == "false" ]]; then
+  LOCAL_DOCKER_COMPOSE_FILES="${LOCAL_DOCKER_COMPOSE_FILES} -f docker-compose.services.yml"
+fi
+if [[ $without_frontends == "false" ]]; then
+  LOCAL_DOCKER_COMPOSE_FILES="${LOCAL_DOCKER_COMPOSE_FILES} -f docker-compose.frontends.yml"
+fi
+
 REMOTE_DOCKER_COMPOSE_FILES="-f docker-compose.yml"
 
 # Append all matching override files
@@ -50,48 +103,45 @@ DOCKER_COMPOSE_FILES="${REMOTE_DOCKER_COMPOSE_FILES} ${LOCAL_DOCKER_COMPOSE_FILE
 
 docker compose $REMOTE_DOCKER_COMPOSE_FILES pull || exit 1
 
-if [[ $SKIP_PULL != "true" ]]; then
+function build {
 
-  docker compose $DOCKER_COMPOSE_FILES pull && VALID=true || VALID=false
+  echo "Building images locally"
 
-  if [[ $VALID == true ]]; then
-    echo -e "${GREEN}channel $channel is valid${NC}"
-  else
-    echo -e "${RED}channel $channel is not valid${NC}"
+  echo "docker compose $DOCKER_COMPOSE_FILES down"
+  docker compose $DOCKER_COMPOSE_FILES down
 
-    read -p "Do you want to use the 'development' channel (y/N)? "
-
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-      channel=development
-      CHANNEL=$channel
-    else
-      echo "exiting"
-      exit 1
-    fi
+  local PARAMS="--target docker --configuration $configuration --tag $channel"
+  if [[ $REGISTRY != "" ]]; then
+    PARAMS="$PARAMS --imageRegistry $REGISTRY"
   fi
+  echo "yarn nx run-many $PARAMS"
+  # don't use double quotes around $PARAMS here. or else the $PARAMS will be expanded and passed as a single argument
+  yarn nx run-many $PARAMS
 
-  export CHANNEL
+}
 
-  docker compose $DOCKER_COMPOSE_FILES pull && VALID=true || VALID=false
+if [[ $BUILD_LOCAL == "true" ]]; then
 
-  if [[ $VALID == true ]]; then
-    echo -e "${GREEN}channel $channel is valid${NC}"
-  else
-    echo -e "${RED}channel $channel is not valid${NC}"
+  build
 
-    read -p "Do you want to try to build the images locally (y/N)? "
+else
 
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-      PARAMS="--target docker --configuration production --tag $channel"
-      if [[ $REGISTRY != "" ]]; then
-        PARAMS="$PARAMS --imageRegistry $REGISTRY"
-      fi
-      echo "yarn nx run-many $PARAMS"
-      # don't use double quotes around $PARAMS here. or else the $PARAMS will be expanded and passed as a single argument
-      yarn nx run-many $PARAMS
+  if [[ $SKIP_PULL != "true" ]]; then
+
+    if docker compose $DOCKER_COMPOSE_FILES pull; then
+      echo -e "${GREEN}channel $channel is valid${NC}"
     else
-      echo "exiting"
-      exit 1
+      echo -e "${RED}channel $channel is not valid${NC}"
+
+      read -rp "Do you want to try to build the images locally (y/N)? "
+
+      if [[ $REPLY =~ ^[Yy]$ ]]; then
+        build
+      else
+        echo "exiting"
+        exit 1
+      fi
+
     fi
 
   fi
