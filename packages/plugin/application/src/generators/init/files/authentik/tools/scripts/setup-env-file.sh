3281a0d4576d3a83304fbf57b8063ec5df9b78ab
Merzough Münker <mmuenker@digitaix.com>
2024-06-25 17:00:21 +0200

    chore: cleanup setup envs

--- packages/plugin/application/src/generators/init/files/authentik/tools/scripts/setup-env-file.sh
+++ packages/plugin/application/src/generators/init/files/authentik/tools/scripts/setup-env-file.sh
@@ -22,3 +22,7 @@ echo "NODE_EXTRA_CA_CERTS=${GIT_ROOT}/docker/traefik/tls/ca.crt" >> "${GIT_ROOT}
 echo "HOST_IP=$(ip addr show | grep -w inet | grep -v 127.0.0.1 | grep -v 172. | awk '{print $2}' | cut -d/ -f1 | head -n 1)" >> "${GIT_ROOT}/.env"
 echo "ROOT_DOMAIN_PORT=8443" >> "${GIT_ROOT}/.env"
 echo "LOG_LEVEL=verbose" >> "${GIT_ROOT}/.env"
+echo "PG_PASS=$(pwgen -s 40 1)" >> "${GIT_ROOT}/.env"
+echo "AUTHENTIK_SECRET_KEY=$(pwgen -s 50 1)" >> "${GIT_ROOT}/.env"
+echo "AUTHENTIK_ERROR_REPORTING__ENABLED=true" >> "${GIT_ROOT}/.env"
+echo "AUTHENTIK_BOOTSTRAP_PASSWORD=development" >> "${GIT_ROOT}/.env"
