827560da1305ebe98096da068f43ba3a2a54f393
Merzough Münker <mmuenker@digitaix.com>
2024-06-25 10:25:28 +0200

    fix: support dynamic props

--- packages/ts-morph/src/lib/angular/coerce-layout-routes.ts
+++ packages/ts-morph/src/lib/angular/coerce-layout-routes.ts
@@ -1,5 +1,6 @@
 import {
   SourceFile,
+  WriterFunction,
   Writers,
 } from 'ts-morph';
 import { CoerceImports } from '../coerce-imports';
@@ -8,40 +9,103 @@ import {
   CoerceRoutesOptions,
 } from './coerce-routes';
 
-export type CoerceLayoutRoutesOptions = CoerceRoutesOptions;
+export interface CoerceLayoutRoutesOptions extends CoerceRoutesOptions {
+  component?: 'LayoutComponent' | 'MinimalLayoutComponent';
+  withNavigation?: boolean;
+  withDefaultHeader?: boolean;
+  withStatusCheckGuard?: boolean;
+}
 
 export function CoerceLayoutRoutes(sourceFile: SourceFile, options: CoerceLayoutRoutesOptions = {}) {
+
+  const {
+    component = 'LayoutComponent',
+    withNavigation = true,
+    withDefaultHeader = true,
+    withStatusCheckGuard = true,
+  } = options;
+
+  const obj: Record<string, string | WriterFunction> = {
+    path: w => w.quote(''),
+    component,
+    children: w => {
+      w.write('[');
+      w.indent(() => {
+        Writers.object({
+          path: w => w.quote('**'),
+          redirectTo: w => w.quote('')
+        })(w);
+      });
+      w.write(']');
+    },
+    providers: w => {
+      w.write('[');
+      w.indent(() => {
+        w.write('provideLayout(');
+        if (withNavigation) {
+          w.newLine();
+          w.indent(() => {
+            w.write('withNavigationConfig(APP_NAVIGATION)');
+            w.write(',');
+          });
+        }
+        if (withDefaultHeader) {
+          w.newLine();
+          w.indent(() => {
+            w.write('widthDefaultHeaderComponent()');
+            w.write(',');
+          });
+        }
+        if (withDefaultHeader || withNavigation) {
+          w.newLine();
+        }
+        w.write(')');
+      });
+      w.write(']');
+    }
+  };
+
+  if (withStatusCheckGuard) {
+    obj['canActivateChild'] = '[StatusCheckGuard]';
+    CoerceImports(sourceFile, [
+      {
+        namedImports: [ 'StatusCheckGuard' ],
+        moduleSpecifier: '@rxap/ngx-status-check'
+      }
+    ]);
+  }
+
+  if (withNavigation) {
+    CoerceImports(sourceFile, [
+      {
+        namedImports: [ 'APP_NAVIGATION' ],
+        moduleSpecifier: './app.navigation'
+      },
+      {
+        namedImports: [ 'withNavigationConfig' ],
+        moduleSpecifier: '@rxap/layout'
+      }
+    ]);
+  }
+
+  if (withDefaultHeader) {
+    CoerceImports(sourceFile, {
+      namedImports: [ 'widthDefaultHeaderComponent' ],
+      moduleSpecifier: '@rxap/layout'
+    });
+  }
+
+  CoerceImports(sourceFile, {
+    namedImports: [ component ],
+    moduleSpecifier: '@rxap/layout'
+  });
 
   const variableDeclaration = CoerceRoutes(sourceFile, {
     ...options,
     initializer: options.initializer ?? (w => {
       w.writeLine('[');
       w.indent(() => {
-        Writers.object({
-          path: w => w.quote(''),
-          component: 'LayoutComponent',
-          canActivateChild: '[StatusCheckGuard]',
-          children: w => {
-            w.writeLine('[');
-            w.indent(() => {
-              Writers.object({
-                path: w => w.quote('**'),
-                redirectTo: w => w.quote('')
-              })(w);
-            });
-            w.write(']');
-          },
-          providers: w => {
-            w.write('[');
-            w.indent(() => {
-              w.write('APP_NAVIGATION_PROVIDER');
-              w.write(',');
-              w.newLine();
-              w.write('NavigationService');
-            });
-            w.write(']');
-          }
-        })(w);
+        Writers.object(obj)(w);
         w.write(',');
       });
 
@@ -53,21 +117,6 @@ export function CoerceLayoutRoutes(sourceFile: SourceFile, options: CoerceLayout
       w.write(']');
     })
   });
-
-  CoerceImports(sourceFile, [
-    {
-      namedImports: [ 'LayoutComponent', 'NavigationService' ],
-      moduleSpecifier: '@rxap/layout'
-    },
-    {
-      namedImports: [ 'StatusCheckGuard' ],
-      moduleSpecifier: '@rxap/ngx-status-check'
-    },
-    {
-      namedImports: [ 'APP_NAVIGATION_PROVIDER' ],
-      moduleSpecifier: './app.navigation'
-    }
-  ]);
 
   return variableDeclaration;
 
