ad41a1d377d16560b56c15f4e08709a1219575da
Merzough Münker <mmuenker@digitaix.com>
2024-06-28 23:43:42 +0200

    chore: add package @rxap/nest-oauth2-proxy

---
+++ packages/nest/oauth2-proxy/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'nest-oauth2-proxy',
+  preset: '../../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/packages/nest/oauth2-proxy',
+};
