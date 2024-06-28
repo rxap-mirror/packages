4ee13238b58ba6255b313514a475ee131c5ce1fa
Merzough Münker <mmuenker@digitaix.com>
2024-06-28 23:33:49 +0200

    chore: add package @rxap/ngx-oauth2-proxy

---
+++ packages/angular/oauth2-proxy/src/test-setup.ts
@@ -0,0 +1,19 @@
+// @ts-expect-error https://thymikee.github.io/jest-preset-angular/docs/getting-started/test-environment
+globalThis.ngJest = {
+  testEnvironmentOptions: {
+    errorOnUnknownElements: true,
+    errorOnUnknownProperties: true,
+  },
+};
+import 'jest-preset-angular/setup-jest';
+
+import { TextDecoder, TextEncoder } from 'util';
+global.TextEncoder ??= TextEncoder as any;
+global.TextDecoder ??= TextDecoder as any;
+
+import '@angular/localize/init';
+jest.spyOn(global as any, '$localize').mockImplementation((...args: any[]) => {
+  // This template tag function just returns the first argument with no transformations.
+  // Change this to fit your unit test needs.
+  return args[0];
+});
