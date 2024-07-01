415c70e7ef985f1014b81864362a70e34a16ac64
Merzough Münker <mmuenker@digitaix.com>
2024-07-01 12:34:49 +0200

    fix: install the rxap cli tool

--- packages/plugin/workspace/src/generators/init/add-package-dependencies.ts
+++ packages/plugin/workspace/src/generators/init/add-package-dependencies.ts
@@ -3,6 +3,7 @@ import { AddPackageJsonDevDependency } from '@rxap/workspace-utilities';
 
 export async function addPackageDependencies(tree: Tree) {
   await AddPackageJsonDevDependency(tree, 'rxap', 'latest', { soft: true });
+  await AddPackageJsonDevDependency(tree, '@rxap/cli', 'latest', { soft: true });
   await AddPackageJsonDevDependency(tree, 'husky', 'latest', { soft: true });
   await AddPackageJsonDevDependency(tree, 'nx-cloud', 'latest', { soft: true });
   await AddPackageJsonDevDependency(tree, '@commitlint/cli', 'latest', { soft: true });
