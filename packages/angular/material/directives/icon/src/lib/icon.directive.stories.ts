ce6f23efac13dceefa388688371957884f9bb62d
Merzough Münker <mmuenker@digitaix.com>
2024-07-02 08:31:30 +0200

    feat: support string icon config

---
+++ packages/angular/material/directives/icon/src/lib/icon.directive.stories.ts
@@ -0,0 +1,56 @@
+import {
+  Component,
+  input,
+} from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { IconDirective } from '@rxap/material-directives/icon';
+import { IconConfig } from '@rxap/utilities';
+import { Meta } from '@storybook/angular';
+
+@Component({
+  selector: 'rxap-test-component',
+  template: '<mat-icon [rxapIcon]="icon()"></mat-icon>',
+  standalone: true,
+  imports: [
+    MatIcon,
+    IconDirective,
+  ]
+})
+class TestComponent {
+
+  icon = input.required<IconConfig | null>();
+
+}
+
+export default {
+  title: 'IconDirective',
+  component: TestComponent,
+} as Meta<TestComponent>;
+
+export const StringInput = {
+  args: {
+    icon: 'delete',
+  },
+};
+
+export const MaterialInput = {
+  args: {
+    icon: {
+      icon: 'visibility',
+    },
+  },
+};
+
+export const SvgInput = {
+  args: {
+    icon: {
+      svgIcon: 'view-dashboard',
+    },
+  },
+};
+
+export const Empty = {
+  args: {
+    icon: null,
+  },
+};
--- packages/angular/material/directives/icon/src/lib/icon.directive.ts
+++ packages/angular/material/directives/icon/src/lib/icon.directive.ts
@@ -1,89 +1,79 @@
 import {
   Directive,
+  effect,
   ElementRef,
-  Inject,
-  Input,
-  OnChanges,
+  inject,
+  input,
   Renderer2,
-  SimpleChanges,
 } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 import {
   IconConfig,
   IsMaterialIcon,
   IsSvgIcon,
+  NormalizedIconConfig,
+  NormalizeIconConfig,
 } from '@rxap/utilities';
-import { MatIcon } from '@angular/material/icon';
 
 @Directive({
   selector: 'mat-icon[rxapIcon]',
   standalone: true,
 })
-export class IconDirective implements OnChanges {
-  @Input('rxapIcon') public icon?: IconConfig | null;
+export class IconDirective {
 
-  constructor(
-    @Inject(MatIcon)
-    private readonly matIcon: MatIcon,
-    @Inject(Renderer2)
-    private readonly renderer: Renderer2,
-    @Inject(ElementRef)
-    private readonly elementRef: ElementRef,
-  ) {
-  }
+  public readonly icon = input.required<IconConfig | null>({ alias: 'rxapIcon' });
 
-  public get isSimpleIcon(): boolean {
-    return typeof this.icon === 'string';
-  }
+  private readonly matIcon = inject(MatIcon);
+  private readonly renderer = inject(Renderer2);
+  private readonly elementRef = inject(ElementRef);
 
-  public get complexIcon(): IconConfig | null {
-    return this.icon as any;
-  }
-
-  public get simpleIcon(): string {
-    return this.icon as any;
-  }
-
-  public ngOnChanges(changes: SimpleChanges) {
-    if (changes['icon']) {
-      this.updateIcon();
-    }
-  }
-
-  public updateIcon(): void {
-    if (this.isSimpleIcon) {
-      this.matIcon._elementRef.nativeElement.textContent = this.simpleIcon;
-      this.matIcon.ngOnInit();
-    } else {
-      const icon = this.complexIcon;
+  constructor() {
+    effect(() => {
+      const icon = this.icon();
       if (icon) {
-        this.matIcon.color = icon.color;
-        if (icon.inline !== undefined) {
-          this.matIcon.inline = icon.inline;
-        }
-        if (icon.fontColor) {
-          this.renderer.setStyle(
-            this.elementRef.nativeElement,
-            'color',
-            icon.fontColor,
-          );
-        } else {
-          this.renderer.removeStyle(this.elementRef.nativeElement, 'color');
-        }
-        if (IsMaterialIcon(icon)) {
-          this.matIcon._elementRef.nativeElement.textContent = icon.icon;
-          this.matIcon.ngOnInit();
-        } else if (IsSvgIcon(icon)) {
-          if (icon.fontIcon) {
-            this.matIcon.fontIcon = icon.fontIcon;
-          }
-          if (icon.fontSet) {
-            this.matIcon.fontSet = icon.fontSet;
-          }
-          this.matIcon.svgIcon = icon.svgIcon;
-        }
+        this.setIcon(NormalizeIconConfig(icon));
+      } else {
+        this.clearIcon();
       }
+    });
+  }
+
+  private setIcon(icon: NormalizedIconConfig) {
+    this.matIcon.color = icon.color;
+    if (icon.inline !== undefined) {
+      this.matIcon.inline = icon.inline;
+    }
+    if (icon.fontColor) {
+      this.renderer.setStyle(
+        this.elementRef.nativeElement,
+        'color',
+        icon.fontColor,
+      );
+    } else {
+      this.renderer.removeStyle(this.elementRef.nativeElement, 'color');
+    }
+    if (IsMaterialIcon(icon)) {
+      this.matIcon._elementRef.nativeElement.textContent = icon.icon;
+      this.matIcon.ngOnInit();
+    } else if (IsSvgIcon(icon)) {
+      if (icon.fontIcon) {
+        this.matIcon.fontIcon = icon.fontIcon;
+      }
+      if (icon.fontSet) {
+        this.matIcon.fontSet = icon.fontSet;
+      }
+      this.matIcon.svgIcon = icon.svgIcon;
     }
   }
+
+  private clearIcon() {
+    this.matIcon._elementRef.nativeElement.textContent = '';
+    this.matIcon.fontIcon = '';
+    this.matIcon.fontSet = '';
+    this.matIcon.svgIcon = '';
+    this.matIcon.ngOnInit();
+  }
+
 }
 
 
