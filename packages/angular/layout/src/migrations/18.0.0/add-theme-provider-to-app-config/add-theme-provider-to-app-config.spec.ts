be8d7d3750c08222eaacd2011d1a3768c783dc03
Merzough Münker <mmuenker@digitaix.com>
2024-07-02 20:59:42 +0200

    chore: add migrations

---
+++ packages/angular/layout/src/migrations/18.0.0/add-theme-provider-to-app-config/add-theme-provider-to-app-config.spec.ts
@@ -0,0 +1,61 @@
+import { createTreeWithEmptyWorkspace } from '@nx/devkit/testing';
+import {
+  addProjectConfiguration,
+  Tree,
+} from '@nx/devkit';
+
+import update from './add-theme-provider-to-app-config';
+
+describe('add-theme-provider-to-app-config migration', () => {
+  let tree: Tree;
+
+  beforeEach(() => {
+    tree = createTreeWithEmptyWorkspace({ layout: 'apps-libs' });
+    addProjectConfiguration(tree, 'service', {
+      root: 'apps/service',
+      projectType: 'application',
+      sourceRoot: 'apps/service/src',
+      tags: [ 'service' ],
+      targets: {},
+    });
+    addProjectConfiguration(tree, 'angularA', {
+      root: 'apps/angularA',
+      projectType: 'application',
+      sourceRoot: 'apps/angularA/src',
+      tags: [ 'angular' ],
+      targets: {},
+    });
+    addProjectConfiguration(tree, 'angularB', {
+      root: 'apps/angularB',
+      projectType: 'application',
+      sourceRoot: 'apps/angularB/src',
+      tags: [ 'angular' ],
+      targets: {},
+    });
+    tree.write('apps/angularB/src/app/layout.routes.ts', '');
+    addProjectConfiguration(tree, 'angularC', {
+      root: 'apps/angularC',
+      projectType: 'application',
+      sourceRoot: 'apps/angularC/src',
+      tags: [ 'angular' ],
+      targets: {},
+    });
+    tree.write('apps/angularB/src/app/layout.routes.ts', '');
+    tree.write('apps/angularB/src/app/app.config.ts', '');
+    addProjectConfiguration(tree, 'angularD', {
+      root: 'apps/angularD',
+      projectType: 'application',
+      sourceRoot: 'apps/angularD/src',
+      tags: [ 'angular' ],
+      targets: {},
+    });
+    tree.write('apps/angularC/src/app/layout.routes.ts', '');
+    tree.write('apps/angularC/src/app/app.config.ts', 'export const appConfig = { providers: [] };');
+  });
+
+  it('should run successfully', () => {
+    update(tree);
+    expect(tree.read('apps/angularB/src/app/app.config.ts', 'utf-8')).toMatchSnapshot();
+    expect(tree.read('apps/angularC/src/app/app.config.ts', 'utf-8')).toMatchSnapshot();
+  });
+});
---
+++ packages/angular/layout/src/migrations/18.0.0/add-theme-provider-to-app-config/add-theme-provider-to-app-config.ts
@@ -0,0 +1,44 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+  getProjects,
+  Tree,
+} from '@nx/devkit';
+import {
+  CoerceAppConfigProvider,
+  CoerceImports,
+} from '@rxap/ts-morph';
+import { TsMorphAngularProjectTransform } from '@rxap/workspace-ts-morph';
+import {
+  GetProjectSourceRoot,
+  IsAngularProject,
+  IsApplicationProject,
+} from '@rxap/workspace-utilities';
+
+export default function update(host: Tree) {
+  for (const [ projectName, project ] of getProjects(host)) {
+    if (!IsApplicationProject(project) || !IsAngularProject(project)) {
+      continue;
+    }
+    const projectSourceRoot = GetProjectSourceRoot(host, projectName);
+    const layoutRoutesPath = `${ projectSourceRoot }/app/layout.routes.ts`;
+    if (!host.exists(layoutRoutesPath)) {
+      console.log(`The project ${ projectName } does not have a layout.routes.ts file. Skip Migration`);
+      continue;
+    }
+    if (!host.exists(`${ projectSourceRoot }/app/app.config.ts`)) {
+      console.log(`The project ${ projectName } does not have a app.config.ts file. Skip Migration`);
+      continue;
+    }
+    TsMorphAngularProjectTransform(host, {
+      project: projectName,
+    }, (_, [ sourceFile ]) => {
+      CoerceAppConfigProvider(sourceFile, {
+        providers: [ 'provideTheme()' ],
+      });
+      CoerceImports(sourceFile, {
+        moduleSpecifier: '@rxap/ngx-theme',
+        namedImports: [ 'provideTheme' ],
+      });
+    }, [ '/app/app.config.ts' ]);
+  }
+}
