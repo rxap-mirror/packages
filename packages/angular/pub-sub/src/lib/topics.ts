1ebb7729c744492d6e3b66d04d15a163d7eaa66d
Merzough Münker <mmuenker@digitaix.com>
2024-07-02 22:15:50 +0200

    docs: improve docs

--- packages/angular/pub-sub/src/lib/topics.ts
+++ packages/angular/pub-sub/src/lib/topics.ts
@@ -1,8 +1,11 @@
 export const RXAP_TOPICS = {
   theme: {
-    // restore is used by the UserSettingsThemeService after the theme is loaded and then used by the ThemeService to
-    // set the restored theme properties
-    // changed is used by the ThemeService to notify the UserSettingsThemeService that the theme properties have changed
+    /**
+     * restore is used by the UserSettingsThemeService after the theme is loaded and then used by the ThemeService to
+     * set the restored theme properties
+     *
+     * changed is used by the ThemeService to notify the UserSettingsThemeService that the theme properties have changed
+     */
     density: {
       restore: 'rxap:theme:density:restore',
       changed: 'rxap:theme:density:changed',
