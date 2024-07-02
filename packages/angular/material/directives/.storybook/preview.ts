11313b2c1ac1e98e7437db9ece41c35c63accc5b
Merzough Münker <mmuenker@digitaix.com>
2024-07-02 08:15:28 +0200

    chore: add storybook

---
+++ packages/angular/material/directives/.storybook/preview.ts
@@ -0,0 +1,35 @@
+import docJson from '../documentation.json';
+import { setCompodocJson } from '@storybook/addon-docs/angular';
+import { HttpClientModule } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
+import { ProvideIconAssetPath } from '@rxap/icon';
+import { ProvideConfig } from '@rxap/config';
+import { ProvideEnvironment } from '@rxap/environment';
+import { withThemeByClassName } from '@storybook/addon-themes';
+import { applicationConfig } from '@storybook/angular';
+import '@angular/localize/init';
+
+setCompodocJson(docJson);
+
+export const decorators = [
+  withThemeByClassName({
+    themes: {
+      light: '',
+      dark: 'dark',
+    },
+    defaultTheme: 'light',
+  }),
+  applicationConfig({
+    providers: [
+      importProvidersFrom(HttpClientModule),
+      ProvideIconAssetPath(['mdi.svg', 'custom.svg']),
+      provideNoopAnimations(),
+      ProvideConfig(),
+      ProvideEnvironment({
+        app: 'angular-material-directives-storybook',
+        production: false,
+      }),
+    ],
+  }),
+];
