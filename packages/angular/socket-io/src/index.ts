e05dcdabfb936402c014624c913794826ec85a0a
Merzough Münker <mmuenker@digitaix.com>
2024-07-10 15:11:37 +0200

    build: add missing index exports

--- packages/angular/socket-io/src/index.ts
+++ packages/angular/socket-io/src/index.ts
@@ -1,5 +1,7 @@
-// region
+// region 
+export * from './lib/provide';
 export * from './lib/socket-io.config';
 export * from './lib/socket-io.module';
 export * from './lib/socket-io.service';
+export * from './lib/tokens';
 // endregion
