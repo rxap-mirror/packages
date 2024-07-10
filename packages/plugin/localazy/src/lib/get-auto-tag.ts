3641c695e3a70f0b6418700987c23346f64329d4
Merzough Münker <mmuenker@digitaix.com>
2024-07-10 13:07:19 +0200

    fix: only create a auto tag for protected refs

--- packages/plugin/localazy/src/lib/get-auto-tag.ts
+++ packages/plugin/localazy/src/lib/get-auto-tag.ts
@@ -1,4 +1,8 @@
 export function GetAutoTag(): string | null {
+  if (process.env.CI_COMMIT_REF_PROTECTED !== 'true') {
+    console.log('Skip auto tag because CI_COMMIT_REF_PROTECTED is not true');
+    return null;
+  }
   if (process.env.CI_COMMIT_REF_SLUG) {
     console.log('Get auto tag from CI_COMMIT_REF_SLUG');
     return process.env.CI_COMMIT_REF_SLUG;
