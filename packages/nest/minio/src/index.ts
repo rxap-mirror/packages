0c487c9bedb21cf7e6673e4eea63fe0846d6a5d4
Merzough Münker <mmuenker@digitaix.com>
2024-08-05 23:01:19 +0200

    chore(release): version
     - @rxap/nest-minio@10.1.5-dev.0

--- packages/nest/minio/src/index.ts
+++ packages/nest/minio/src/index.ts
@@ -3,4 +3,5 @@ export * from './lib/configurable-module-builder';
 export * from './lib/minio.health-indicator';
 export * from './lib/minio.module';
 export * from './lib/minio.service';
+export * from './lib/tokens';
 // endregion
