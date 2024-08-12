f6ff05d5c6caa3454ea55f07661e17ae953d27ea
Merzough Münker <mmuenker@digitaix.com>
2024-08-12 14:36:14 +0200

    chore: add missing index exports

--- packages/angular/oauth2-proxy/src/index.ts
+++ packages/angular/oauth2-proxy/src/index.ts
@@ -1,4 +1,6 @@
 // region 
+export * from './lib/get-oauth2-proxy-user-profile.method';
+export * from './lib/oauth2-proxy-user-profile.data-source';
 export * from './lib/oauth2-proxy.guard';
 export * from './lib/provide-oauth2-proxy';
 // endregion
