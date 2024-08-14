bef04385bf30c4e7fbd0a2b231d2d67d70f07bd3
Merzough Münker <mmuenker@digitaix.com>
2024-08-14 11:10:39 +0200

    fix: improve types

--- packages/nest/rabbitmq/src/lib/client-rmq-exchange-module-options-factory.ts
+++ packages/nest/rabbitmq/src/lib/client-rmq-exchange-module-options-factory.ts
@@ -1,13 +1,16 @@
-import {
-  ClientProvider,
-  ClientsModuleOptionsFactory,
-} from '@nestjs/microservices';
+import { ClientsModuleOptionsFactory } from '@nestjs/microservices';
+import { CustomClientOptions } from '@nestjs/microservices/interfaces';
 import { ClientRMQExchange } from './client-rmq-exchange';
+import { ExchangeRmqOptions } from './options';
 import { RabbitmqOptionsFactory } from './rabbitmq-options-factory';
+
+export interface ClientRmqExchangeModuleOptions extends CustomClientOptions {
+  options: ExchangeRmqOptions
+}
 
 export class ClientRmqExchangeModuleOptionsFactory extends RabbitmqOptionsFactory implements ClientsModuleOptionsFactory {
 
-  async createClientOptions(exchange: string = this.config.getOrThrow('RABBITMQ_EXCHANGE')): Promise<ClientProvider> {
+  async createClientOptions(exchange: string = this.config.getOrThrow('RABBITMQ_EXCHANGE')): Promise<ClientRmqExchangeModuleOptions> {
 
     const options = await this.build();
 
