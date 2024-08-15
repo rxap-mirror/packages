76a0fe1510c2866d3934a30d56d920bc265ce46d
Merzough Münker <mmuenker@digitaix.com>
2024-08-15 18:18:48 +0200

    fix: use typed logger

--- packages/plugin/nestjs/src/generators/init-application/assert-main-statements.ts
+++ packages/plugin/nestjs/src/generators/init-application/assert-main-statements.ts
@@ -2,7 +2,7 @@ import { CoerceImports } from '@rxap/ts-morph';
 import { SourceFile } from 'ts-morph';
 
 const MAIN_BOOTSTRAP_OPTIONS_STATEMENT = 'const bootstrapOptions: Partial<MonolithicBootstrapOptions> = {};';
-const MAIN_SERVER_STATEMENT = 'const server = new Monolithic<NestApplicationOptions, NestExpressApplication>(AppModule, environment, options, bootstrapOptions);';
+const MAIN_SERVER_STATEMENT = 'const server = new Monolithic<NestApplicationOptions, RxapLogger, NestExpressApplication>(AppModule, environment, options, bootstrapOptions);';
 export const MAIN_BOOTSTRAP_STATEMENT = 'server.bootstrap().catch((e) => console.error(\'Server bootstrap failed: \' + e.message));';
 const MAIN_SETUP_HELMET_STATEMENT = 'server.after(SetupHelmet());';
 const MAIN_SETUP_COOKIE_STATEMENT = 'server.after(SetupCookieParser());';
