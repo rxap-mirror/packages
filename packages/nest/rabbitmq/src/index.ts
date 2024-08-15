1ab945453a3f6c418e98a77c56097c97df318922
Merzough Münker <mmuenker@digitaix.com>
2024-08-15 12:10:32 +0200

    fix: support proper error de/serialization

--- packages/nest/rabbitmq/src/index.ts
+++ packages/nest/rabbitmq/src/index.ts
@@ -1,11 +1,14 @@
-// region 
+// region
 export * from './lib/client-rmq-exchange-module-options-factory';
 export * from './lib/client-rmq-exchange';
-export * from './lib/constants';
+export * from './lib/error.deserializer';
+export * from './lib/error.serializer';
 export * from './lib/incoming-request.deserializer';
+export * from './lib/incoming-response.deserializer';
 export * from './lib/options';
 export * from './lib/rabbitmq-options-factory';
 export * from './lib/rabbitmq.health-indicator';
+export * from './lib/serialized-error';
 export * from './lib/server-rmq';
 export * from './lib/tokens';
 // endregion
--- packages/nest/rabbitmq/src/lib/constants.ts
+++
@@ -1,7 +0,0 @@
-import { Options } from 'amqplib';
-
-export const REPLY_QUEUE = 'amq.rabbitmq.reply-to';
-
-export const RQM_DEFAULT_EXCHANGE = 'default';
-export const RQM_DEFAULT_EXCHANGE_TYPE = 'topic';
-export const RQM_DEFAULT_EXCHANGE_OPTIONS: Options.AssertExchange = { durable: false };
