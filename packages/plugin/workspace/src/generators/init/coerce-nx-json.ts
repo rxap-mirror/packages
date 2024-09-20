ff6fb56af6ace04313ba6b057415837c6e9bf42b
Merzough Münker <mmuenker@digitaix.com>
2024-09-20 12:43:22 +0200

    build: improve output cache rule

--- packages/plugin/workspace/src/generators/init/coerce-nx-json.ts
+++ packages/plugin/workspace/src/generators/init/coerce-nx-json.ts
@@ -98,7 +98,7 @@ export function coerceNxJson(tree: Tree, options: InitGeneratorSchema) {
     'outputs': [
       '{workspaceRoot}/coverage/{projectRoot}',
       '{workspaceRoot}/junit/{projectRoot}',
-      '{workspaceRoot}/{projectRoot}/coverage',
+      '{projectRoot}/coverage',
     ],
     'inputs': [
       'test',
