6e4b71050efdbcd13a6eac406212d9c1eee1ec64
Merzough Münker <mmuenker@digitaix.com>
2024-09-27 14:51:50 +0200

    chore: add n8n plugin

---
+++ packages/plugin/n8n/jest.config.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default {
+  displayName: 'plugin-n8n',
+  preset: '../../../jest.preset.js',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/packages/plugin/n8n',
+};
