8831fe883ea8908ad9c3d395b9e9ad09f1da1387
Merzough Münker <mmuenker@digitaix.com>
2024-10-17 12:33:32 +0200

    fix: add missing generic type

--- packages/nest/utilities/src/lib/cache-module-options-loader.ts
+++ packages/nest/utilities/src/lib/cache-module-options-loader.ts
@@ -9,19 +9,19 @@ import {
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class CacheModuleOptionsLoader implements CacheOptionsFactory {
+export class CacheModuleOptionsLoader<StoreConfig extends Record<any, any> = Record<string, any>> implements CacheOptionsFactory<StoreConfig> {
 
   @Inject(ConfigService)
-  private readonly config!: ConfigService;
+  protected readonly config!: ConfigService;
 
-  createCacheOptions(): CacheModuleOptions {
+  createCacheOptions(): CacheModuleOptions<StoreConfig> {
     const ttl = Number(this.config.get('CACHE_TTL', 60 * 60 * 1000));
     const max = Number(this.config.get('CACHE_MAX', 100));
     return {
       isGlobal: true,
       ttl: isNaN(ttl) ? 60 * 60 * 1000 : ttl,
       max: isNaN(max) ? 100 : max,
-    };
+    } as CacheModuleOptions<StoreConfig>;
   }
 
 }
