eb635dd6e5a16a1534861bc873253c178038e44e
Merzough Münker <mmuenker@digitaix.com>
2025-01-22 17:25:22 +0100

    test: add missing test environment

--- packages/preset/workspace/jest.config.ts
+++ packages/preset/workspace/jest.config.ts
@@ -2,6 +2,7 @@
 export default {
   displayName: 'preset-workspace',
   preset: '../../../jest.preset.js',
+  testEnvironment: 'node',
   setupFilesAfterEnv: [ '<rootDir>/src/test-setup.ts' ],
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
