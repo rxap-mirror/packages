03c84844012e9153a54835c024be4fc993d76f41
Merzough Münker <mmuenker@digitaix.com>
2025-01-29 10:17:58 +0100

    fix: dont generate package json for root nestjs applications

--- packages/plugin/nestjs/src/application.ts
+++ packages/plugin/nestjs/src/application.ts
@@ -116,8 +116,10 @@ async function createProjectConfiguration(
   if (!projectConfiguration) {
     throw new Error(`Could not find project in '${ projectPath }'`);
   }
-
-  targets['generate-package-json'] = createGeneratePackageJsonTarget();
+  // only generate the package json if the project is not in the root
+  if (['', '/'].includes(projectConfiguration.root)) {
+    targets['generate-package-json'] = createGeneratePackageJsonTarget();
+  }
   if (existsSync(join(projectPath, 'src/swagger.ts'))) {
     targets['swagger-build'] = createSwaggerBuildTarget(projectPath);
     targets['swagger-generate'] = createSwaggerGenerateTarget(projectPath);
