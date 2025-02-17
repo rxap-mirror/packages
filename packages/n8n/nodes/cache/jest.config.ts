f411288bd571d5ad64d0c9050b7033ea63fc4c87
Merzough Münker <mmuenker@digitaix.com>
2025-02-17 13:42:02 +0100

    chore: add cache n8n nodes package

---
+++ packages/n8n/nodes/cache/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'n8n-nodes-cache',
+  preset: '../../../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../../coverage/packages/n8n/nodes/cache',
+};
