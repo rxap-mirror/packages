fbea06200e09e356db1b47c1c23df1284ef1bbac
Merzough Münker <mmuenker@digitaix.com>
2025-02-17 11:24:24 +0100

    fix: ignore non compodoc related files

--- packages/plugin/compodoc/src/generators/init-library/init-project.ts
+++ packages/plugin/compodoc/src/generators/init-library/init-project.ts
@@ -40,10 +40,14 @@ export async function initProject(tree: Tree, projectName: string, project: Proj
       .filter(([_, project]) => !IsWorkspaceProject(project))
       .filter(([projectName]) => tree.exists(join(GetProjectRoot(tree, projectName), 'tsconfig.compodoc.json')))
       .map(([projectName]) => GetProjectSourceRoot(tree, projectName))
-      .map(sourceRoot => join(sourceRoot, '**/*.ts'));
+      .map(sourceRoot => [
+        join(sourceRoot, 'lib/**/*.ts'),
+        join(sourceRoot, 'index.ts'),
+      ])
+      .flat();
     CoerceCompodocTsConfig(tree, GetWorkspaceProjectName(tree), angularProjectIncludeList);
   } else {
-    CoerceCompodocTsConfig(tree, projectName);
+    CoerceCompodocTsConfig(tree, projectName, ['src/index.ts', 'src/lib/**/*.ts']);
   }
 
   CoerceGitIgnore(tree, projectName);
