4316b0a7a609f3cd5a2bf278d747e18eb8faa723
Merzough Münker <mmuenker@digitaix.com>
2025-02-17 12:07:11 +0100

    fix: update the package json

--- packages/plugin/n8n/src/generators/node/generator.ts
+++ packages/plugin/n8n/src/generators/node/generator.ts
@@ -2,10 +2,12 @@ import { Tree } from '@nx/devkit';
 import { createNodeJson } from './create-node-json';
 import { createNodeTs } from './create-node-ts';
 import { NodeGeneratorSchema } from './schema';
+import { updatePackageJson } from './update-package-json';
 
 export async function nodeGenerator(tree: Tree, { project, name, nodeNamePrefix = 'rxap', description = name }: NodeGeneratorSchema) {
   createNodeJson(tree, project, name, nodeNamePrefix);
   createNodeTs(tree, project, name, nodeNamePrefix, description);
+  updatePackageJson(tree, project, name);
 }
 
 export default nodeGenerator;
