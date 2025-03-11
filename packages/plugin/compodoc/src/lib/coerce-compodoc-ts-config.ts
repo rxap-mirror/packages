0ac866e9b712d16e956fb1d564271f719554a6ff
Merzough Münker <mmuenker@enduradocs.de>
2025-03-11 16:19:12 +0100

    fix(compodoc): guard tsconfig update with existence check
    Ensure tsconfig update logic only runs if a tsconfig.json file exists to prevent errors in non-typical setups.

--- packages/plugin/compodoc/src/lib/coerce-compodoc-ts-config.ts
+++ packages/plugin/compodoc/src/lib/coerce-compodoc-ts-config.ts
@@ -3,6 +3,7 @@ import { CoerceArrayItems } from '@rxap/utilities';
 import {
   GetProject,
   GetProjectRoot,
+  HasTsConfigJson,
   IsAngularProject,
   IsNestJsProject,
   IsWorkspaceProject,
@@ -39,9 +40,11 @@ export function CoerceCompodocTsConfig(tree: Tree, projectName: string, include:
     }
   }, { infix: 'compodoc', basePath: projectRoot, create: true });
 
-  UpdateTsConfigJson(tree, tsConfig => {
-    tsConfig.references ??= [];
-    CoerceArrayItems(tsConfig.references, [{ path: './tsconfig.compodoc.json' }], (a, b) => a.path === b.path);
-  }, { basePath: projectRoot });
+  if (HasTsConfigJson(tree, { basePath: projectRoot })) {
+    UpdateTsConfigJson(tree, tsConfig => {
+      tsConfig.references ??= [];
+      CoerceArrayItems(tsConfig.references, [ { path: './tsconfig.compodoc.json' } ], (a, b) => a.path === b.path);
+    }, { basePath: projectRoot });
+  }
 
 }
