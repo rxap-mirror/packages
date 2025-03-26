f0142a8e2bf7f64e90daa896219405263ff85407
Merzough Münker <mmuenker@enduradocs.de>
2025-03-26 14:45:30 +0100

    feat(backend): add serverId option for OpenAPI client SDK

--- packages/schematic/angular/src/lib/backend/nest-js-backend-options.ts
+++ packages/schematic/angular/src/lib/backend/nest-js-backend-options.ts
@@ -12,12 +12,17 @@ export interface NestJsBackendOptions extends BaseBackendOptions {
    * The nx workspace project name where the backend will be created
    */
   project?: string;
+  /**
+   * The OpenAPI client sdk serverId used to build operationIds and import paths for operations
+   */
+  serverId?: string
 }
 
 export interface NormalizedNestJsBackendOptions
   extends Readonly<Normalized<Omit<NestJsBackendOptions, keyof BaseBackendOptions>> & NormalizedBaseBackendOptions> {
   kind: BackendTypes.NESTJS;
   project: string | null;
+  serverId: string | null;
 }
 
 export function NormalizeNestJsBackendOptions(options: NestJsBackendOptions, backendContext?: BackendContext): NormalizedNestJsBackendOptions {
@@ -25,5 +30,6 @@ export function NormalizeNestJsBackendOptions(options: NestJsBackendOptions, bac
     ...NormalizeBaseBackendOptions(options, backendContext),
     kind: BackendTypes.NESTJS,
     project: options.project ?? null,
+    serverId: options.serverId ?? null,
   };
 }
