15817bff61b9c37fcb8dbb3bc718c27db0591fcb
Merzough Münker <mmuenker@enduradocs.de>
2025-03-26 14:45:19 +0100

    feat: replace buildNestProjectName with buildOperationServerId

--- packages/schematic/ts-morph/src/lib/nest/operation-id-utilities.ts
+++ packages/schematic/ts-morph/src/lib/nest/operation-id-utilities.ts
@@ -1,11 +1,11 @@
 import { dasherize } from '@rxap/schematics-utilities';
-import { buildNestProjectName } from '@rxap/workspace-utilities';
+import { buildOperationServerId } from '@rxap/workspace-utilities';
 
 export function buildOperationId(options: {
   project: string,
   feature: string | null,
   shared: boolean
-  backend: { project?: string | null, kind?: any } | undefined;
+  backend: { project?: string | null, kind?: any, serverId?: string } | undefined;
 }, operation: string, controller: string): string {
-  return `${ dasherize(controller) }-controller-${ dasherize(operation) }@${ buildNestProjectName(options) }`;
+  return `${ dasherize(controller) }-controller-${ dasherize(operation) }@${ buildOperationServerId(options) }`;
 }
