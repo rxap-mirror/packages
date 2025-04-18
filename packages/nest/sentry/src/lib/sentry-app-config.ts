02ca841afd06b01e952fb7cf1b8cfa2ccd3c54fc
Merzough Münker <mmuenker@enduradocs.de>
2025-04-18 11:01:38 +0200

    fix: support sentry config defaults

--- packages/nest/sentry/src/lib/sentry-app-config.ts
+++ packages/nest/sentry/src/lib/sentry-app-config.ts
@@ -2,22 +2,50 @@ import { Environment } from '@rxap/nest-utilities';
 import * as Joi from 'joi';
 import type { SchemaMap } from 'joi';
 
-export function sentryValidationSchema(environment: Environment): SchemaMap {
+export interface SentryConfig {
+  debug: boolean;
+  serverName: string;
+  release: string;
+  environment: string;
+  enabled: boolean;
+  dsn: string;
+}
+
+export function sentryValidationSchema(
+  environment: Environment,
+  {
+    debug = environment.sentry?.debug ?? false,
+    serverName = process.env['ROOT_DOMAIN'] ?? environment.app,
+    release,
+    environment: environmentName,
+    enabled = environment.sentry?.enabled ?? false,
+    dsn
+  }: Partial<SentryConfig> = {}
+): SchemaMap {
   const schema: SchemaMap = {};
 
-  schema['SENTRY_DEBUG'] = Joi.string().default(
-    environment.sentry?.debug ?? false
-  );
+  schema['SENTRY_DEBUG'] = Joi.string().default(debug);
+  schema['SENTRY_SERVER_NAME'] = Joi.string().default(serverName);
 
-  schema['SENTRY_SERVER_NAME'] = Joi.string().default(
-    process.env['ROOT_DOMAIN'] ?? environment.app
-  );
-  schema['SENTRY_RELEASE'] = Joi.string();
-  schema['SENTRY_ENVIRONMENT'] = Joi.string();
-  schema['SENTRY_ENABLED'] = Joi.string().default(
-    environment.sentry?.enabled ?? false
-  );
-  schema['SENTRY_DSN'] = Joi.string();
+  let releaseConfig = Joi.string();
+  if (release) {
+    releaseConfig = releaseConfig.default(release);
+  }
+  schema['SENTRY_RELEASE'] = releaseConfig;
+
+  let environmentConfig = Joi.string();
+  if (environmentName) {
+    environmentConfig = environmentConfig.default(environmentName);
+  }
+  schema['SENTRY_ENVIRONMENT'] = environmentConfig;
+
+  schema['SENTRY_ENABLED'] = Joi.string().default(enabled);
+
+  let dsnConfig = Joi.string();
+  if (dsn) {
+    dsnConfig = dsnConfig.default(dsn);
+  }
+  schema['SENTRY_DSN'] = dsnConfig;
 
   return schema;
 }
