0d9e380e77d315823a7df5c356f53675aff039fd
Merzough Münker <mmuenker@enduradocs.de>
2025-08-27 11:31:22 +0200

    feat(utilities): add parameterized defaults for validation schema
    Introduce optional parameters for `cookieSecret`, `throttlerLimit`, and `throttlerTTL` in `defaultValidationSchema` to allow customization alongside default values.

--- packages/nest/utilities/src/lib/default-validation-schema.ts
+++ packages/nest/utilities/src/lib/default-validation-schema.ts
@@ -3,19 +3,24 @@ import { GenerateRandomString } from '@rxap/utilities';
 import * as Joi from 'joi';
 import type { SchemaMap } from 'joi';
 
-export function defaultValidationSchema(environment: Environment): SchemaMap {
+export function defaultValidationSchema(
+  environment: Environment,
+  {
+    cookieSecret = GenerateRandomString(),
+    throttlerLimit = 10,
+    throttlerTTL = 1,
+  }: {
+    cookieSecret?: string,
+    throttlerLimit?: number,
+    throttlerTTL?: number,
+  }
+  ): SchemaMap {
 
   const schema: SchemaMap = {};
 
-  schema['COOKIE_SECRET'] = Joi.string().default(
-    GenerateRandomString()
-  );
-  schema['THROTTLER_LIMIT'] = Joi.string().default(10);
-  schema['THROTTLER_TTL'] = Joi.string().default(1);
-  schema['GLOBAL_API_PREFIX'] = Joi.string().default(
-    'api'
-  );
-  schema['PORT'] = Joi.number().default(3183);
+  schema['COOKIE_SECRET'] = Joi.string().default(cookieSecret);
+  schema['THROTTLER_LIMIT'] = Joi.string().default(throttlerLimit);
+  schema['THROTTLER_TTL'] = Joi.string().default(throttlerTTL);
 
   return schema;
 
