6b1b5ff6552a22be754bac3077887e9202fa7d4a
Merzough Münker <mmuenker@enduradocs.de>
2025-09-01 12:28:19 +0200

    feat(ts-morph): add utilities for coercing interface and function declarations
    Introduce `CoerceInterface` and `CoerceFunction` utilities to simplify the process of adding or modifying interface and function declarations in source files.

---
+++ packages/ts-morph/src/lib/coerce-interface.ts
@@ -0,0 +1,22 @@
+import {
+  InterfaceDeclarationStructure,
+  OptionalKind,
+  SourceFile,
+} from 'ts-morph';
+
+export function CoerceInterface(
+  sourceFile: SourceFile,
+  interfaceName: string,
+  structure: Omit<OptionalKind<InterfaceDeclarationStructure>, 'name'> = {},
+) {
+  let interfaceDeclaration = sourceFile.getInterface(interfaceName);
+  if (!interfaceDeclaration) {
+    interfaceDeclaration = sourceFile.addInterface({
+      ...structure,
+      name: interfaceName,
+    });
+  }
+  return interfaceDeclaration;
+}
+
+
