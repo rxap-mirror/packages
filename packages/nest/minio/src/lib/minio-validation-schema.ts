032cfb9fa4a11ac5f29fb4c6ec27d470f1a034bd
Merzough Münker <mmuenker@enduradocs.de>
2025-09-01 22:16:17 +0200

    feat(minio): enhance validation schema with dynamic defaults
    Refactor the `minioValidationSchema` function to accept dynamic default values for `endPoint`, `port`, `useSSL`, `accessKey`, and `secretKey`, improving flexibility and configurability.

--- packages/nest/minio/src/lib/minio-validation-schema.ts
+++ packages/nest/minio/src/lib/minio-validation-schema.ts
@@ -2,15 +2,27 @@ import { Environment } from '@rxap/nest-utilities';
 import { SchemaMap } from 'joi';
 import * as Joi from 'joi';
 
-export function minioValidationSchema(environment: Environment) {
+export function minioValidationSchema(environment: Environment, {
+  endPoint = 'minio',
+  useSSL = endPoint !== 'minio',
+  accessKey = 'minioadmin',
+  secretKey = 'minioadmin',
+  port = useSSL ? 443 : 9000,
+}: {
+  endPoint?: string;
+  port?: number;
+  useSSL?: boolean;
+  accessKey?: string;
+  secretKey?: string;
+}) {
 
   const schema: SchemaMap = {};
 
-  schema['MINIO_END_POINT'] = Joi.string().default('minio');
-  schema['MINIO_PORT'] = Joi.number().default(9000);
-  schema['MINIO_USE_SSL'] = Joi.boolean().default(false);
-  schema['MINIO_ACCESS_KEY'] = Joi.string().default('minioadmin');
-  schema['MINIO_SECRET_KEY'] = Joi.string().default('minioadmin');
+  schema['MINIO_END_POINT'] = Joi.string().default(endPoint);
+  schema['MINIO_PORT'] = Joi.number().default(port);
+  schema['MINIO_USE_SSL'] = Joi.boolean().default(useSSL);
+  schema['MINIO_ACCESS_KEY'] = Joi.string().default(accessKey);
+  schema['MINIO_SECRET_KEY'] = Joi.string().default(secretKey);
 
   return schema;
 }
