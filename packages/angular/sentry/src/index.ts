4718bc38a70f447c5d799668172e1d82e3b07871
Merzough Münker <mmuenker@enduradocs.de>
2025-09-08 11:18:49 +0200

    feat(angular-sentry): upgrade to sentry 10

--- packages/angular/sentry/src/index.ts
+++ packages/angular/sentry/src/index.ts
@@ -1,6 +1,5 @@
-// region 
+// region
 export * from './lib/determine-sentry-environment';
 export * from './lib/determine-sentry-release';
 export * from './lib/sentry-init-bootstrap-hook';
-export * from './lib/sentry-init';
 // endregion
