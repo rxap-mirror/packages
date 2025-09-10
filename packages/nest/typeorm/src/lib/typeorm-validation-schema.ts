12fc2e2fc89756fa5f7a0f7f9258f4c1fe859281
Merzough Münker <mmuenker@enduradocs.de>
2025-09-10 16:17:22 +0200

    refactor(typeorm): update validation schema to support configurable defaults
    - Added parameters for `synchronize`, `logging`, and `type` with configurable default values.
    - Adjusted Joi schema defaults to reflect the passed options.

--- packages/nest/typeorm/src/lib/typeorm-validation-schema.ts
+++ packages/nest/typeorm/src/lib/typeorm-validation-schema.ts
@@ -1,15 +1,26 @@
-import { SchemaMap } from 'joi';
-import * as Joi from 'joi';
 import { Environment } from '@rxap/nest-utilities';
+import * as Joi from 'joi';
+import { SchemaMap } from 'joi';
 import { postgresValidationSchema } from './postgres-validation-schema';
 import { sqliteValidationSchema } from './sqlite-validation-schema';
 
-export const typeOrmValidationSchema: (environment: Environment) => SchemaMap = (environment: Environment) => {
+export function typeOrmValidationSchema(
+  environment: Environment,
+  {
+    synchronize = !environment.production,
+    logging = process.env['LOG_LEVEL'] === 'verbose',
+    type = 'sqlite',
+  }: {
+    synchronize?: boolean,
+    logging?: boolean,
+    type?: string,
+  } = {}
+): SchemaMap {
   return {
     ...postgresValidationSchema(environment),
     ...sqliteValidationSchema(environment),
-    TYPEORM_TYPE: Joi.string().default('sqlite'),
-    TYPEORM_SYNCHRONIZE: Joi.boolean().default(!environment.production),
-    TYPEORM_LOGGING: Joi.boolean().default(process.env['LOG_LEVEL'] === 'verbose'),
+    TYPEORM_TYPE: Joi.string().default(type),
+    TYPEORM_SYNCHRONIZE: Joi.boolean().default(synchronize),
+    TYPEORM_LOGGING: Joi.boolean().default(logging),
   };
-};
+}
