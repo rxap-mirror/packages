1fdfc7f21af21d9bee04adb0768c6badc713a617
Merzough Münker <mmuenker@enduradocs.de>
2025-09-15 14:40:27 +0200

    refactor(server): enhance CORS setup for flexible origin and headers
    - Improved `SetupCors` to handle comma-separated and regex-based origins.
    - Updated Joi schema to validate headers as strings instead of arrays.

--- packages/nest/server/src/lib/setup-cors.ts
+++ packages/nest/server/src/lib/setup-cors.ts
@@ -1,4 +1,7 @@
-import { INestApplication } from '@nestjs/common';
+import {
+  INestApplication,
+  Logger,
+} from '@nestjs/common';
 import type {
   CorsOptions,
   CorsOptionsDelegate,
@@ -14,15 +17,23 @@ export interface SetupCorsOptions {
 }
 
 export function SetupCors({ corsOptions }: SetupCorsOptions = {}) {
-  return (app: INestApplication, config: ConfigService) =>
-    app.enableCors({
-      credentials: config.get('CORS_CREDENTIALS', true),
-      origin: config.get('CORS_ORIGIN', true),
-      allowedHeaders: config.get('CORS_ALLOWED_HEADERS', ['sentry-trace', 'baggage']),
-      exposedHeaders: config.get('CORS_EXPOSED_HEADERS'),
-      methods: config.get('CORS_METHODS'),
+  return (app: INestApplication, config: ConfigService, logger: Logger) => {
+    let origin: any = config.getOrThrow<string | boolean>('CORS_ORIGIN');
+    if (typeof origin === 'string') {
+      origin = origin.split(',').map(item => item.trim())
+        .map(item => item.startsWith('/') && item.endsWith('/') ? new RegExp(item) : item);
+    }
+    const options = {
+      credentials: config.getOrThrow('CORS_CREDENTIALS'),
+      origin,
+      allowedHeaders: config.getOrThrow('CORS_ALLOWED_HEADERS', [ 'sentry-trace', 'baggage' ]).split(','),
+      exposedHeaders: config.getOrThrow('CORS_EXPOSED_HEADERS').split(','),
+      methods: config.getOrThrow('CORS_METHODS').split(','),
       ...corsOptions,
-    });
+    };
+    logger.debug(`Enable cors with options: %JSON`, options, 'Bootstrap::SetupCors');
+    app.enableCors(options);
+  };
 }
 
 export function corsValidationSchema(
@@ -30,19 +41,25 @@ export function corsValidationSchema(
   {
     origin = true,
     credentials = true,
+    methods = [ 'GET', 'PUT', 'POST', 'DELETE' ],
+    allowedHeaders = [ 'sentry-trace', 'baggage' ],
+    exposedHeaders = []
   }: {
-    origin?: any
-    credentials?: boolean
+    origin?: string | boolean,
+    credentials?: boolean,
+    methods?: string[],
+    allowedHeaders?: string[],
+    exposedHeaders?: string[]
   } = {}
 ) {
 
   const schema: SchemaMap = {};
 
-  schema['CORS_ORIGIN'] = Joi.any().default(origin);
+  schema['CORS_ORIGIN'] = Joi.allow(Joi.string(), Joi.boolean()).default(origin);
   schema['CORS_CREDENTIALS'] = Joi.boolean().default(credentials);
-  schema['CORS_ALLOWED_HEADERS'] = Joi.array().items(Joi.string()).default(['sentry-trace', 'baggage']);
-  schema['CORS_EXPOSED_HEADERS'] = Joi.array().items(Joi.string());
-  schema['CORS_METHODS'] = Joi.array().items(Joi.string().valid('GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS', 'TRACE', 'CONNECT'));
+  schema['CORS_ALLOWED_HEADERS'] = Joi.string().default(allowedHeaders.join(','));
+  schema['CORS_EXPOSED_HEADERS'] = Joi.string().default(exposedHeaders.join(','));
+  schema['CORS_METHODS'] = Joi.string().default(methods.join(','));
 
   return schema;
 
