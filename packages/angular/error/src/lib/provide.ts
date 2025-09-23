cabd1fa97c6c56bdc3d794e0682af23b99b82b85
Merzough Münker <mmuenker@enduradocs.de>
2025-09-23 14:50:39 +0200

    feat(error): add conditional provider for error dialog disablement
    - Introduced `withErrorCaptureDialogDisabledIf` function to conditionally provide `RXAP_ERROR_DIALOG_DISABLED`.

--- packages/angular/error/src/lib/provide.ts
+++ packages/angular/error/src/lib/provide.ts
@@ -5,6 +5,7 @@ import {
 } from '@angular/core';
 import {
   RXAP_ERROR_CAPTURE_DIALOG_SERVICE,
+  RXAP_ERROR_DIALOG_DISABLED,
 } from './tokens';
 import { Constructor } from '@rxap/utilities';
 import { IErrorCaptureDialogService } from './error-capture-dialog.service';
@@ -24,6 +25,13 @@ export function withErrorCaptureDialogService(service: Constructor<IErrorCapture
   return {
     provide: RXAP_ERROR_CAPTURE_DIALOG_SERVICE,
     useClass: service,
+  };
+}
+
+export function withErrorCaptureDialogDisabledIf(condition: boolean | (() => boolean)): Provider {
+  return {
+    provide: RXAP_ERROR_DIALOG_DISABLED,
+    useValue: typeof condition === 'function' ? condition() : condition,
   };
 }
 
