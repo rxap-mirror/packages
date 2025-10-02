02da71e2d0e51ef9612d41277566a2fbc04c6241
Merzough Münker <mmuenker@enduradocs.de>
2025-10-02 12:37:46 +0200

    feat(xml-parser): add element registry to track registered tags
    - Introduced `ELEMENT_REGISTRY` to maintain a map of registered tags and their associated classes.
    - Added a warning log when attempting to register a duplicate tag, enhancing error handling.

--- packages/xml-parser/src/lib/decorators/element-def.ts
+++ packages/xml-parser/src/lib/decorators/element-def.ts
@@ -3,6 +3,9 @@ import {
   setMetadata,
 } from '@rxap/reflect-metadata';
 import { ElementParserMetaData } from './metadata-keys';
+import { ParsedElementType } from './utilities';
+
+export const ELEMENT_REGISTRY = new Map<string, ParsedElementType>();
 
 /**
  * A decorator factory that assigns a specific HTML tag name to a class and configures its metadata for element parsing.
@@ -26,6 +29,10 @@ import { ElementParserMetaData } from './metadata-keys';
 export function ElementDef(tag: string) {
   return function (target: any) {
     target.TAG = tag;
+    if (ELEMENT_REGISTRY.has(tag)) {
+      console.warn(`The tag ${ tag } is already registered`);
+    }
+    ELEMENT_REGISTRY.set(tag, target);
     // TODO : remove deprecated
     setMetadata(ElementParserMetaData.NAME, tag, target);
     if (!hasMetadata(ElementParserMetaData.PARSER, target)) {
