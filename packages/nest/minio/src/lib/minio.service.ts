2748e56c013798e42b84a0e7bb473136194dc1c7
Merzough Münker <mmuenker@enduradocs.de>
2025-10-29 14:05:27 +0100

    feat(minio): add endpoint getter in MinioService
    - Added a `get endPoint` method to access the `endPoint` option from `ClientOptions`.
    - Updated the constructor to make `options` a protected readonly property.

--- packages/nest/minio/src/lib/minio.service.ts
+++ packages/nest/minio/src/lib/minio.service.ts
@@ -11,8 +11,12 @@ import { MINIO_OPTIONS } from './tokens';
 @Injectable()
 export class MinioService extends Client {
 
+  get endPoint(): string {
+    return this.options.endPoint;
+  }
+
   constructor(
-    @Inject(MINIO_OPTIONS) options: ClientOptions,
+    @Inject(MINIO_OPTIONS) protected readonly options: ClientOptions,
   ) {
     super(options);
   }
